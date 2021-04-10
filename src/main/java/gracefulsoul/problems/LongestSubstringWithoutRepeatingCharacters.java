63138d7b035a538748268218ac285f17d0e7628e
yongsik <mymyyoo@naver.com>
2021-04-10 23:09:01 +0900

    Add code about Longest Substring Without Repeating Characters

---
+++ src/main/java/gracefulsoul/problems/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,29 @@
+package gracefulsoul.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+	public static void main(String[] args) {
+		System.out.println(lengthOfLongestSubstring("abcabcbb"));
+		System.err.println(lengthOfLongestSubstring("bbbbb"));
+		System.out.println(lengthOfLongestSubstring("pwwkew"));
+	}
+
+	// https://leetcode.com/submissions/detail/478726995/
+	public static int lengthOfLongestSubstring(String s) {
+		int result = 0;
+		Map<Character, Integer> map = new HashMap<>();
+		for (int i = 0, j = 0; i < s.length(); ++i) {
+			char c = s.charAt(i);
+			if (map.containsKey(c)) {
+				j = Math.max(j, map.get(c) + 1);
+			}
+			map.put(c, i);
+			result = Math.max(result, i - j + 1);
+		}
+		return result;
+	}
+
+}
