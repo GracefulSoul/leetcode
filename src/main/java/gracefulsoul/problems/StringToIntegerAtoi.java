47bfaf4ab50be416b4047909f7c26decb1983beb
yongsik <mymyyoo@naver.com>
2021-04-16 17:31:10 +0900

    Add code about String To Integer (atoi)

--- src/main/java/gracefulsoul/problems/StringToIntegerAtoi.java
+++ src/main/java/gracefulsoul/problems/StringToIntegerAtoi.java
@@ -2,6 +2,7 @@ package gracefulsoul.problems;
 
 public class StringToIntegerAtoi {
 
+	// https://leetcode.com/submissions/detail/481299519/
 	public static void main(String[] args) {
 		System.out.println(myAtoi("42"));
 		System.out.println(myAtoi("   -42"));
@@ -13,19 +14,19 @@ public class StringToIntegerAtoi {
 
 	public static int myAtoi(String s) {
 		int result = 0;
-		if (s.length() == 0) {
-			return result;
-		}
 		int idx = getBeginning(s);
 		int sign = 1;
-		if (idx < s.length() && (s.charAt(idx) == '+' || s.charAt(idx) == '-')) {
-			sign = (s.charAt(idx++) == '-') ? -1 : 1;
+		if (s.length() == 0 || idx == s.length()) {
+			return result;
+		}
+		if (s.charAt(idx) == '+' || s.charAt(idx) == '-') {
+			sign = s.charAt(idx++) == '-' ? -1 : 1;
 		}
 		while (idx < s.length() && s.charAt(idx) >= '0' && s.charAt(idx) <= '9') {
 			if (isOverflow(result, s.charAt(idx))) {
 				return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
 			}
-			result = result * 10 + (s.charAt(idx++) - '0');
+			result = result * 10 + s.charAt(idx++) - '0';
 		}
 		return result * sign;
 	}
