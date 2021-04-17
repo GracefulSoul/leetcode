25b396222ca0f38eefae858f8a4224b76ab25bad
yongsik <mymyyoo@naver.com>
2021-04-17 15:05:51 +0900

    Add code about Leetcode - Regular Expression Matching

---
+++ src/main/java/gracefulsoul/problems/RegularExpressionMatching.java
@@ -0,0 +1,51 @@
+package gracefulsoul.problems;
+
+public class RegularExpressionMatching {
+
+	// https://leetcode.com/submissions/detail/481614271/
+	public static void main(String[] args) {
+		System.out.println(isMatch("aa", "a"));
+		System.out.println(isMatch("aa", "a*"));
+		System.out.println(isMatch("ab", ".*"));
+		System.out.println(isMatch("aab", "c*a*b"));
+		System.out.println(isMatch("mississippi", "mis*is*p*."));
+	}
+
+	public static boolean isMatch(String s, String p) {
+		if (isBlank(p)) {
+			return isBlank(s);
+		}
+		boolean[][] dp = initDp(s, p);
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = 0; j < p.length(); j++) {
+				if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {
+					dp[i + 1][j + 1] = dp[i][j];
+				}
+				if (p.charAt(j) == '*') {
+					if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+						dp[i + 1][j + 1] = dp[i + 1][j - 1];
+					} else {
+						dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+					}
+				}
+			}
+		}
+		return dp[s.length()][p.length()];
+	}
+
+	private static boolean isBlank(String s) {
+		return s == null || s.length() == 0;
+	}
+
+	private static boolean[][] initDp(String s, String p) {
+		boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+		dp[0][0] = true;
+		for (int i = 0; i < p.length(); i++) {
+			if (p.charAt(i) == '*' && dp[0][i - 1]) {
+				dp[0][i + 1] = true;
+			}
+		}
+		return dp;
+	}
+
+}
