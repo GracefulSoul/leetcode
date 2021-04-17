97fcd9692a2f9d88915a586d6a23a8f7a557c727
yongsik <mymyyoo@naver.com>
2021-04-17 12:40:48 +0900

    Add code about Leetcode - Palindrome Number

--- src/main/java/gracefulsoul/problems/PalindromeNumber.java
+++ src/main/java/gracefulsoul/problems/PalindromeNumber.java
@@ -2,7 +2,6 @@ package gracefulsoul.problems;
 
 public class PalindromeNumber {
 
-	// https://leetcode.com/submissions/detail/481574918/
 	public static void main(String[] args) {
 		System.out.println(isPalindrome(121));
 		System.out.println(isPalindrome(-121));
@@ -10,6 +9,7 @@ public class PalindromeNumber {
 		System.out.println(isPalindrome(-101));
 	}
 
+	// https://leetcode.com/submissions/detail/481574918/
 	public static boolean isPalindrome(int x) {
 		if (x < 0 || (x != 0 && x % 10 == 0)) {
 			return false;
@@ -22,5 +22,20 @@ public class PalindromeNumber {
 			return x == reverse || x == reverse / 10;
 		}
 	}
+
+	// https://leetcode.com/submissions/detail/481576427/
+//	public static boolean isPalindrome(int x) {
+//		if (x < 0) {
+//			return false;
+//		} else {
+//			int origin = x;
+//			int reverse = 0;
+//			while (x != 0) {
+//				reverse = reverse * 10 + x % 10;
+//				x /= 10;
+//			}
+//			return origin == reverse;
+//		}
+//	}
 
 }
