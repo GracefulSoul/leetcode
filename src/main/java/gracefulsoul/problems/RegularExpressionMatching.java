7b100632c9108a9dbf7dbbf17e30f26aec144b3b
yongsik <mymyyoo@naver.com>
2021-04-17 17:16:48 +0900

    Add code about Leetcode - Regular Expression Matching

--- src/main/java/gracefulsoul/problems/RegularExpressionMatching.java
+++ src/main/java/gracefulsoul/problems/RegularExpressionMatching.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class RegularExpressionMatching {
 
-	// https://leetcode.com/submissions/detail/481614271/
+	// https://leetcode.com/submissions/detail/481648922/
 	public static void main(String[] args) {
 		System.out.println(isMatch("aa", "a"));
 		System.out.println(isMatch("aa", "a*"));
@@ -20,8 +20,7 @@ public class RegularExpressionMatching {
 			for (int j = 0; j < p.length(); j++) {
 				if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {
 					dp[i + 1][j + 1] = dp[i][j];
-				}
-				if (p.charAt(j) == '*') {
+				} else if (p.charAt(j) == '*') {
 					if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
 						dp[i + 1][j + 1] = dp[i + 1][j - 1];
 					} else {
@@ -40,10 +39,8 @@ public class RegularExpressionMatching {
 	private static boolean[][] initDp(String s, String p) {
 		boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
 		dp[0][0] = true;
-		for (int i = 0; i < p.length(); i++) {
-			if (p.charAt(i) == '*' && dp[0][i - 1]) {
-				dp[0][i + 1] = true;
-			}
+		for (int i = 1; i < p.length(); i+=2) {
+			dp[0][i + 1] = p.charAt(i) == '*' && dp[0][i - 1];
 		}
 		return dp;
 	}
