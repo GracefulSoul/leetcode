ed741e6eed5ecec4c079240d78821a6285047736
yongsik <mymyyoo@naver.com>
2021-04-19 20:16:50 +0900

    Add code about Leetcode - Container With Most Water

---
+++ src/main/java/gracefulsoul/problems/ContainerWithMostWater.java
@@ -0,0 +1,30 @@
+package gracefulsoul.problems;
+
+public class ContainerWithMostWater {
+
+	// https://leetcode.com/submissions/detail/482578956/
+	public static void main(String[] args) {
+		System.out.println(maxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+		System.out.println(maxArea(new int[] { 1, 1 }));
+		System.out.println(maxArea(new int[] { 4, 3, 2, 1, 4 }));
+		System.out.println(maxArea(new int[] { 1, 2, 1 }));
+		System.out.println(maxArea(new int[] { 2, 3, 4, 5, 18, 17, 6 }));
+		System.out.println(maxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 25, 7 }));
+	}
+
+	public static int maxArea(int[] height) {
+		int start = 0;
+		int end = height.length - 1;
+		int max = 0;
+		while (start < end) {
+			max = Math.max(max, Math.min(height[start], height[end]) * (end - start));
+			if (height[start] < height[end]) {
+				start++;
+			} else {
+				end--;
+			}
+		}
+		return max;
+	}
+
+}
