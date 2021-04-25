b2813660641362a490bdb1e9eb7d4e5165569ad3
yongsik <mymyyoo@naver.com>
2021-04-25 10:32:03 +0900

    Add code about Leetcode - 3Sum

---
+++ src/main/java/gracefulsoul/problems/ThreeSum.java
@@ -0,0 +1,50 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+
+	public static void main(String[] args) {
+		ThreeSum test = new ThreeSum();
+		print(test.threeSum(new int[] { -1, 0, 1, 2, -1, -4 }));
+		print(test.threeSum(new int[] {}));
+		print(test.threeSum(new int[] { 0 }));
+	}
+
+	public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length - 2; i++) {
+			if (i == 0 || i > 0 && nums[i] != nums[i - 1]) {
+				int j = i + 1;
+				int k = nums.length - 1;
+				int sum = 0 - nums[i];
+				while (j < k) {
+					if (nums[j] + nums[k] == sum) {
+						result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+						while (j < k && nums[j] == nums[j + 1]) {
+							j++;
+						}
+						while (j < k && nums[k - 1] == nums[k]) {
+							k--;
+						}
+						j++;
+						k--;
+					} else if (nums[j] + nums[k] < sum) {
+						j++;
+					} else {
+						k--;
+					}
+				}
+			}
+		}
+		return result;
+	}
+	
+	private static void print(List<List<Integer>> list) {
+		list.forEach(System.out::println);
+	}
+	
+}
