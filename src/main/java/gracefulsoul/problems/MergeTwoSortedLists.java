79fae259218639193fb93bfcd15723e082ec9ed4
yongsik <mymyyoo@naver.com>
2021-05-01 17:29:20 +0900

    Add code about Leetcode - Merge Two Sorted Lists

--- src/main/java/gracefulsoul/problems/MergeTwoSortedLists.java
+++ src/main/java/gracefulsoul/problems/MergeTwoSortedLists.java
@@ -7,10 +7,9 @@ public class MergeTwoSortedLists {
 	// https://leetcode.com/submissions/detail/487337839/
 	public static void main(String[] args) {
 		MergeTwoSortedLists test = new MergeTwoSortedLists();
-		ListNode node1 = null;
-		ListNode node2 = new ListNode(0, null);
-		test.mergeTwoLists(node1, node2);
-
+		print(test.mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4, null))), new ListNode(1, new ListNode(3, new ListNode(4, null)))));
+		print(test.mergeTwoLists(new ListNode(), new ListNode()));
+		print(test.mergeTwoLists(new ListNode(), new ListNode(0)));
 	}
 
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
@@ -27,6 +26,17 @@ public class MergeTwoSortedLists {
 				return l2;
 			}
 		}
+	}
+
+	private static void print(ListNode listNode) {
+		while (listNode != null) {
+			System.out.print(listNode.val);
+			if (listNode.next != null) {
+				System.out.print(",");
+			}
+			listNode = listNode.next;
+		}
+		System.out.println();
 	}
 
 }
