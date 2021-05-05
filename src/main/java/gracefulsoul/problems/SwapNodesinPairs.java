5a50b627cab8149cc4576e831dcb690bdbe31391
yongsik <mymyyoo@naver.com>
2021-05-05 09:48:49 +0900

    Add code about Leetcode - Swap Nodes in Pairs

--- src/main/java/gracefulsoul/problems/SwapNodesinPairs.java
+++ src/main/java/gracefulsoul/problems/SwapNodesinPairs.java
@@ -7,6 +7,7 @@ import gracefulsoul.object.ListNode;
 
 public class SwapNodesinPairs {
 
+	// https://leetcode.com/submissions/detail/488972679/
 	public static void main(String[] args) {
 		SwapNodesinPairs test = new SwapNodesinPairs();
 		print(test.swapPairs(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))))));
@@ -17,23 +18,27 @@ public class SwapNodesinPairs {
 		if (head == null || head.next == null) {
 			return head;
 		}
+		List<Integer> list = this.getValuesInListNodeToList(head);
+		ListNode ln = null;
+		for (int idx = list.size(); idx > 0; idx--) {
+			ln = new ListNode(list.get(idx - 1), ln);
+		}
+		return ln;
+	}
+	
+	private List<Integer> getValuesInListNodeToList(ListNode head) {
 		List<Integer> list = new ArrayList<>();
 		while (head != null) {
 			if (head.next == null) {
 				list.add(head.val);
 				head = head.next;
 			} else {
-				ListNode temp = head.next;
-				list.add(temp.val);
+				list.add(head.next.val);
 				list.add(head.val);
 				head = head.next.next;
 			}
 		}
-		ListNode ln = null;
-		for (int idx = list.size(); idx > 0; idx--) {
-			ln = new ListNode(list.get(idx - 1), ln);
-		}
-		return ln;
+		return list;
 	}
 
 	private static void print(ListNode listNode) {
