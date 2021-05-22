3091d08f734d911ac0cb3e86ff4dbe86c4a50801
yongsik <mymyyoo@naver.com>
2021-05-22 11:23:46 +0900

    Add code about Leetcode - First Missing Positive

---
+++ src/main/java/gracefulsoul/problems/FirstMissingPositive.java
@@ -0,0 +1,35 @@
+package gracefulsoul.problems;
+
+public class FirstMissingPositive {
+
+	// https://leetcode.com/submissions/detail/496465405/
+	public static void main(String[] args) {
+		FirstMissingPositive test = new FirstMissingPositive();
+//		System.out.println(test.firstMissingPositive(new int[] { 1, 2, 0 }));
+//		System.out.println(test.firstMissingPositive(new int[] { 3, 4, -1, 1 }));
+		System.out.println(test.firstMissingPositive(new int[] { 7, 8, 9, 11, 12 }));
+	}
+
+	public int firstMissingPositive(int[] nums) {
+		int idx = 0;
+		while (idx < nums.length) {
+			if (nums[idx] >= 1 && nums[idx] <= nums.length && nums[nums[idx] - 1] != nums[idx]) {
+				swap(nums, idx, nums[idx] - 1);
+			} else {
+				idx++;
+			}
+		}
+		idx = 0;
+		while (idx < nums.length && nums[idx] == idx + 1) {
+			idx++;
+		}
+		return idx + 1;
+	}
+
+	private void swap(int[] nums, int left, int right) {
+		int temp = nums[left];
+		nums[left] = nums[right];
+		nums[right] = temp;
+	}
+
+}
