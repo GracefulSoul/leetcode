d1fd7091d377fafbc0a885b5e03f2c87623c1f0a
yongsik <mymyyoo@naver.com>
2021-05-27 16:09:26 +0900

    Add code about Leetcode - Jump Game II

---
+++ src/main/java/gracefulsoul/problems/JumpGameII.java
@@ -0,0 +1,28 @@
+package gracefulsoul.problems;
+
+public class JumpGameII {
+
+	public static void main(String[] args) {
+		JumpGameII test = new JumpGameII();
+		System.out.println(test.jump(new int[] { 2, 3, 1, 1, 4 }));
+		System.out.println(test.jump(new int[] { 2, 3, 0, 1, 4 }));
+	}
+
+	public int jump(int[] nums) {
+		int count = 0;
+		int position = 0;
+		int target = 0;
+		for (int idx = 0; idx < nums.length - 1; idx++) {
+			target = Math.max(target, idx + nums[idx]);
+			if (idx == position) {
+				count++;
+				position = target;
+			}
+			if (position == nums.length - 1) {
+				break;
+			}
+		}
+		return count;
+	}
+
+}
