c03279aa08a2c1df17d259cdcc750df2aebd06aa
yongsik <mymyyoo@naver.com>
2021-05-28 20:32:41 +0900

    Add code about Leetcode - Permutations

---
+++ src/main/java/gracefulsoul/problems/Permutations.java
@@ -0,0 +1,41 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+
+	// https://leetcode.com/submissions/detail/499405241/
+	public static void main(String[] args) {
+		Permutations test = new Permutations();
+		print(test.permute(new int[] { 1, 2, 3 }));
+		print(test.permute(new int[] { 0, 1 }));
+		print(test.permute(new int[] { 1 }));
+	}
+
+	public List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		this.getPermutations(result, new ArrayList<>(), nums);
+		return result;
+	}
+
+	private void getPermutations(List<List<Integer>> result, List<Integer> list, int[] nums) {
+		if (list.size() == nums.length) {
+			result.add(new ArrayList<Integer>(list));
+		} else {
+			for (int idx = 0; idx < nums.length; idx++) {
+				if (list.contains(nums[idx])) {
+					continue;
+				}
+				list.add(nums[idx]);
+				this.getPermutations(result, list, nums);
+				list.remove(list.size() - 1);
+			}
+		}
+	}
+
+	private static void print(List<List<Integer>> list) {
+		System.out.println(list);
+	}
+
+}
