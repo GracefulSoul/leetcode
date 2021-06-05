267929b9d4ac6ca89de69aa4883e090f9f8be4b8
yongsik <mymyyoo@naver.com>
2021-06-05 19:16:44 +0900

    Add code about Leetcode - Merge Intervals

---
+++ src/main/java/gracefulsoul/problems/MergeIntervals.java
@@ -0,0 +1,49 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+
+	// https://leetcode.com/submissions/detail/503302199/
+	public static void main(String[] args) {
+		MergeIntervals test = new MergeIntervals();
+		print(test.merge(new int[][] {
+			new int[] { 1, 3 },
+			new int[] { 2, 6 },
+			new int[] { 8, 10 },
+			new int[] { 15, 18 }
+		}));
+		print(test.merge(new int[][] {
+			new int[] { 1, 4 },
+			new int[] { 0, 4 }
+		}));
+	}
+
+	public int[][] merge(int[][] intervals) {
+		if (intervals.length == 1) {
+			return intervals;
+		}
+		Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
+		List<int[]> result = new ArrayList<>();
+		int[] temp = intervals[0];
+		result.add(temp);
+		for (int[] interval : intervals) {
+			if (temp[1] >= interval[0]) {
+				temp[1] = Math.max(temp[1], interval[1]);
+			} else {
+				temp = interval;
+				result.add(temp);
+			}
+		}
+		return result.toArray(new int[result.size()][]);
+	}
+
+	private static void print(int[][] intervals) {
+		for (int[] array : intervals) {
+			System.out.println(Arrays.toString(array));
+		}
+	}
+
+}
