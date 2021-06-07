7c972442320710bab64601e317fc9e605924d4b2
yongsik <mymyyoo@naver.com>
2021-06-07 12:14:06 +0900

    Add code about Leetcode - Length Of Last Word

---
+++ src/main/java/gracefulsoul/problems/LengthOfLastWord.java
@@ -0,0 +1,26 @@
+package gracefulsoul.problems;
+
+public class LengthOfLastWord {
+
+	// https://leetcode.com/submissions/detail/504201297/
+	public static void main(String[] args) {
+		LengthOfLastWord test = new LengthOfLastWord();
+		System.out.println(test.lengthOfLastWord("Hello World"));
+		System.out.println(test.lengthOfLastWord(" "));
+	}
+	
+	public int lengthOfLastWord(String s) {
+        int result = 0;
+		for (int idx = s.length() - 1; idx >= 0; idx--) {
+        	if (s.charAt(idx) != ' ') {
+        		result++;
+        	} else {
+        		if (result > 0) {
+        			break;
+        		}
+        	}
+        }
+		return result;
+    }
+
+}
