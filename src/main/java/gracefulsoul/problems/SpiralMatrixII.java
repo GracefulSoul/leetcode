39a3be201956f46dff6de67723d478b4cff44821
yongsik <mymyyoo@naver.com>
2021-06-08 12:32:58 +0900

    Add code about Leetcode - Spiral Matrix II

---
+++ src/main/java/gracefulsoul/problems/SpiralMatrixII.java
@@ -0,0 +1,48 @@
+package gracefulsoul.problems;
+
+import java.util.Arrays;
+
+public class SpiralMatrixII {
+
+	public static void main(String[] args) {
+		SpiralMatrixII test = new SpiralMatrixII();
+		print(test.generateMatrix(3));
+		print(test.generateMatrix(1));
+	}
+
+	// https://leetcode.com/submissions/detail/504715101/
+	public int[][] generateMatrix(int n) {
+		int[][] result = new int[n][n];
+		int row = 0;
+		int col = 0;
+		int rowMax = n - 1;
+		int colMax = n - 1;
+		int num = 1;
+		while (num <= n * n) {
+			for (int idx = col; idx <= colMax && num <= n * n; idx++) {
+				result[row][idx] = num++;
+			}
+			for (int idx = row + 1; idx <= rowMax - 1 && num <= n * n; idx++) {
+				result[idx][colMax] = num++;
+			}
+			for (int idx = colMax; idx >= col && num <= n * n; idx--) {
+				result[rowMax][idx] = num++;
+			}
+			for (int idx = rowMax - 1; idx >= row + 1 && num <= n * n; idx--) {
+				result[idx][col] = num++;
+			}
+			row++;
+			col++;
+			rowMax--;
+			colMax--;
+		}
+		return result;
+	}
+
+	private static void print(int[][] matrix) {
+		for (int[] array : matrix) {
+			System.out.println(Arrays.toString(array));
+		}
+	}
+
+}
