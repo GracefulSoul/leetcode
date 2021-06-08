77be51865e0293f2862ce6c6aeca493226697338
yongsik <mymyyoo@naver.com>
2021-06-08 17:34:48 +0900

    Add code about Leetcode - Spiral Matrix II

--- src/main/java/gracefulsoul/problems/SpiralMatrixII.java
+++ src/main/java/gracefulsoul/problems/SpiralMatrixII.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class SpiralMatrixII {
 
-	// https://leetcode.com/submissions/detail/504715101/
+	// https://leetcode.com/submissions/detail/504834978/
 	public static void main(String[] args) {
 		SpiralMatrixII test = new SpiralMatrixII();
 		print(test.generateMatrix(3));
@@ -13,28 +13,24 @@ public class SpiralMatrixII {
 
 	public int[][] generateMatrix(int n) {
 		int[][] result = new int[n][n];
-		int row = 0;
-		int col = 0;
-		int rowMax = n - 1;
-		int colMax = n - 1;
+		int min = 0;
+		int max = n - 1;
 		int num = 1;
 		while (num <= n * n) {
-			for (int idx = col; idx <= colMax && num <= n * n; idx++) {
-				result[row][idx] = num++;
+			for (int idx = min; idx <= max && num <= n * n; idx++) {
+				result[min][idx] = num++;
 			}
-			for (int idx = row + 1; idx <= rowMax - 1 && num <= n * n; idx++) {
-				result[idx][colMax] = num++;
+			for (int idx = min + 1; idx <= max - 1 && num <= n * n; idx++) {
+				result[idx][max] = num++;
 			}
-			for (int idx = colMax; idx >= col && num <= n * n; idx--) {
-				result[rowMax][idx] = num++;
+			for (int idx = max; idx >= min && num <= n * n; idx--) {
+				result[max][idx] = num++;
 			}
-			for (int idx = rowMax - 1; idx >= row + 1 && num <= n * n; idx--) {
-				result[idx][col] = num++;
+			for (int idx = max - 1; idx >= min + 1 && num <= n * n; idx--) {
+				result[idx][min] = num++;
 			}
-			row++;
-			col++;
-			rowMax--;
-			colMax--;
+			min++;
+			max--;
 		}
 		return result;
 	}
