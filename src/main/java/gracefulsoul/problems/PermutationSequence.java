c4d4c259cebc6306f1a1de4b2737fa7903256972
yongsik <mymyyoo@naver.com>
2021-06-09 12:58:45 +0900

    Add code about Leetcode - Permutation Sequence

---
+++ src/main/java/gracefulsoul/problems/PermutationSequence.java
@@ -0,0 +1,38 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutationSequence {
+
+	// https://leetcode.com/submissions/detail/505230381/
+	public static void main(String[] args) {
+		PermutationSequence test = new PermutationSequence();
+		System.out.println(test.getPermutation(3, 3));
+		System.out.println(test.getPermutation(4, 9));
+		System.out.println(test.getPermutation(3, 1));
+	}
+
+	private List<Integer> nums = new ArrayList<>();
+	private int factorial = 1;
+
+	public String getPermutation(int n, int k) {
+		this.initVariable(n);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0, j = k - 1; i < n; i++) {
+			this.factorial /= (n - i);
+			int index = (j / this.factorial);
+			sb.append(this.nums.remove(index));
+			j -= index * this.factorial;
+		}
+		return sb.toString();
+	}
+
+	private void initVariable(int n) {
+		for (int idx = 1; idx <= n; idx++) {
+			this.factorial *= idx;
+			this.nums.add(idx);
+		}
+	}
+
+}
