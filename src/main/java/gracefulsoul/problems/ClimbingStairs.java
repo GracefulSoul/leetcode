0c26ed6b6955b4ee0403e7b3a54fb3ff293ffe50
yongsik <mymyyoo@naver.com>
2021-06-19 16:44:05 +0900

    Add code about Leetcode - Climbing Stairs

--- src/main/java/gracefulsoul/problems/ClimbingStairs.java
+++ src/main/java/gracefulsoul/problems/ClimbingStairs.java
@@ -9,24 +9,24 @@ public class ClimbingStairs {
 	}
 
 	// Use loop (https://leetcode.com/submissions/detail/510029589/)
-//	public int climbStairs(int n) {
-//		int result = 1;
-//		for (int idx = 0, diff = 0; idx < n; idx++) {
-//			result += diff;
-//			diff = result - diff;
-//		}
-//		return result;
-//	}
+	public int climbStairs(int n) {
+		int result = 1;
+		for (int idx = 0, diff = 0; idx < n; idx++) {
+			result += diff;
+			diff = result - diff;
+		}
+		return result;
+	}
 
 	// Use array (https://leetcode.com/submissions/detail/510033864/)
-	public int climbStairs(int n) {
-		int[] fibonacci = new int[n + 1];
-		fibonacci[0] = 1;
-		fibonacci[1] = 2;
-		for (int idx = 2; idx < n; idx++) {
-			fibonacci[idx] = fibonacci[idx - 2] + fibonacci[idx - 1];
-		}
-		return fibonacci[n - 1];
-	}
+//	public int climbStairs(int n) {
+//		int[] fibonacci = new int[n + 1];
+//		fibonacci[0] = 1;
+//		fibonacci[1] = 2;
+//		for (int idx = 2; idx < n; idx++) {
+//			fibonacci[idx] = fibonacci[idx - 2] + fibonacci[idx - 1];
+//		}
+//		return fibonacci[n - 1];
+//	}
 
 }
