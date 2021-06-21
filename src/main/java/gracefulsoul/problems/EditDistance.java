8b67ea66ceb70975b93190f0dcc1d684d28064dc
yongsik <mymyyoo@naver.com>
2021-06-21 15:23:36 +0900

    Add code about Leetcode - Edit Distance

--- src/main/java/gracefulsoul/problems/EditDistance.java
+++ src/main/java/gracefulsoul/problems/EditDistance.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class EditDistance {
 
-	// https://leetcode.com/submissions/detail/510925821/
+	// https://leetcode.com/submissions/detail/510970148/
 	public static void main(String[] args) {
 		EditDistance test = new EditDistance();
 		System.out.println(test.minDistance("horse", "ros"));
@@ -11,12 +11,12 @@ public class EditDistance {
 
 	public int minDistance(String word1, String word2) {
 		int[][] dp = this.initDp(word1.length(), word2.length());
-		for (int i = 1; i <= word1.length(); i++) {
-			for (int j = 1; j <= word2.length(); j++) {
-				if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-					dp[i][j] = dp[i - 1][j - 1];
+		for (int i = 0; i < word1.length(); i++) {
+			for (int j = 0; j < word2.length(); j++) {
+				if (word1.charAt(i) == word2.charAt(j)) {
+					dp[i + 1][j + 1] = dp[i][j];
 				} else {
-					dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+					dp[i + 1][j + 1] = 1 + Math.min(dp[i][j], Math.min(dp[i][j + 1], dp[i + 1][j]));
 				}
 			}
 		}
