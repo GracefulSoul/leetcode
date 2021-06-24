2fea3de07abf4dc8e9b02a3cdec4803f8de0c289
yongsik <mymyyoo@naver.com>
2021-06-25 07:34:14 +0900

    Add code about Leetcode - Minimum Window Substring

--- src/main/java/gracefulsoul/problems/MinimumWindowSubstring.java
+++ src/main/java/gracefulsoul/problems/MinimumWindowSubstring.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class MinimumWindowSubstring {
 
-	// https://leetcode.com/submissions/detail/512739432/
+	// https://leetcode.com/submissions/detail/512748176/
 	public static void main(String[] args) {
 		MinimumWindowSubstring test = new MinimumWindowSubstring();
 		System.out.println(test.minWindow("ADOBECODEBANC", "ABC"));
@@ -11,28 +11,28 @@ public class MinimumWindowSubstring {
 	}
 
 	public String minWindow(String s, String t) {
-		int[] cnt = new int[128];
-		for (char c : t.toCharArray()) {
-			cnt[c]++;
-		}
-		int start = 0;
-		int end = Integer.MAX_VALUE;
-		int counter = t.length();
-		for (int i = 0, j = 0; i < s.length(); i++) {
-			if (cnt[s.charAt(i)]-- > 0) {
-				counter--;
-			}
-			while (counter == 0) {
-				if (i - j + 1 < end) {
-					end = i - j + 1;
-					start = j;
-				}
-				if (++cnt[s.charAt(j++)] > 0) {
-					counter++;
-				}
-			}
-		}
-		return end == Integer.MAX_VALUE ? "" : s.substring(start, start + end);
+		int[] charArray = new int[128];
+        for (char c : t.toCharArray()) {
+        	charArray[c]++;
+        }
+        int start = 0;
+        int length = Integer.MAX_VALUE;
+        int counter = t.length();
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (charArray[s.charAt(i)]-- > 0) {
+            	counter--;
+            }
+            while (counter == 0) {
+                if (i - j + 1 < length) {
+                	length = i - j + 1;
+                	start = j;
+                }
+                if (++charArray[s.charAt(j++)] > 0) {
+                	counter++;
+                }
+            }
+        }
+        return length == Integer.MAX_VALUE ? "" : s.substring(start, start + length);
 	}
 
 }
