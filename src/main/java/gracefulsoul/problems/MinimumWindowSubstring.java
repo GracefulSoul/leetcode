003df6ed060a32b62c6878cea07befdc12c63354
yongsik <mymyyoo@naver.com>
2021-06-25 07:00:03 +0900

    Add code about Leetcode - Minimum Window Substring

--- src/main/java/gracefulsoul/problems/MinimumWindowSubstring.java
+++ src/main/java/gracefulsoul/problems/MinimumWindowSubstring.java
@@ -12,27 +12,27 @@ public class MinimumWindowSubstring {
 
 	public String minWindow(String s, String t) {
 		int[] cnt = new int[128];
-        for (char c : t.toCharArray()) {
-        	cnt[c]++;
-        }
-        int start = 0;
-        int end = Integer.MAX_VALUE;
-        int counter = t.length();
-        for (int i = 0, j = 0; i < s.length(); i++) {
-            if (cnt[s.charAt(i)]-- > 0) {
-            	counter--;
-            }
-            while (counter == 0) {
-                if (i - j + 1 < end) {
-                	end = i - j + 1;
-                	start = j;
-                }
-                if (++cnt[s.charAt(j++)] > 0) {
-                	counter++;
-                }
-            }
-        }
-        return end == Integer.MAX_VALUE ? "" : s.substring(start, start + end);
+		for (char c : t.toCharArray()) {
+			cnt[c]++;
+		}
+		int start = 0;
+		int end = Integer.MAX_VALUE;
+		int counter = t.length();
+		for (int i = 0, j = 0; i < s.length(); i++) {
+			if (cnt[s.charAt(i)]-- > 0) {
+				counter--;
+			}
+			while (counter == 0) {
+				if (i - j + 1 < end) {
+					end = i - j + 1;
+					start = j;
+				}
+				if (++cnt[s.charAt(j++)] > 0) {
+					counter++;
+				}
+			}
+		}
+		return end == Integer.MAX_VALUE ? "" : s.substring(start, start + end);
 	}
 
 }
