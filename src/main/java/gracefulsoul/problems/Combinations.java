ce5b19e3f1d0d7e8dc7960f8a874ecd1142fe7b8
yongsik <mymyyoo@naver.com>
2021-06-26 07:39:58 +0900

    Add code about Leetcode - Combinations

---
+++ src/main/java/gracefulsoul/problems/Combinations.java
@@ -0,0 +1,32 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+
+	// https://leetcode.com/submissions/detail/513189707/
+	public static void main(String[] args) {
+		Combinations test = new Combinations();
+		System.out.println(test.combine(4, 2));
+		System.out.println(test.combine(1, 1));
+	}
+
+	public List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> result = new ArrayList<>();
+		if (k == 0) {
+			result.add(new ArrayList<Integer>());
+			return result;
+		} else if (k > n || k < 0) {
+			return result;
+		} else {
+			result = this.combine(n - 1, k - 1);
+			for (List<Integer> list : result) {
+				list.add(n);
+			}
+			result.addAll(this.combine(n - 1, k));
+			return result;
+		}
+	}
+
+}
