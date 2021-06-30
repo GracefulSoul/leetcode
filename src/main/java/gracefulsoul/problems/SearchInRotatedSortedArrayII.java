adb07589c6926d475b04d54fa589e4afcaeb6555
yongsik <mymyyoo@naver.com>
2021-06-30 12:32:21 +0900

    Add code about Leetcode - Search in Rotated Sorted Array II

--- src/main/java/gracefulsoul/problems/SearchInRotatedSortedArrayII.java
+++ src/main/java/gracefulsoul/problems/SearchInRotatedSortedArrayII.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class SearchInRotatedSortedArrayII {
 
-	// https://leetcode.com/submissions/detail/515183219/
+	// https://leetcode.com/submissions/detail/515187191/
 	public static void main(String[] args) {
 		SearchInRotatedSortedArrayII test = new SearchInRotatedSortedArrayII();
 		System.out.println(test.search(new int[] { 2, 5, 6, 0, 0, 1, 2 }, 0));
@@ -10,8 +10,8 @@ public class SearchInRotatedSortedArrayII {
 	}
 
 	public boolean search(int[] nums, int target) {
-		for (int idx = 0; idx < nums.length; idx++) {
-			if (nums[idx] == target) {
+		for (int num : nums) {
+			if (num == target) {
 				return true;
 			}
 		}
