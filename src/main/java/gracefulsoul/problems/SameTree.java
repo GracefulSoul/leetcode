502982c25e8117821d3b554a0ceac9754cbb140b
yongsik <mymyyoo@naver.com>
2021-07-20 12:50:04 +0900

    Add code about Leetcode - Same Tree

---
+++ src/main/java/gracefulsoul/problems/SameTree.java
@@ -0,0 +1,26 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.object.TreeNode;
+
+public class SameTree {
+
+	// https://leetcode.com/submissions/detail/525281341/
+	public static void main(String[] args) {
+		SameTree test = new SameTree();
+		System.out.println(test.isSameTree(new TreeNode(1, new TreeNode(2), new TreeNode(3)), new TreeNode(1, new TreeNode(2), new TreeNode(3))));
+		System.out.println(test.isSameTree(new TreeNode(1, new TreeNode(2), null), new TreeNode(1, null, new TreeNode(2))));
+	}
+
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		} else if (p == null || q == null) {
+			return false;
+		} else if (p.val == q.val) {
+			return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
+		} else {
+			return false;
+		}
+	}
+
+}
