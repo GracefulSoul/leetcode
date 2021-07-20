8fb41c6dbaa17c0e3d3f5bb7bf42436b1d8e416b
yongsik <mymyyoo@naver.com>
2021-07-21 08:14:05 +0900

    Add code about Leetcode - Symmetric Tree

---
+++ src/main/java/gracefulsoul/problems/SymmetricTree.java
@@ -0,0 +1,30 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.object.TreeNode;
+
+public class SymmetricTree {
+
+	// https://leetcode.com/submissions/detail/525723018/
+	public static void main(String[] args) {
+		SymmetricTree test = new SymmetricTree();
+		System.out.println(test.isSymmetric(new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)))));
+		System.out.println(test.isSymmetric(new TreeNode(1, new TreeNode(2, null, new TreeNode(3)), new TreeNode(2, null, new TreeNode(3)))));
+	}
+
+	public boolean isSymmetric(TreeNode root) {
+		return this.recursive(root.left, root.right);
+	}
+
+	private boolean recursive(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		} else if (p == null || q == null) {
+			return false;
+		} else if (p.val == q.val) {
+			return this.recursive(p.left, q.right) && this.recursive(p.right, q.left);
+		} else {
+			return false;
+		}
+	}
+
+}
