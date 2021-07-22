4128fda89900763cbe79b73170f9b04afaa1c24e
yongsik <mymyyoo@naver.com>
2021-07-23 08:28:39 +0900

    Add code about Leetcode - Binary Tree Zigzag Level Order Traversal

--- src/main/java/gracefulsoul/problems/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/main/java/gracefulsoul/problems/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,20 +1,40 @@
 package gracefulsoul.problems;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import gracefulsoul.object.TreeNode;
 
 public class BinaryTreeZigzagLevelOrderTraversal {
 
+	// https://leetcode.com/submissions/detail/526784911/
 	public static void main(String[] args) {
 		BinaryTreeZigzagLevelOrderTraversal test = new BinaryTreeZigzagLevelOrderTraversal();
-		test.zigzagLevelOrder(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7))));
-		test.zigzagLevelOrder(new TreeNode(1));
-		test.zigzagLevelOrder(new TreeNode());
+		System.out.println(test.zigzagLevelOrder(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
+		System.out.println(test.zigzagLevelOrder(new TreeNode(1)));
+		System.out.println(test.zigzagLevelOrder(new TreeNode()));
 	}
-	
+
 	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        
-    }
+		List<List<Integer>> result = new ArrayList<>();
+		this.recursive(root, result, 0);
+		return result;
+	}
+
+	private void recursive(TreeNode treeNode, List<List<Integer>> result, int level) {
+		if (treeNode == null) {
+			return;
+		}
+		if (result.size() < level + 1) {
+			result.add(new ArrayList<>());
+		}
+		if (level % 2 == 0) {
+			result.get(level).add(treeNode.val);
+		} else {
+			result.get(level).add(0, treeNode.val);
+		}
+		this.recursive(treeNode.left, result, level + 1);
+		this.recursive(treeNode.right, result, level + 1);
+	}
 
 }
