8cc7cfca324b6eda444ad2c16bb2938a222c20d3
yongsik <mymyyoo@naver.com>
2021-07-26 15:11:26 +0900

    Add code about Leetcode - Construct Binary Tree from Preorder and Inorder Traversal

--- src/main/java/gracefulsoul/problems/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/gracefulsoul/problems/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -11,7 +11,7 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
 	public static void main(String[] args) {
 		ConstructBinaryTreeFromPreorderAndInorderTraversal test = new ConstructBinaryTreeFromPreorderAndInorderTraversal();
 		print(test.buildTree(new int[] { 3, 9, 20, 15, 7 }, new int[] { 9, 3, 15, 20, 7 }), true, false);
-
+		print(test.buildTree(new int[] { -1 }, new int[] { -1 }), true, false);
 	}
 
 	public TreeNode buildTree(int[] preorder, int[] inorder) {
@@ -51,6 +51,9 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
 		}
 		if (isLeft) {
 			System.out.print(", ");
+		}
+		if (isRoot) {
+			System.out.println();
 		}
 	}
 
