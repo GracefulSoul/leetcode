8a3f7108f715d059fe17aac88bb903224e3dd1eb
yongsik <mymyyoo@naver.com>
2021-07-26 15:08:43 +0900

    Add code about Leetcode - Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/main/java/gracefulsoul/problems/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,61 @@
+package gracefulsoul.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import gracefulsoul.object.TreeNode;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+
+	// https://leetcode.com/submissions/detail/528433792/
+	public static void main(String[] args) {
+		ConstructBinaryTreeFromPreorderAndInorderTraversal test = new ConstructBinaryTreeFromPreorderAndInorderTraversal();
+		print(test.buildTree(new int[] { 3, 9, 20, 15, 7 }, new int[] { 9, 3, 15, 20, 7 }), true, false);
+
+	}
+
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		Map<Integer, Integer> inorderMap = new HashMap<>();
+		for (int idx = 0; idx < inorder.length; idx++) {
+			inorderMap.put(inorder[idx], idx);
+		}
+		return this.recursive(preorder, inorderMap, 0, preorder.length - 1, 0, inorder.length - 1);
+	}
+
+	public TreeNode recursive(int[] preorder, Map<Integer, Integer> inorderMap, int preStart, int preEnd, int inStart, int inEnd) {
+		if (preStart > preEnd || inStart > inEnd) {
+			return null;
+		}
+		TreeNode treeNode = new TreeNode(preorder[preStart]);
+		int inRoot = inorderMap.get(treeNode.val);
+		int numsLeft = inRoot - inStart;
+		treeNode.left = this.recursive(preorder, inorderMap, preStart + 1, preStart + numsLeft, inStart, inRoot - 1);
+		treeNode.right = this.recursive(preorder, inorderMap, preStart + numsLeft + 1, preEnd, inRoot + 1, inEnd);
+		return treeNode;
+	}
+
+	private static void print(TreeNode treeNode, boolean isRoot, boolean isLeft) {
+		if (treeNode != null) {
+			if (!isRoot) {
+				System.out.print("(");
+			}
+			print(treeNode.left, false, true);
+			print(treeNode.val);
+			System.out.print(", ");
+			print(treeNode.right, false, false);
+			if (!isRoot) {
+				System.out.print(")");
+			}
+		} else {
+			print("null");
+		}
+		if (isLeft) {
+			System.out.print(", ");
+		}
+	}
+
+	private static void print(Object val) {
+		System.out.print("(" + val + ")");
+	}
+
+}
