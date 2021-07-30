90de343c6eab08de4a9d94668a3adb5dce154ff7
yongsik <mymyyoo@naver.com>
2021-07-30 14:20:31 +0900

    Add code about Leetcode - Convert Sorted List to Binary Search Tree

---
+++ src/main/java/gracefulsoul/problems/ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,68 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.object.ListNode;
+import gracefulsoul.object.TreeNode;
+
+public class ConvertSortedListToBinarySearchTree {
+
+	// https://leetcode.com/submissions/detail/530467502/
+	public static void main(String[] args) {
+		ConvertSortedListToBinarySearchTree test = new ConvertSortedListToBinarySearchTree();
+		print(test.sortedListToBST(new ListNode(-10, new ListNode(-3, new ListNode(0, new ListNode(5, new ListNode(9)))))), true, false);
+		print(test.sortedListToBST(new ListNode()), true, false);
+		print(test.sortedListToBST(new ListNode(0)), true, false);
+		print(test.sortedListToBST(new ListNode(1, new ListNode(3))), true, false);
+	}
+
+	public TreeNode sortedListToBST(ListNode head) {
+		if (head == null) {
+			return null;
+		} else {
+			return this.recursive(head, null);
+		}
+	}
+
+	public TreeNode recursive(ListNode head, ListNode tail) {
+		if (head == tail) {
+			return null;
+		}
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != tail && fast.next != tail) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		TreeNode teeNode = new TreeNode(slow.val);
+		teeNode.left = this.recursive(head, slow);
+		teeNode.right = this.recursive(slow.next, tail);
+		return teeNode;
+	}
+
+	private static void print(TreeNode treeNode, boolean isRoot, boolean isLeft) {
+		if (treeNode != null) {
+			if (!isRoot) {
+				System.out.print("(");
+			}
+			print(treeNode.left, false, true);
+			print(treeNode.val);
+			System.out.print(", ");
+			print(treeNode.right, false, false);
+			if (!isRoot) {
+				System.out.print(")");
+			}
+		} else {
+			print("null");
+		}
+		if (isLeft) {
+			System.out.print(", ");
+		}
+		if (isRoot) {
+			System.out.println();
+		}
+	}
+
+	private static void print(Object val) {
+		System.out.print("(" + val + ")");
+	}
+
+}
