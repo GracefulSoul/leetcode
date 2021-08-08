331e491cbfd4030c87494267dca68f96d54384e0
yongsik <mymyyoo@naver.com>
2021-08-08 16:07:41 +0900

    Add code about Leetcode - Pascal's Triangle

--- src/main/java/gracefulsoul/problems/PascalsTriangle.java
+++ src/main/java/gracefulsoul/problems/PascalsTriangle.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class PascalsTriangle {
 
-	// https://leetcode.com/submissions/detail/535118115/
+	// https://leetcode.com/submissions/detail/535120781/
 	public static void main(String[] args) {
 		PascalsTriangle test = new PascalsTriangle();
 		System.out.println(test.generate(5));
@@ -14,26 +14,21 @@ public class PascalsTriangle {
 
 	public List<List<Integer>> generate(int numRows) {
 		List<List<Integer>> result = new ArrayList<>();
-		this.recursive(result, numRows);
-		return result;
-	}
-
-	private void recursive(List<List<Integer>> result, int numRows) {
-		List<Integer> list = new ArrayList<>();
-		list.add(1);
-		if (result.size() > 1) {
-			List<Integer> pre = result.get(result.size() - 1);
-			for (int i = 1; i < result.size(); i++) {
-				list.add(pre.get(i - 1) + pre.get(i));
-			}
-		}
-		if (result.size() > 0) {
+		while (result.size() < numRows) {
+			List<Integer> list = new ArrayList<>();
 			list.add(1);
+			if (result.size() > 1) {
+				List<Integer> pre = result.get(result.size() - 1);
+				for (int i = 1; i < result.size(); i++) {
+					list.add(pre.get(i - 1) + pre.get(i));
+				}
+			}
+			if (result.size() > 0) {
+				list.add(1);
+			}
+			result.add(list);
 		}
-		result.add(list);
-		if (result.size() < numRows) {
-			this.recursive(result, numRows);
-		}
+		return result;
 	}
 
 }
