f08a65d706f4c3875b7d845da7b5d0b36b5a4e1e
yongsik <mymyyoo@naver.com>
2021-08-08 15:46:26 +0900

    Add code about Leetcode - Pascal's Triangle

--- src/main/java/gracefulsoul/problems/PascalsTriangle.java
+++ src/main/java/gracefulsoul/problems/PascalsTriangle.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class PascalsTriangle {
 
-	// https://leetcode.com/submissions/detail/535108653/
+	// https://leetcode.com/submissions/detail/535112209/
 	public static void main(String[] args) {
 		PascalsTriangle test = new PascalsTriangle();
 		System.out.println(test.generate(5));
@@ -14,25 +14,25 @@ public class PascalsTriangle {
 
 	public List<List<Integer>> generate(int numRows) {
 		List<List<Integer>> result = new ArrayList<>();
-		this.recursive(result, numRows, 1);
+		this.recursive(result, numRows);
 		return result;
 	}
 
-	private void recursive(List<List<Integer>> result, int numRows, int loopCnt) {
+	private void recursive(List<List<Integer>> result, int numRows) {
 		List<Integer> list = new ArrayList<Integer>();
 		list.add(1);
-		if (loopCnt > 2) {
-			List<Integer> pre = result.get(loopCnt - 2);
-			for (int i = 1; i < loopCnt - 1; i++) {
+		if (result.size() > 1) {
+			List<Integer> pre = result.get(result.size() - 1);
+			for (int i = 1; i < result.size(); i++) {
 				list.add(pre.get(i - 1) + pre.get(i));
 			}
 		}
-		if (loopCnt / 2 > 0) {
+		if ((result.size() + 1) / 2 > 0) {
 			list.add(1);
 		}
 		result.add(list);
-		if (loopCnt < numRows) {
-			this.recursive(result, numRows, loopCnt + 1);
+		if (result.size() < numRows) {
+			this.recursive(result, numRows);
 		}
 	}
 
