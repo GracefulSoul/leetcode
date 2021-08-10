99f1299219e0fa935d864e64e8f0cf4a673c0df6
yongsik <mymyyoo@naver.com>
2021-08-10 11:13:19 +0900

    Add code about Leetcode - Triangle

--- src/main/java/gracefulsoul/problems/Triangle.java
+++ src/main/java/gracefulsoul/problems/Triangle.java
@@ -38,9 +38,9 @@ public class Triangle {
 	public int minimumTotal(List<List<Integer>> triangle) {
 		int[] dp = new int[triangle.size() + 1];
 		for (int i = triangle.size() - 1; i >= 0; i--) {
-			List<Integer> temp = triangle.get(i);
-			for (int j = 0; j < temp.size(); j++) {
-				dp[j] = Math.min(dp[j], dp[j + 1]) + temp.get(j);
+			List<Integer> row = triangle.get(i);
+			for (int j = 0; j < row.size(); j++) {
+				dp[j] = Math.min(dp[j], dp[j + 1]) + row.get(j);
 			}
 		}
 		return dp[0];
