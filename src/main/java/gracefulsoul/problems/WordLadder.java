5378f2d6ba9bd6adc6f1e37043449137ee6c2a3b
yongsik <mymyyoo@naver.com>
2021-08-16 14:27:34 +0900

    Add codes about Leetcode - Word Ladder

--- src/main/java/gracefulsoul/problems/WordLadder.java
+++ src/main/java/gracefulsoul/problems/WordLadder.java
@@ -17,25 +17,25 @@ public class WordLadder {
 	}
 
 	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-		Set<String> dict = new HashSet<>(wordList);
+		Set<String> wordSet = new HashSet<>(wordList);
 		Queue<String> queue = new LinkedList<>();
 		queue.add(beginWord);
 		int length = 1;
 		while (!queue.isEmpty()) {
-			int size = queue.size();
-			for (int i = 0; i < size; i++) {
+			int queueSize = queue.size();
+			for (int i = 0; i < queueSize; i++) {
 				char[] wordCharArr = queue.poll().toCharArray();
 				for (int j = 0; j < wordCharArr.length; j++) {
 					char temp = wordCharArr[j];
 					for (char chr = 'a'; chr <= 'z'; chr++) {
 						wordCharArr[j] = chr;
 						String dest = new String(wordCharArr);
-						if (dict.contains(dest)) {
+						if (wordSet.contains(dest)) {
 							if (dest.equals(endWord)) {
 								return length + 1;
 							}
 							queue.add(dest);
-							dict.remove(dest);
+							wordSet.remove(dest);
 						}
 					}
 					wordCharArr[j] = temp;
