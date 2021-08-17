7e94dc9bde480656612ea7a14ab50248b8b1e6ad
yongsik <mymyyoo@naver.com>
2021-08-17 12:32:38 +0900

    Add code about Leetcode - Longest Consecutive Sequence

--- src/main/java/gracefulsoul/problems/LongestConsecutiveSequence.java
+++ src/main/java/gracefulsoul/problems/LongestConsecutiveSequence.java
@@ -20,7 +20,7 @@ public class LongestConsecutiveSequence {
 		int sum = 1;
 		int count = 1;
 		for (int i = 1; i < nums.length; i++) {
-			if (nums[i] == nums[i - 1]) {
+			if (nums[i - 1] == nums[i]) {
 				continue;
 			} else if (nums[i] == curr + 1) {
 				curr++;
