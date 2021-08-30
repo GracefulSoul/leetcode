3cec2df39952931ac3c3bb3f2edf5404714398dd
yongsik <mymyyoo@naver.com>
2021-08-30 12:38:48 +0900

    Add code about Leetcode - Linked List Cycle

---
+++ src/main/java/gracefulsoul/problems/LinkedListCycle.java
@@ -0,0 +1,35 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.object.ListNode;
+
+public class LinkedListCycle {
+
+	// https://leetcode.com/submissions/detail/546453516/
+	public static void main(String[] args) {
+		LinkedListCycle test = new LinkedListCycle();
+		ListNode listNode1 = new ListNode(2);
+		listNode1.next = new ListNode(0, new ListNode(-4, listNode1));
+		System.out.println(test.hasCycle(new ListNode(3, listNode1)));
+		ListNode listNode2 = new ListNode(1);
+		listNode2.next = new ListNode(2, listNode2);
+		System.out.println(test.hasCycle(listNode2));
+		System.out.println(test.hasCycle(new ListNode(1)));
+	}
+
+	public boolean hasCycle(ListNode head) {
+		if (head == null) {
+			return false;
+		}
+		ListNode fast = head;
+		ListNode slow = head;
+		while (fast.next != null && fast.next.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+			if (slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
