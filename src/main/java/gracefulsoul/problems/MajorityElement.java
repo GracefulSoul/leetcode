6cb5dda5ed6e4db8b5c9a37c4e79b9c6ade8ba0d
yongsik <mymyyoo@naver.com>
2021-09-20 08:04:54 +0900

    Add code about Leetcode - Majority Element

---
+++ src/main/java/gracefulsoul/problems/MajorityElement.java
@@ -0,0 +1,27 @@
+package gracefulsoul.problems;
+
+public class MajorityElement {
+
+	public static void main(String[] args) {
+		MajorityElement test = new MajorityElement();
+		System.out.println(test.majorityElement(new int[] { 3, 2, 3 }));
+		System.out.println(test.majorityElement(new int[] { 2, 2, 1, 1, 1, 2, 2 }));
+	}
+
+	public int majorityElement(int[] nums) {
+		int result = 0;
+		int count = 0;
+		for (int num : nums) {
+			if (count == 0) {
+				result = num;
+			}
+			if (num != result) {
+				count--;
+			} else {
+				count++;
+			}
+		}
+		return result;
+	}
+
+}
