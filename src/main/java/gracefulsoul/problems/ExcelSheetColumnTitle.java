5dffd0c0aa2b313cc8d9dcbe7c6abfe6be82c54f
yongsik <mymyyoo@naver.com>
2021-09-19 10:15:57 +0900

    Add code about Leetcode - Excel Sheet Column Title

---
+++ src/main/java/gracefulsoul/problems/ExcelSheetColumnTitle.java
@@ -0,0 +1,27 @@
+package gracefulsoul.problems;
+
+public class ExcelSheetColumnTitle {
+
+	// https://leetcode.com/submissions/detail/557233944/
+	public static void main(String[] args) {
+		ExcelSheetColumnTitle test = new ExcelSheetColumnTitle();
+		System.out.println(test.convertToTitle(1));
+		System.out.println(test.convertToTitle(28));
+		System.out.println(test.convertToTitle(701));
+		System.out.println(test.convertToTitle(2147483647));
+	}
+	
+	public String convertToTitle(int columnNumber) {
+		StringBuilder result = new StringBuilder();
+        while (columnNumber > 0) {
+        	columnNumber--;
+        	result.append((char)('A' + (columnNumber % 26)));
+        	columnNumber /= 26;
+        }
+        if (result.length() > 1) {
+        	result.reverse();
+        }
+		return result.toString();
+    }
+
+}
