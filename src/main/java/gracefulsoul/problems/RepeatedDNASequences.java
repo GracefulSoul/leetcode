933cdad1f085c8b92464c9f4b85ddb1b0463fa34
yongsik <mymyyoo@naver.com>
2021-09-27 12:10:11 +0900

    Add code about Leetcode - Repeated DNA Sequences

---
+++ src/main/java/gracefulsoul/problems/RepeatedDNASequences.java
@@ -0,0 +1,29 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RepeatedDNASequences {
+
+	// https://leetcode.com/submissions/detail/561605294/
+	public static void main(String[] args) {
+		RepeatedDNASequences test = new RepeatedDNASequences();
+		System.out.println(test.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
+		System.out.println(test.findRepeatedDnaSequences("AAAAAAAAAAAAA"));
+	}
+
+	public List<String> findRepeatedDnaSequences(String s) {
+		Set<String> result = new HashSet<>();
+		Set<String> temp = new HashSet<>();
+		for (int idx = 0; idx + 9 < s.length(); idx++) {
+			String sequences = s.substring(idx, idx + 10);
+			if (!temp.add(sequences)) {
+				result.add(sequences);
+			}
+		}
+		return new ArrayList<>(result);
+	}
+
+}
