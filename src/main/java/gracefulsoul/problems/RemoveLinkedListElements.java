30652d00a3137d457ac8621a749775ede5f8ad60
yongsik <mymyyoo@naver.com>
2021-10-07 08:20:33 +0900

    Add code about Leetcode - Remove Linked List Elements

--- src/main/java/gracefulsoul/problems/RemoveLinkedListElements.java
+++ src/main/java/gracefulsoul/problems/RemoveLinkedListElements.java
@@ -4,6 +4,7 @@ import gracefulsoul.object.ListNode;
 
 public class RemoveLinkedListElements {
 
+	// https://leetcode.com/submissions/detail/567033188/
 	public static void main(String[] args) {
 		RemoveLinkedListElements test = new RemoveLinkedListElements();
 		print(test.removeElements(new ListNode(1, new ListNode(2, new ListNode(6, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))))), 6));
