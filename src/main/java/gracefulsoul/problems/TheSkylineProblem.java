de5b54317b04820dac01d6837491025a39d19257
yongsik <mymyyoo@naver.com>
2021-10-24 13:25:26 +0900

    Add code about Leetcode - The Skyline Problem

--- src/main/java/gracefulsoul/problems/TheSkylineProblem.java
+++ src/main/java/gracefulsoul/problems/TheSkylineProblem.java
@@ -7,7 +7,7 @@ import java.util.Queue;
 
 public class TheSkylineProblem {
 
-	// https://leetcode.com/submissions/detail/576218395/
+	// https://leetcode.com/submissions/detail/576219670/
 	public static void main(String[] args) {
 		TheSkylineProblem test = new TheSkylineProblem();
 		System.out.println(test.getSkyline(new int[][] {
@@ -27,36 +27,36 @@ public class TheSkylineProblem {
 		List<List<Integer>> result = new ArrayList<>();
 		Queue<int[]> queue = new PriorityQueue<>((a, b) -> b[2] - a[2]);
 		int index = 0;
-		int[] point = null;
-		while (point != null || index < buildings.length) {
-			if (point == null) {
-				point = buildings[index];
-				this.addPoint(result, point[0], point[2]);
-			} else if (index < buildings.length && buildings[index][0] <= point[1]) {
-				if (buildings[index][2] > point[2]) {
-					if (buildings[index][0] == point[0]) {
+		int[] building = null;
+		while (building != null || index < buildings.length) {
+			if (building == null) {
+				building = buildings[index];
+				this.addPoint(result, building[0], building[2]);
+			} else if (index < buildings.length && buildings[index][0] <= building[1]) {
+				if (buildings[index][2] > building[2]) {
+					if (buildings[index][0] == building[0]) {
 						result.remove(result.size() - 1);
 					}
-					if (buildings[index][1] <= point[1]) {
-						queue.add(point);
+					if (buildings[index][1] <= building[1]) {
+						queue.add(building);
 					}
-					point = buildings[index];
-					this.addPoint(result, point[0], point[2]);
-				} else if (buildings[index][1] > point[1]) {
+					building = buildings[index];
+					this.addPoint(result, building[0], building[2]);
+				} else if (buildings[index][1] > building[1]) {
 					queue.add(buildings[index]);
 				}
 				index++;
 			} else {
 				int[] lower = queue.poll();
-				while (lower != null && lower[1] <= point[1]) {
+				while (lower != null && lower[1] <= building[1]) {
 					lower = queue.poll();
 				}
 				if (lower == null) {
-					this.addPoint(result, point[1], 0);
-				} else if (lower[2] < point[2]) {
-					this.addPoint(result, point[1], lower[2]);
+					this.addPoint(result, building[1], 0);
+				} else if (lower[2] < building[2]) {
+					this.addPoint(result, building[1], lower[2]);
 				}
-				point = lower;
+				building = lower;
 			}
 		}
 		return result;
