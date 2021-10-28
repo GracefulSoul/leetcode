7d3cfcaff552fa271a4a54c668011529a0eaab96
yongsik <mymyyoo@naver.com>
2021-10-28 15:53:25 +0900

    Add code about Leetcode - Count Complete Tree Nodes

--- src/main/java/gracefulsoul/problems/CountCompleteTreeNodes.java
+++ src/main/java/gracefulsoul/problems/CountCompleteTreeNodes.java
@@ -4,7 +4,7 @@ import gracefulsoul.object.TreeNode;
 
 public class CountCompleteTreeNodes {
 
-	// https://leetcode.com/submissions/detail/578424222/
+	// https://leetcode.com/submissions/detail/578429714/
 	public static void main(String[] args) {
 		CountCompleteTreeNodes test = new CountCompleteTreeNodes();
 		System.out.println(test.countNodes(new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6), null))));
@@ -19,19 +19,19 @@ public class CountCompleteTreeNodes {
 
 	public int countNodes(TreeNode root) {
 		int depth = this.getDepth(root);
-		if (depth == 0) {
+		if (depth == -1) {
 			return 0;
 		} else {
 			if (this.getDepth(root.right) == depth - 1) {
-				return (1 << (depth - 1)) + this.countNodes(root.right);
+				return (1 << depth) + this.countNodes(root.right);
 			} else {
-				return (1 << (depth - 2)) + this.countNodes(root.left);
+				return (1 << (depth - 1)) + this.countNodes(root.left);
 			}
 		}
 	}
 
 	private int getDepth(TreeNode root) {
-		return root == null ? 0 : 1 + this.getDepth(root.left);
+		return root == null ? -1 : 1 + this.getDepth(root.left);
 	}
 
 }
