75a9e4b94ce41926de40155cb6040680969e8d64
yongsik <mymyyoo@naver.com>
2021-10-29 09:49:23 +0900

    Add code about Leetcode - Rectangle Area

--- src/main/java/gracefulsoul/problems/RectangleArea.java
+++ src/main/java/gracefulsoul/problems/RectangleArea.java
@@ -12,11 +12,11 @@ public class RectangleArea {
 	public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
 		int x = Math.min(ax2, bx2) - Math.max(ax1, bx1);
 		int y = Math.min(ay2, by2) - Math.max(ay1, by1);
-		int union = 0;
 		if (x > 0 && y > 0) {
-			union = x * y;
+			return ((ax2 - ax1) * (ay2 - ay1)) + ((bx2 - bx1) * (by2 - by1)) - (x * y);
+		} else {
+			return ((ax2 - ax1) * (ay2 - ay1)) + ((bx2 - bx1) * (by2 - by1));
 		}
-		return ((ax2 - ax1) * (ay2 - ay1)) + ((bx2 - bx1) * (by2 - by1)) - union;
 	}
 
 }
