c408a7edc69891b6b9e8d401ee928ad06f5b44f6
yongsik <mymyyoo@naver.com>
2021-11-10 10:29:42 +0900

    Add code about Leetcode - Palindrome Linked List

---
+++ src/main/java/gracefulsoul/problems/PalindromeLinkedList.java
@@ -0,0 +1,38 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.object.ListNode;
+
+public class PalindromeLinkedList {
+
+	// https://leetcode.com/submissions/detail/584756564/
+	public static void main(String[] args) {
+		PalindromeLinkedList test = new PalindromeLinkedList();
+		System.out.println(test.isPalindrome(new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))))));
+		System.out.println(test.isPalindrome(new ListNode(1, new ListNode(2))));
+	}
+
+	public boolean isPalindrome(ListNode head) {
+		if (head == null) {
+			return true;
+		}
+		ListNode fast = head;
+		ListNode slow = head;
+		ListNode prev = null;
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			ListNode temp = prev;
+			prev = slow;
+			slow = slow.next;
+			prev.next = temp;
+		}
+		if (fast != null) {
+			slow = slow.next;
+		}
+		while (prev != null && prev.val == slow.val) {
+			slow = slow.next;
+			prev = prev.next;
+		}
+		return prev == null;
+	}
+
+}
