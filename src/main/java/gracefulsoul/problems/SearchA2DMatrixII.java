32709b22714eeb1cbd531692a2e10f543ab9906c
yongsik <mymyyoo@naver.com>
2021-11-17 00:21:57 +0900

    Add code about Leetcode - Search a 2D Matrix II

--- src/main/java/gracefulsoul/problems/SearchA2DMatrixII.java
+++ src/main/java/gracefulsoul/problems/SearchA2DMatrixII.java
@@ -5,22 +5,15 @@ public class SearchA2DMatrixII {
 	// https://leetcode.com/submissions/detail/588101792/
 	public static void main(String[] args) {
 		SearchA2DMatrixII test = new SearchA2DMatrixII();
-		int[][] matrix1 = new int[][] {
+		int[][] matrix = new int[][] {
 			new int[] { 1, 4, 7, 11, 15 },
 			new int[] { 2, 5, 8, 12, 19 },
 			new int[] { 3, 6, 9, 16, 22 },
 			new int[] { 10, 13, 14, 17, 24 },
 			new int[] { 18, 21, 23, 26, 30 }
 		};
-		System.out.println(test.searchMatrix(matrix1, 5));
-		int[][] matrix2 = new int[][] {
-			new int[] { 1, 4, 7, 11, 15 },
-			new int[] { 2, 5, 8, 12, 19 },
-			new int[] { 3, 6, 9, 16, 22 },
-			new int[] { 10, 13, 14, 17, 24 },
-			new int[] { 18, 21, 23, 26, 30 }
-		};
-		System.out.println(test.searchMatrix(matrix2, 20));
+		System.out.println(test.searchMatrix(matrix, 5));
+		System.out.println(test.searchMatrix(matrix, 20));
 	}
 
 	public boolean searchMatrix(int[][] matrix, int target) {
