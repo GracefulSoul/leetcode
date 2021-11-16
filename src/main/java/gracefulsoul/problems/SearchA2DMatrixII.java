24aecf11bf374b326e6409887054bd7b3c4810f7
yongsik <mymyyoo@naver.com>
2021-11-17 00:19:36 +0900

    Add code about Leetcode - Search a 2D Matrix II

---
+++ src/main/java/gracefulsoul/problems/SearchA2DMatrixII.java
@@ -0,0 +1,41 @@
+package gracefulsoul.problems;
+
+public class SearchA2DMatrixII {
+
+	// https://leetcode.com/submissions/detail/588101792/
+	public static void main(String[] args) {
+		SearchA2DMatrixII test = new SearchA2DMatrixII();
+		int[][] matrix1 = new int[][] {
+			new int[] { 1, 4, 7, 11, 15 },
+			new int[] { 2, 5, 8, 12, 19 },
+			new int[] { 3, 6, 9, 16, 22 },
+			new int[] { 10, 13, 14, 17, 24 },
+			new int[] { 18, 21, 23, 26, 30 }
+		};
+		System.out.println(test.searchMatrix(matrix1, 5));
+		int[][] matrix2 = new int[][] {
+			new int[] { 1, 4, 7, 11, 15 },
+			new int[] { 2, 5, 8, 12, 19 },
+			new int[] { 3, 6, 9, 16, 22 },
+			new int[] { 10, 13, 14, 17, 24 },
+			new int[] { 18, 21, 23, 26, 30 }
+		};
+		System.out.println(test.searchMatrix(matrix2, 20));
+	}
+
+	public boolean searchMatrix(int[][] matrix, int target) {
+		int col = 0;
+		int row = matrix.length - 1;
+		while (col <= matrix[0].length - 1 && row >= 0) {
+			if (target == matrix[row][col]) {
+				return true;
+			} else if (target < matrix[row][col]) {
+				row--;
+			} else if (target > matrix[row][col]) {
+				col++;
+			}
+		}
+		return false;
+	}
+
+}
