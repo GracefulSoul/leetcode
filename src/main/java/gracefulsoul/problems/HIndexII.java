4e655c43e2690bd65b1f6568c0c0e30ba9b027a6
yongsik <mymyyoo@naver.com>
2021-11-28 13:43:40 +0900

    Add blog about Leetcode - H-Index II

---
+++ src/main/java/gracefulsoul/problems/HIndexII.java
@@ -0,0 +1,30 @@
+package gracefulsoul.problems;
+
+public class HIndexII {
+
+	// https://leetcode.com/submissions/detail/593772514/
+	public static void main(String[] args) {
+		HIndexII test = new HIndexII();
+		System.out.println(test.hIndex(new int[] { 0, 1, 3, 5, 6 }));
+		System.out.println(test.hIndex(new int[] { 1, 2, 100 }));
+	}
+
+	public int hIndex(int[] citations) {
+		int length = citations.length;
+		int left = 0;
+		int right = length - 1;
+		while (left <= right) {
+			int med = (right + left) / 2;
+			int diff = length - med;
+			if (citations[med] == diff) {
+				return diff;
+			} else if (citations[med] < diff) {
+				left = med + 1;
+			} else {
+				right = med - 1;
+			}
+		}
+		return length - left;
+	}
+
+}
