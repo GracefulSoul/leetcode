332bec2c6e05e2271138f3354265d130b2adac95
yongsik <mymyyoo@naver.com>
2021-11-30 03:31:34 +0900

    Add code about Leetcode - Perfect Squares

---
+++ src/main/java/gracefulsoul/problems/PerfectSquares.java
@@ -0,0 +1,34 @@
+package gracefulsoul.problems;
+
+public class PerfectSquares {
+
+	// https://leetcode.com/submissions/detail/594656524/
+	public static void main(String[] args) {
+		PerfectSquares test = new PerfectSquares();
+		System.out.println(test.numSquares(12));
+		System.out.println(test.numSquares(13));
+	}
+
+	public int numSquares(int n) {
+		for (int idx = 1; idx < n; idx++) {
+			if (this.isDividedBy(n, idx)) {
+				return idx;
+			}
+		}
+		return n;
+	}
+
+	private boolean isDividedBy(int n, int count) {
+		if (count == 1) {
+			return Math.abs(Math.pow((int) Math.sqrt(n), 2) - n) < 1e-5;
+		} else {
+			for (int idx = 1; idx * idx <= n; idx++) {
+				if (this.isDividedBy(n - idx * idx, count - 1)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+}
