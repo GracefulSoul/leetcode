ec0fd87e0af1085d3875cceb9c1ccfb0891153b6
yongsik <mymyyoo@naver.com>
2021-11-29 19:39:57 +0900

    Add code about Leetcode - First Bad Version

---
+++ src/main/java/gracefulsoul/problems/FirstBadVersion.java
@@ -0,0 +1,35 @@
+package gracefulsoul.problems;
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+boolean isBadVersion(int version); */
+public class FirstBadVersion extends VersionControl {
+
+	// https://leetcode.com/submissions/detail/594394246/
+	public static void main(String[] args) {
+		FirstBadVersion test = new FirstBadVersion();
+		test.isBadVersion(3);
+		test.isBadVersion(5);
+		test.isBadVersion(4);
+	}
+
+	public int firstBadVersion(int n) {
+		int low = 1;
+		int high = n;
+		while (low <= high) {
+			int mid = low + ((high - low) / 2);
+			if (super.isBadVersion(mid)) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return low;
+	}
+
+}
+
+class VersionControl {
+	boolean isBadVersion(int version) {
+		return false;
+	}
+}
