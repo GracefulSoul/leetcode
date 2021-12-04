55fe06068be3331c93a124ec0077a45980c272e1
yongsik <mymyyoo@naver.com>
2021-12-04 12:17:24 +0900

    Add code about Leetcode - Move Zeroes

--- src/main/java/gracefulsoul/problems/MoveZeroes.java
+++ src/main/java/gracefulsoul/problems/MoveZeroes.java
@@ -16,8 +16,9 @@ public class MoveZeroes {
 	public void moveZeroes(int[] nums) {
 		int idx = 0;
 		for (int num : nums) {
-			if (num != 0)
+			if (num != 0) {
 				nums[idx++] = num;
+			}
 		}
 		while (idx < nums.length) {
 			nums[idx++] = 0;
