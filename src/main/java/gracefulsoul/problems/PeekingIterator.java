1cf9b448d1be2bf443cd691cfb4c9a2fe81a05df
yongsik <mymyyoo@naver.com>
2021-12-05 09:51:02 +0900

    Add code about Leetcode - Peeking Iterator

--- src/main/java/gracefulsoul/problems/PeekingIterator.java
+++ src/main/java/gracefulsoul/problems/PeekingIterator.java
@@ -2,50 +2,61 @@ package gracefulsoul.problems;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 //Java Iterator interface reference:
 //https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 public class PeekingIterator implements Iterator<Integer> {
 
-	// https://leetcode.com/submissions/detail/597032835/
+	// https://leetcode.com/submissions/detail/597040579/
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>();
 		list.add(1);
 		list.add(2);
 		list.add(3);
 		PeekingIterator peekingIterator = new PeekingIterator(list.iterator()); // [1,2,3]
-		peekingIterator.next();    // return 1, the pointer moves to the next element [1,2,3].
-		peekingIterator.peek();    // return 2, the pointer does not move [1,2,3].
-		peekingIterator.next();    // return 2, the pointer moves to the next element [1,2,3]
-		peekingIterator.next();    // return 3, the pointer moves to the next element [1,2,3]
-		peekingIterator.hasNext(); // return False
+		System.out.println(peekingIterator.next());    // return 1, the pointer moves to the next element [1,2,3].
+		System.out.println(peekingIterator.peek());    // return 2, the pointer does not move [1,2,3].
+		System.out.println(peekingIterator.next());    // return 2, the pointer moves to the next element [1,2,3]
+		System.out.println(peekingIterator.next());    // return 3, the pointer moves to the next element [1,2,3]
+		System.out.println(peekingIterator.hasNext()); // return False
 	}
 
-	LinkedList<Integer> iterator = new LinkedList<>();
+	private Iterator<Integer> iterator;
+	private Integer peekVal;
+	private boolean cached;
 
 	public PeekingIterator(Iterator<Integer> iterator) {
-		while (iterator.hasNext()) {
-			this.iterator.offer(iterator.next());
-		}
+		// initialize any member here.
+		this.iterator = iterator;
 	}
 
 	// Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
-		return this.iterator.peek();
+		if (!this.cached) {
+			this.peekVal = this.iterator.next();
+			this.cached = true;
+		}
+		return this.peekVal;
 	}
 
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-		return this.iterator.poll();
+		if (this.cached) {
+			this.cached = false;
+			return this.peekVal;
+		}
+		return this.iterator.next();
 	}
 
 	@Override
 	public boolean hasNext() {
-		return !this.iterator.isEmpty();
+		if (this.cached) {
+			return true;
+		}
+		return this.iterator.hasNext();
 	}
 
 }
