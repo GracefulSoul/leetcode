728b9cee72ff650059e0709e4b0a92e3b9ac8b1c
yongsik <mymyyoo@naver.com>
2021-12-11 20:11:06 +0900

    Add code about Leetcode - Serialize and Deserialize Binary Tree

--- src/main/java/gracefulsoul/problems/SerializeAndDeserializeBinaryTree.java
+++ src/main/java/gracefulsoul/problems/SerializeAndDeserializeBinaryTree.java
@@ -12,9 +12,7 @@ public class SerializeAndDeserializeBinaryTree {
 	public static void main(String[] args) {
 		Codec ser = new Codec();
 		Codec deser = new Codec();
-		print(deser.deserialize(
-				ser.serialize(new TreeNode(1, new TreeNode(2), new TreeNode(3, new TreeNode(4), new TreeNode(5))))),
-				true, false);
+		print(deser.deserialize(ser.serialize(new TreeNode(1, new TreeNode(2), new TreeNode(3, new TreeNode(4), new TreeNode(5))))), true, false);
 		print(deser.deserialize(ser.serialize(new TreeNode())), true, false);
 		print(deser.deserialize(ser.serialize(new TreeNode(1))), true, false);
 		print(deser.deserialize(ser.serialize(new TreeNode(1, new TreeNode(2), null))), true, false);
