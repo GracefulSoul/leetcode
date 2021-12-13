f34c01fb49747c30ac2e50a0565d79c12ccce3e5
yongsik <mymyyoo@naver.com>
2021-12-13 14:55:22 +0900

    Add code about Leetcode - Longest Increasing Subsequence

--- src/main/java/gracefulsoul/problems/LongestIncreasingSubsequence.java
+++ src/main/java/gracefulsoul/problems/LongestIncreasingSubsequence.java
@@ -2,6 +2,7 @@ package gracefulsoul.problems;
 
 public class LongestIncreasingSubsequence {
 
+	// https://leetcode.com/submissions/detail/601087702/
 	public static void main(String[] args) {
 		LongestIncreasingSubsequence test = new LongestIncreasingSubsequence();
 		System.out.println(test.lengthOfLIS(new int[] { 10, 9, 2, 5, 3, 7, 101, 18 }));
@@ -10,22 +11,21 @@ public class LongestIncreasingSubsequence {
 
 	}
 
-	// https://leetcode.com/submissions/detail/601031230/
 	public int lengthOfLIS(int[] nums) {
 		int[] dp = new int[nums.length + 1];
 		dp[0] = Integer.MIN_VALUE;
-		int length = 1;
+		int length = 0;
 		for (int idx = 0; idx < nums.length; idx++) {
-			int right = length - 1;
-			while (dp[right] >= nums[idx]) {
-				right--;
+			int position = length;
+			while (dp[position] >= nums[idx]) {
+				position--;
 			}
-			if (right == length - 1) {
+			if (position == length) {
 				length++;
 			}
-			dp[right + 1] = nums[idx];
+			dp[position + 1] = nums[idx];
 		}
-		return length - 1;
+		return length;
 	}
 
 }
