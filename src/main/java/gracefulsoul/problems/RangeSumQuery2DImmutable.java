165e96c7894758f259942b0d40558df98b5e3930
yongsik <mymyyoo@naver.com>
2021-12-17 17:49:27 +0900

    Add code about Leetcode - Range Sum Query 2D - Immutable

--- src/main/java/gracefulsoul/problems/RangeSumQuery2DImmutable.java
+++ src/main/java/gracefulsoul/problems/RangeSumQuery2DImmutable.java
@@ -44,13 +44,13 @@ class NumMatrix {
 
 	public int sumRegion(int row1, int col1, int row2, int col2) {
 		int sum = matrix[row2][col2];
-		if (row1 - 1 >= 0) {
+		if (row1 > 0) {
 			sum -= matrix[row1 - 1][col2];
 		}
-		if (col1 - 1 >= 0) {
+		if (col1 > 0) {
 			sum -= matrix[row2][col1 - 1];
 		}
-		if (row1 - 1 >= 0 && col1 - 1 >= 0) {
+		if (row1 > 0 && col1 > 0) {
 			sum += matrix[row1 - 1][col1 - 1];
 		}
 		return sum;
