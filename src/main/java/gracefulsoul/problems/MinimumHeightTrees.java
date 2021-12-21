1a58fe54fa97d15207fbe930cf3bd8d3de34d7fc
yongsik <mymyyoo@naver.com>
2021-12-21 15:52:22 +0900

    Add code about Leetcode - Minimum Height Trees

---
+++ src/main/java/gracefulsoul/problems/MinimumHeightTrees.java
@@ -0,0 +1,74 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class MinimumHeightTrees {
+
+	public static void main(String[] args) {
+		MinimumHeightTrees test = new MinimumHeightTrees();
+		System.out.println(test.findMinHeightTrees(4, new int[][] {
+			new int[] { 1, 0 },
+			new int[] { 1, 2 },
+			new int[] { 1, 3 }
+		}));
+		System.out.println(test.findMinHeightTrees(6, new int[][] {
+			new int[] { 3, 0 },
+			new int[] { 3, 1 },
+			new int[] { 3, 2 },
+			new int[] { 3, 4 },
+			new int[] { 5, 4 }
+		}));
+	}
+
+	public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+		List<Integer> result = new ArrayList<>();
+		if (n == 1) {
+			result.add(0);
+			return result;
+		} else if (n == 2) {
+			result.add(0);
+			result.add(1);
+			return result;
+		}
+		List<Integer>[] graph = new ArrayList[n];
+		int[] degree = new int[n];
+		for (int idx = 0; idx < n; idx++) {
+			graph[idx] = new ArrayList<>();
+		}
+		for (int idx = 0; idx < edges.length; idx++) {
+			int v1 = edges[idx][0];
+			int v2 = edges[idx][1];
+			degree[v1]++;
+			degree[v2]++;
+			graph[v1].add(v2);
+			graph[v2].add(v1);
+		}
+		Deque<Integer> deque = new ArrayDeque<>();
+		for (int idx = 0; idx < n; idx++) {
+			if (degree[idx] == 1) {
+				deque.add(idx);
+			}
+		}
+		while (n > 2) {
+			int size = deque.size();
+			n -= size;
+			while (size > 0) {
+				int idx = deque.pop();
+				degree[idx]--;
+				for (int val : graph[idx]) {
+					degree[val]--;
+					if (degree[val] == 1) {
+						deque.add(val);
+					}
+				}
+				size--;
+			}
+		}
+		result.addAll(deque);
+		return result;
+	}
+
+}
