aea8b14778221dd6d8fc839fa45f55e47045bed0
yongsik <mymyyoo@naver.com>
2021-12-24 11:04:42 +0900

    Add code about Leetcode - Count of Smaller Numbers After Self

---
+++ src/main/java/gracefulsoul/problems/CountOfSmallerNumbersAfterSelf.java
@@ -0,0 +1,52 @@
+package gracefulsoul.problems;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CountOfSmallerNumbersAfterSelf {
+
+	public static void main(String[] args) {
+		CountOfSmallerNumbersAfterSelf test = new CountOfSmallerNumbersAfterSelf();
+		System.out.println(test.countSmaller(new int[] { 5, 2, 6, 1 }));
+		System.out.println(test.countSmaller(new int[] { -1 }));
+		System.out.println(test.countSmaller(new int[] { -1, -1 }));
+	}
+
+	public List<Integer> countSmaller(int[] nums) {
+		int length = nums.length;
+		Integer[] result = new Integer[length];
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for (int num : nums) {
+			min = Math.min(min, num);
+		}
+		for (int idx = 0; idx < length; idx++) {
+			nums[idx] = nums[idx] - min + 1;
+			max = Math.max(max, nums[idx]);
+		}
+		int[] tree = new int[max + 1];
+		for (int idx = length - 1; idx >= 0; idx--) {
+			result[idx] = this.get(tree, nums[idx] - 1);
+			this.update(tree, nums[idx]);
+		}
+		return Arrays.asList(result);
+	}
+
+	private void update(int[] tree, int index) {
+		int length = tree.length;
+		while (index < length) {
+			tree[index]++;
+			index += (index & -index);
+		}
+	}
+
+	private int get(int[] tree, int index) {
+		int count = 0;
+		while (index > 0) {
+			count += tree[index];
+			index -= (index & -index);
+		}
+		return count;
+	}
+
+}
