7b8890b2660f1d092432f6f8ac067a3dd4f2e63a
yongsik <mymyyoo@naver.com>
2021-12-31 13:14:03 +0900

    Add code about Leetcode - Coin Change

--- src/main/java/gracefulsoul/problems/CoinChange.java
+++ src/main/java/gracefulsoul/problems/CoinChange.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CoinChange {
 
-	// https://leetcode.com/submissions/detail/610172871/
+	// https://leetcode.com/submissions/detail/610175098/
 	public static void main(String[] args) {
 		CoinChange test = new CoinChange();
 		System.out.println(test.coinChange(new int[] { 1, 2, 5 }, 11));
@@ -18,8 +18,9 @@ public class CoinChange {
 		dp[0] = 0;
 		for (int idx = 0; idx < amount + 1; idx++) {
 			for (int coin : coins) {
-				if (idx >= coin)
+				if (idx >= coin) {
 					dp[idx] = Math.min(dp[idx], 1 + dp[idx - coin]);
+				}
 			}
 		}
 		return dp[amount] > amount ? -1 : dp[amount];
