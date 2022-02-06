32d0baae406a35c93367eaeba5910c5659953f96
yongsik <mymyyoo@naver.com>
2022-02-06 10:45:02 +0900

    Add code about Leetcode - Combination Sum IV

--- src/main/java/gracefulsoul/problems/CombinationSumIV.java
+++ src/main/java/gracefulsoul/problems/CombinationSumIV.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CombinationSumIV {
 
-	// https://leetcode.com/submissions/detail/635345142/
+	// https://leetcode.com/submissions/detail/635349215/
 	public static void main(String[] args) {
 		CombinationSumIV test = new CombinationSumIV();
 		System.out.println(test.combinationSum4(new int[] { 1, 2, 3 }, 4));
@@ -14,22 +14,22 @@ public class CombinationSumIV {
 	public int combinationSum4(int[] nums, int target) {
 		int[] dp = new int[target + 1];
 		Arrays.fill(dp, -1);
-		return this.resursive(nums, dp, 0, target);
+		return this.resursive(nums, dp, target);
 	}
 
-	private int resursive(int[] nums, int[] dp, int index, int target) {
-		if (index == target) {
+	private int resursive(int[] nums, int[] dp, int target) {
+		if (target == 0) {
 			return 1;
-		} else if (index > target) {
+		} else if (target < 0) {
 			return 0;
-		} else if (dp[target - index] == -1) {
+		} else if (dp[target] == -1) {
 			int result = 0;
 			for (int num : nums) {
-				result += this.resursive(nums, dp, index + num, target);
+				result += this.resursive(nums, dp, target - num);
 			}
-			dp[target - index] = result;
+			dp[target] = result;
 		}
-		return dp[target - index];
+		return dp[target];
 	}
 
 }
