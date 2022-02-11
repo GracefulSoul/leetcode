d83b05f54303c83d2c07d2151e9800e63243325a
yongsik <mymyyoo@naver.com>
2022-02-11 11:23:43 +0900

    Add code about Leetcode - Ransom Note

---
+++ src/main/java/gracefulsoul/problems/RansomNote.java
@@ -0,0 +1,27 @@
+package gracefulsoul.problems;
+
+public class RansomNote {
+
+	// https://leetcode.com/submissions/detail/638972145/
+	public static void main(String[] args) {
+		RansomNote test = new RansomNote();
+		System.out.println(test.canConstruct("a", "b"));
+		System.out.println(test.canConstruct("aa", "ab"));
+		System.out.println(test.canConstruct("aa", "aab"));
+	}
+
+	public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26]; 
+        for(char ransomNoteChar : ransomNote.toCharArray()){
+        	int num = ransomNoteChar - 'a';
+            int preIndex = count[num];
+            int index = magazine.indexOf(ransomNoteChar, preIndex);
+            if(index == -1) {
+                return false;
+            }
+            count[ransomNoteChar - 'a'] = index + 1;
+        }
+        return true;
+	}
+
+}
