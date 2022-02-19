280205ae3a8f46e7e9a0267ab05d3091652edd56
yongsik <mymyyoo@naver.com>
2022-02-19 17:22:23 +0900

    Add code about Leetcode - Perfect Rectangle

--- src/main/java/gracefulsoul/problems/PerfectRectangle.java
+++ src/main/java/gracefulsoul/problems/PerfectRectangle.java
@@ -21,6 +21,12 @@ public class PerfectRectangle {
 			new int[] { 3, 1, 4, 2 },
 			new int[] { 3, 2, 4, 4 }
 		}));
+		System.out.println(test.isRectangleCover(new int[][] {
+			new int[] { 1, 1, 3, 3 },
+			new int[] { 1, 1, 3, 3 },
+			new int[] { 1, 3, 2, 4 },
+			new int[] { 2, 2, 4, 4 }
+		}));
 	}
 
 	public boolean isRectangleCover(int[][] rectangles) {
