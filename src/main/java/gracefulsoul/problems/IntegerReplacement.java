be2dc099888c2904fb5ad74591f3ce10b578642a
yongsik <mymyyoo@naver.com>
2022-02-25 12:50:54 +0900

    Add code about Leetcode - Integer Replacement

---
+++ src/main/java/gracefulsoul/problems/IntegerReplacement.java
@@ -0,0 +1,26 @@
+package gracefulsoul.problems;
+
+public class IntegerReplacement {
+
+	// https://leetcode.com/submissions/detail/648436492/
+	public static void main(String[] args) {
+		IntegerReplacement test = new IntegerReplacement();
+		System.out.println(test.integerReplacement(8));
+		System.out.println(test.integerReplacement(7));
+		System.out.println(test.integerReplacement(4));
+	}
+
+	public int integerReplacement(int n) {
+		int count = 0;
+		while (n > 1) {
+			if ((n & 1) != 0) {
+				n += (n == 3 || ((n >> 1) & 1) == 0) ? -1 : 1;
+				count++;
+			}
+			n >>>= 1;
+			count++;
+		}
+		return count;
+	}
+
+}
