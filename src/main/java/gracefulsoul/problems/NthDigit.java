3ad19bdd5d5ec7c66ef0371ee693531ef5d8b562
yongsik <mymyyoo@naver.com>
2022-02-28 08:57:37 +0900

    Add code about Leetcode - Nth Digit

---
+++ src/main/java/gracefulsoul/problems/NthDigit.java
@@ -0,0 +1,28 @@
+package gracefulsoul.problems;
+
+public class NthDigit {
+
+	// https://leetcode.com/submissions/detail/650211613/
+	public static void main(String[] args) {
+		NthDigit test = new NthDigit();
+		System.out.println(test.findNthDigit(3));
+		System.out.println(test.findNthDigit(11));
+	}
+
+	public int findNthDigit(int n) {
+		int length = 1, base = 1;
+		while (n > 9L * base * length) {
+			n -= 9 * base * length;
+			length++;
+			base *= 10;
+		}
+		int result = 0;
+		int num = (n - 1) / length + base;
+		for (int i = (n - 1) % length; i < length; i++) {
+			result = num % 10;
+			num /= 10;
+		}
+		return result;
+	}
+
+}
