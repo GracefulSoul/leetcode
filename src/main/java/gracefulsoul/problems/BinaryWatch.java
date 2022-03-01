59e1a675fd2a4e8010806da1db3976063635015c
yongsik <mymyyoo@naver.com>
2022-03-01 14:52:33 +0900

    Add code about Leetcode - Binary Watch

--- src/main/java/gracefulsoul/problems/BinaryWatch.java
+++ src/main/java/gracefulsoul/problems/BinaryWatch.java
@@ -13,7 +13,7 @@ public class BinaryWatch {
 	}
 
 	public List<String> readBinaryWatch(int turnedOn) {
-		List<String> times = new ArrayList<String>();
+		List<String> times = new ArrayList<>();
 		for (int hour = 0; hour < 12; hour++) {
 			for (int minute = 0; minute < 60; minute++) {
 				int bits = Integer.bitCount(hour) + Integer.bitCount(minute);
