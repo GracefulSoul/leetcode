2092799f7df3377329417866be5a1c6c27743ac2
yongsik <mymyyoo@naver.com>
2022-03-02 12:17:34 +0900

    Add code about Leetcode - Remove K Digits

--- src/main/java/gracefulsoul/problems/RemoveKDigits.java
+++ src/main/java/gracefulsoul/problems/RemoveKDigits.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class RemoveKDigits {
 
-	// https://leetcode.com/submissions/detail/651625905/
+	// https://leetcode.com/submissions/detail/651629059/
 	public static void main(String[] args) {
 		RemoveKDigits test = new RemoveKDigits();
 		System.out.println(test.removeKdigits("1432219", 3));
@@ -12,21 +12,21 @@ public class RemoveKDigits {
 
 	public String removeKdigits(String num, int k) {
 		char[] numCharArray = num.toCharArray();
-		int index = this.findIndex(numCharArray, k);
 		int start = 0;
-		while (index > start && numCharArray[start] == '0') {
+		int end = this.findEndIndex(numCharArray, k);
+		while (end > start && numCharArray[start] == '0') {
 			start++;
 		}
-		return start == index ? "0" : new String(numCharArray, start, index - start);
+		return start == end ? "0" : new String(numCharArray, start, end - start);
 	}
 
-	private int findIndex(char[] numCharArray, int k) {
+	private int findEndIndex(char[] numCharArray, int k) {
 		int index = 0;
 		for (int idx = 0; idx < numCharArray.length; idx++) {
-			if (index > 0 && k > 0 && numCharArray[idx] < numCharArray[index - 1]) {
-				index--;
+			if (index > 0 && k > 0 && numCharArray[index - 1] > numCharArray[idx]) {
 				k--;
 				idx--;
+				index--;
 			} else {
 				numCharArray[index++] = numCharArray[idx];
 			}
