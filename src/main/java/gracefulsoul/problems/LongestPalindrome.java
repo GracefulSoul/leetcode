ad2b322df2403df01da7f777a6d661cfa71f1f2d
yongsik <mymyyoo@naver.com>
2022-03-08 11:22:35 +0900

    Add code about Leetcode - Longest Palindrome

---
+++ src/main/java/gracefulsoul/problems/LongestPalindrome.java
@@ -0,0 +1,30 @@
+package gracefulsoul.problems;
+
+public class LongestPalindrome {
+
+	// https://leetcode.com/submissions/detail/655533536/
+	public static void main(String[] args) {
+		LongestPalindrome test = new LongestPalindrome();
+		System.out.println(test.longestPalindrome("abccccdd"));
+		System.out.println(test.longestPalindrome("a"));
+		System.out.println(test.longestPalindrome("bb"));
+	}
+
+	public int longestPalindrome(String s) {
+		int[] count = new int[58];
+		for (char c : s.toCharArray()) {
+			count[c - 'A']++;
+		}
+		int length = 0;
+		boolean isOdd = false;
+		for (int idx = 0; idx < 58; idx++) {
+			length += count[idx];
+			if (count[idx] % 2 == 1) {
+				length--;
+				isOdd = true;
+			}
+		}
+		return length + (isOdd ? 1 : 0);
+	}
+
+}
