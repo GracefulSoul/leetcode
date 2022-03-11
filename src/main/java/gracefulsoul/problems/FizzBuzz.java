852d87e325cecd9ac801ceb6ceafc9cc15d35c1e
yongsik <mymyyoo@naver.com>
2022-03-11 12:11:19 +0900

    Add code about Leetcode - Fizz Buzz

--- src/main/java/gracefulsoul/problems/FizzBuzz.java
+++ src/main/java/gracefulsoul/problems/FizzBuzz.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class FizzBuzz {
 
-	// https://leetcode.com/submissions/detail/657576127/
+	// https://leetcode.com/submissions/detail/657577727/
 	public static void main(String[] args) {
 		FizzBuzz test = new FizzBuzz();
 		System.out.println(test.fizzBuzz(3));
@@ -14,19 +14,19 @@ public class FizzBuzz {
 	}
 
 	public List<String> fizzBuzz(int n) {
-		List<String> list = new ArrayList<>();
+		List<String> result = new ArrayList<>();
 		for (int idx = 1; idx <= n; idx++) {
 			if (idx % 15 == 0) {
-				list.add("FizzBuzz");
+				result.add("FizzBuzz");
 			} else if (idx % 3 == 0) {
-				list.add("Fizz");
+				result.add("Fizz");
 			} else if (idx % 5 == 0) {
-				list.add("Buzz");
+				result.add("Buzz");
 			} else {
-				list.add(String.valueOf(idx));
+				result.add(String.valueOf(idx));
 			}
 		}
-		return list;
+		return result;
 	}
 
 }
