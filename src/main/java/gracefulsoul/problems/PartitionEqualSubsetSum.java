3a82987f9e927e141414412ee1764c5e382f94c3
yongsik <mymyyoo@naver.com>
2022-03-16 12:01:20 +0900

    Add code about Leetcode - Partition Equal Subset Sum

---
+++ src/main/java/gracefulsoul/problems/PartitionEqualSubsetSum.java
@@ -0,0 +1,41 @@
+package gracefulsoul.problems;
+
+public class PartitionEqualSubsetSum {
+
+	public static void main(String[] args) {
+		PartitionEqualSubsetSum test = new PartitionEqualSubsetSum();
+		System.out.println(test.canPartition(new int[] { 1, 5, 11, 5 }));
+		System.out.println(test.canPartition(new int[] { 1, 2, 3, 5 }));
+	}
+
+	// https://leetcode.com/submissions/detail/660893913/
+	public boolean canPartition(int[] nums) {
+		int sum = 0;
+		for (int num : nums) {
+			sum += num;
+		}
+		if ((sum & 1) > 0) {
+			return false;
+		}
+		int target = sum / 2;
+		return this.dfs(nums, new boolean[target + 1], 0, target);
+	}
+
+	private boolean dfs(int[] nums, boolean[] dp, int index, int target) {
+		if (index >= nums.length) {
+			return false;
+		} else if (nums[index] == target) {
+			return true;
+		} else {
+			if (target - nums[index] > 0) {
+				int num = target - nums[index];
+				if (!dp[num]) {
+					dp[num] = true;
+					return this.dfs(nums, dp, index + 1, num) || this.dfs(nums, dp, index + 1, target);
+				}
+			}
+			return this.dfs(nums, dp, index + 1, target);
+		}
+	}
+
+}
