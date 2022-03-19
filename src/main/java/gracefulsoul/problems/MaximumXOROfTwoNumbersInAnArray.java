8c929b93f50b21aaf4aca2b759b291fbf99227a3
yongsik <mymyyoo@naver.com>
2022-03-19 12:17:20 +0900

    Add code about Leetcode - Maximum XOR of Two Numbers in an Array

---
+++ src/main/java/gracefulsoul/problems/MaximumXOROfTwoNumbersInAnArray.java
@@ -0,0 +1,39 @@
+package gracefulsoul.problems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MaximumXOROfTwoNumbersInAnArray {
+
+	// https://leetcode.com/submissions/detail/662749365/
+	public static void main(String[] args) {
+		MaximumXOROfTwoNumbersInAnArray test = new MaximumXOROfTwoNumbersInAnArray();
+		System.out.println(test.findMaximumXOR(new int[] { 3, 10, 5, 25, 2, 8 }));
+		System.out.println(test.findMaximumXOR(new int[] { 14, 70, 53, 83, 49, 91, 36, 80, 92, 51, 66, 70 }));
+	}
+
+	public int findMaximumXOR(int[] nums) {
+		int result = 0;
+		int mask = 0;
+		int max = 0;
+		for (int num : nums) {
+			max = Math.max(max, num);
+		}
+		Set<Integer> set = new HashSet<>();
+		for (int idx = 31 - Integer.numberOfLeadingZeros(max); idx >= 0; idx--) {
+			set.clear();
+			int bit = 1 << idx;
+			mask = mask | bit;
+			int temp = result | bit;
+			for (int num : nums) {
+				if (set.contains((num & mask) ^ temp)) {
+					result = temp;
+					break;
+				}
+				set.add(num & mask);
+			}
+		}
+		return result;
+	}
+
+}
