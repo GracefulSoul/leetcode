fb9796e387e06ecc466250b65eef3844342e2d97
yongsik <mymyyoo@naver.com>
2022-03-20 12:24:03 +0900

    Add code about Leetcode - Reconstruct Original Digits from English

--- src/main/java/gracefulsoul/problems/ReconstructOriginalDigitsFromEnglish.java
+++ src/main/java/gracefulsoul/problems/ReconstructOriginalDigitsFromEnglish.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class ReconstructOriginalDigitsFromEnglish {
 
-	// https://leetcode.com/submissions/detail/663411450/
+	// https://leetcode.com/submissions/detail/663438039/
 	public static void main(String[] args) {
 		ReconstructOriginalDigitsFromEnglish test = new ReconstructOriginalDigitsFromEnglish();
 		System.out.println(test.originalDigits("owoztneoer"));
@@ -10,7 +10,7 @@ public class ReconstructOriginalDigitsFromEnglish {
 	}
 
 	public String originalDigits(String s) {
-		char[] charCount = new char[26 + 'a'];
+		char[] charCount = new char['a' + 26];
 		for (char c : s.toCharArray()) {
 			charCount[c]++;
 		}
@@ -23,7 +23,7 @@ public class ReconstructOriginalDigitsFromEnglish {
 		count[3] = charCount['h'] - count[8];
 		count[5] = charCount['f'] - count[4];
 		count[7] = charCount['s'] - count[6];
-		count[9] = charCount['i'] - count[6] - count[5] - count[8];
+		count[9] = charCount['i'] - count[5] - count[6] - count[8];
 		count[1] = charCount['n'] - count[7] - (2 * count[9]);
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 10; i++) {
