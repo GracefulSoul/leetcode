32e605d18ae8933a9cd6f461c89a28a511071c0b
yongsik <mymyyoo@naver.com>
2022-03-26 19:35:46 +0900

    Add code about Leetcode - Number of Segments in a String

--- src/main/java/gracefulsoul/problems/NumberOfSegmentsInAString.java
+++ src/main/java/gracefulsoul/problems/NumberOfSegmentsInAString.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class NumberOfSegmentsInAString {
 
-	// https://leetcode.com/submissions/detail/667546090/
+	// https://leetcode.com/submissions/detail/667549397/
 	public static void main(String[] args) {
 		NumberOfSegmentsInAString test = new NumberOfSegmentsInAString();
 		System.out.println(test.countSegments("Hello, my name is John"));
@@ -10,18 +10,20 @@ public class NumberOfSegmentsInAString {
 	}
 
 	public int countSegments(String s) {
+		s = s.trim();
 		int length = s.length();
 		char[] charArray = s.toCharArray();
+		int count = 0;
 		if (length == 0) {
-			return 0;
-		}
-		int count = charArray[0] != ' ' ? 1 : 0;
-		for (int i = 1; i < length; i++) {
-			if (charArray[i] != ' ' && charArray[i - 1] == ' ') {
-				count++;
+			return count;
+		} else {
+			for (int idx = 0; idx < length; idx++) {
+				if (charArray[idx] == ' ' && charArray[idx + 1] != ' ') {
+					count++;
+				}
 			}
+			return count + 1;
 		}
-		return count;
 	}
 
 }
