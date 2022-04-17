313801a77033d0c41df5cf6d0b286a3c1d8679d5
yongsik <mymyyoo@naver.com>
2022-04-17 13:53:13 +0900

    Add code about Leetcode - Poor Pigs

--- src/main/java/gracefulsoul/problems/PoorPigs.java
+++ src/main/java/gracefulsoul/problems/PoorPigs.java
@@ -11,11 +11,11 @@ public class PoorPigs {
 	}
 
 	public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int pigs = 0;
-        while (Math.pow((minutesToTest / minutesToDie) + 1, pigs) < buckets) {
-            pigs++;
-        }
-        return pigs;
+		int pigs = 0;
+		while (Math.pow((minutesToTest / minutesToDie) + 1, pigs) < buckets) {
+			pigs++;
+		}
+		return pigs;
 	}
 
 }
