6dc5dcfbb57d7fb3735f688e13692c7737b3a8b1
yongsik <mymyyoo@naver.com>
2022-04-23 10:27:26 +0900

    Add code about Leetcode - Can I Win

--- src/main/java/gracefulsoul/problems/CanIWin.java
+++ src/main/java/gracefulsoul/problems/CanIWin.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class CanIWin {
 
-	// https://leetcode.com/submissions/detail/685692531/
+	// https://leetcode.com/submissions/detail/685709388/
 	public static void main(String[] args) {
 		CanIWin test = new CanIWin();
 		System.out.println(test.canIWin(10, 11));
@@ -15,20 +15,20 @@ public class CanIWin {
 			return false;
 		} else if (maxChoosableInteger >= desiredTotal) {
 			return true;
-		} else { 
+		} else {
 			return this.dfs(maxChoosableInteger, desiredTotal, new boolean[1 << maxChoosableInteger], 0);
 		}
 	}
 
-	private boolean dfs(int max, int total, boolean[] state, int cur) {
-		if (state[cur]) {
-			return state[cur];
+	private boolean dfs(int max, int total, boolean[] dp, int cur) {
+		if (dp[cur]) {
+			return dp[cur];
 		}
 		for (int idx = 0; idx < max; idx++) {
 			if ((cur & 1 << idx) != 0) {
 				continue;
-			} else if (idx + 1 >= total || !this.dfs(max, total - idx - 1, state, cur | 1 << idx)) {
-				state[cur] = true;
+			} else if (idx + 1 >= total || !this.dfs(max, total - idx - 1, dp, cur | 1 << idx)) {
+				dp[cur] = true;
 				return true;
 			}
 		}
