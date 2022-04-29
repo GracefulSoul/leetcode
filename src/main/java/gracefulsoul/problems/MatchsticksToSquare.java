e04d3690fbe604c9e5042ff91b3af21bb09f86d4
yongsik <mymyyoo@naver.com>
2022-04-29 12:07:13 +0900

    Add code about Leetcode - Matchsticks to Square

---
+++ src/main/java/gracefulsoul/problems/MatchsticksToSquare.java
@@ -0,0 +1,51 @@
+package gracefulsoul.problems;
+
+import java.util.Arrays;
+
+public class MatchsticksToSquare {
+
+	// https://leetcode.com/submissions/detail/689443557/
+	public static void main(String[] args) {
+		MatchsticksToSquare test = new MatchsticksToSquare();
+		System.out.println(test.makesquare(new int[] { 1, 1, 2, 2, 2 }));
+		System.out.println(test.makesquare(new int[] { 3, 3, 3, 3, 4 }));
+	}
+
+	public boolean makesquare(int[] nums) {
+		if (nums.length == 0) {
+			return false;
+		}
+		int sum = 0;
+		for (int num : nums) {
+			sum += num;
+		}
+		if (sum % 4 != 0) {
+			return false;
+		}
+		Arrays.sort(nums);
+		return this.dfs(nums, new boolean[nums.length], 0, sum / 4, 0, 1);
+	}
+
+	private boolean dfs(int[] nums, boolean[] dp, int curr, int target, int sum, int group) {
+		if (group == 4) {
+			return true;
+		} else if (sum == target) {
+			return this.dfs(nums, dp, 0, target, 0, group + 1);
+		} else if (sum > target) {
+			return false;
+		} else {
+			for (int idx = curr; idx < nums.length; idx++) {
+				if (dp[idx] || (idx > 0 && nums[idx] == nums[idx - 1] && !dp[idx - 1])) {
+					continue;
+				}
+				dp[idx] = true;
+				if (this.dfs(nums, dp, idx + 1, target, sum + nums[idx], group)) {
+					return true;
+				}
+				dp[idx] = false;
+			}
+			return false;
+		}
+	}
+
+}
