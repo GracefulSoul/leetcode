ae1e9193271e84654d9c690ca020f0c6ab3b50ca
yongsik <mymyyoo@naver.com>
2022-05-06 10:22:30 +0900

    Add code about Leetcode - Largest Palindrome Product

---
+++ src/main/java/gracefulsoul/problems/LargestPalindromeProduct.java
@@ -0,0 +1,31 @@
+package gracefulsoul.problems;
+
+public class LargestPalindromeProduct {
+
+	// https://leetcode.com/submissions/detail/693991657/
+	public static void main(String[] args) {
+		LargestPalindromeProduct test = new LargestPalindromeProduct();
+		System.out.println(test.largestPalindrome(2));
+		System.out.println(test.largestPalindrome(1));
+	}
+
+	public int largestPalindrome(int n) {
+		if (n > 1) {
+			int num = (int) Math.pow(10, n);
+			for (long x = 2; x < num; x += 2) {
+				long high = num - x;
+				long low = Long.parseLong(new StringBuilder(Long.toString(high)).reverse().toString());
+				long temp = x * x - 4 * low;
+				if (temp < 0) {
+					continue;
+				}
+				long sqrt = (long) Math.sqrt(temp);
+				if (sqrt * sqrt == temp) {
+					return (int) ((high * num + low) % 1337);
+				}
+			}
+		}
+		return 9;
+	}
+
+}
