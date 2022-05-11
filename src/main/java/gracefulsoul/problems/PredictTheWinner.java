267c69ec3233043cd4643ea5ad6861241a94e2e2
yongsik <mymyyoo@naver.com>
2022-05-11 11:20:24 +0900

    Add code about Leetcode - Predict the Winner

---
+++ src/main/java/gracefulsoul/problems/PredictTheWinner.java
@@ -0,0 +1,30 @@
+package gracefulsoul.problems;
+
+public class PredictTheWinner {
+
+	// https://leetcode.com/submissions/detail/697145372/
+	public static void main(String[] args) {
+		PredictTheWinner test = new PredictTheWinner();
+		System.out.println(test.PredictTheWinner(new int[] { 1, 5, 2 }));
+		System.out.println(test.PredictTheWinner(new int[] { 1, 5, 233, 7 }));
+	}
+
+	public boolean PredictTheWinner(int[] nums) {
+		int length = nums.length;
+		if ((length & 1) == 0) {
+			return true;
+		}
+		int[] dp = new int[length];
+		for (int i = length - 1; i >= 0; i--) {
+			for (int j = i; j < length; j++) {
+				if (i == j) {
+					dp[i] = nums[i];
+				} else {
+					dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+				}
+			}
+		}
+		return dp[length - 1] >= 0;
+	}
+
+}
