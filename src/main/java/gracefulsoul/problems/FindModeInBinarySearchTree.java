fb833f040aba0fcd15c508d1166d73fc1aff6142
yongsik <mymyyoo@naver.com>
2022-05-21 22:25:47 +0900

    Add code about Leetcode - Find Mode in Binary Search Tree

---
+++ src/main/java/gracefulsoul/problems/FindModeInBinarySearchTree.java
@@ -0,0 +1,54 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gracefulsoul.object.node.TreeNode;
+import gracefulsoul.util.PrintUtil;
+
+public class FindModeInBinarySearchTree {
+
+	// https://leetcode.com/submissions/detail/704054867/
+	public static void main(String[] args) {
+		PrintUtil.print(new FindModeInBinarySearchTree().findMode(new TreeNode(1, null, new TreeNode(2, new TreeNode(2), null))));
+		PrintUtil.print(new FindModeInBinarySearchTree().findMode(new TreeNode(0)));
+	}
+
+	private int max = 0;
+	private int count = 1;
+	private TreeNode prev = null;
+
+	public int[] findMode(TreeNode root) {
+		List<TreeNode> list = new ArrayList<>();
+		this.dfs(root, list);
+		int[] result = new int[list.size()];
+		for (int idx = 0; idx < list.size(); idx++) {
+			result[idx] = list.get(idx).val;
+		}
+		return result;
+	}
+
+	public void dfs(TreeNode node, List<TreeNode> list) {
+		if (node == null) {
+			return;
+		}
+		this.dfs(node.left, list);
+		if (this.prev != null) {
+			if (node.val == this.prev.val) {
+				this.count++;
+			} else {
+				this.count = 1;
+			}
+		}
+		if (this.count > this.max) {
+			list.clear();
+			list.add(node);
+			this.max = this.count;
+		} else if (this.count == this.max) {
+			list.add(node);
+		}
+		this.prev = node;
+		this.dfs(node.right, list);
+	}
+
+}
