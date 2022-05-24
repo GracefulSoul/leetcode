9159adea60ec92071168dbb909b94ee6c932453f
yongsik <mymyyoo@naver.com>
2022-05-24 11:36:32 +0900

    Add code about Leetcode - Base 7

--- src/main/java/gracefulsoul/problems/Base7.java
+++ src/main/java/gracefulsoul/problems/Base7.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class Base7 {
 
-	// https://leetcode.com/submissions/detail/705870441/
+	// https://leetcode.com/submissions/detail/705875657/
 	public static void main(String[] args) {
 		Base7 test = new Base7();
 		System.out.println(test.convertToBase7(100));
@@ -23,8 +23,7 @@ public class Base7 {
 				sb.append(num % 7);
 				num /= 7;
 			}
-			sb.append(sign);
-			return sb.reverse().toString();
+			return sb.append(sign).reverse().toString();
 		}
 	}
 
