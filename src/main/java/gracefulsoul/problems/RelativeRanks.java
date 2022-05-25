90184cbc29ce0ee79d47b68d3d501c895c7130aa
yongsik <mymyyoo@naver.com>
2022-05-25 10:50:30 +0900

    Add code about Leetcode - Relative Ranks

---
+++ src/main/java/gracefulsoul/problems/RelativeRanks.java
@@ -0,0 +1,41 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.util.PrintUtil;
+
+public class RelativeRanks {
+
+	// https://leetcode.com/submissions/detail/706584087/
+	public static void main(String[] args) {
+		RelativeRanks test = new RelativeRanks();
+		PrintUtil.print(test.findRelativeRanks(new int[] { 5, 4, 3, 2, 1 }));
+		PrintUtil.print(test.findRelativeRanks(new int[] { 10, 3, 8, 9, 4 }));
+	}
+
+	public String[] findRelativeRanks(int[] score) {
+		int length = score.length;
+		int max = 0;
+		for (int s : score) {
+			if (s > max) {
+				max = s;
+			}
+		}
+		int[] map = new int[max + 1];
+		for (int idx = 0; idx < length; idx++) {
+			map[score[idx]] = idx + 1;
+		}
+		String[] result = new String[length];
+		int count = 0;
+		for (int idx = max; idx >= 0; idx--) {
+			if (map[idx] != 0) {
+				switch (++count) {
+					case 1: result[map[idx] - 1] = "Gold Medal"; break;
+					case 2: result[map[idx] - 1] = "Silver Medal"; break;
+					case 3: result[map[idx] - 1] = "Bronze Medal"; break;
+					default: result[map[idx] - 1] = Integer.toString(count);
+				}
+			}
+		}
+		return result;
+	}
+
+}
