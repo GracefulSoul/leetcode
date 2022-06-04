7b869dc8c7ae0418075c73d97ba10df686a858fa
yongsik <mymyyoo@naver.com>
2022-06-05 08:53:06 +0900

    Add code about Leetcode - Random Flip Matrix

---
+++ src/main/java/gracefulsoul/object/solution/random/flip/Solution.java
@@ -0,0 +1,42 @@
+package gracefulsoul.object.solution.random.flip;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Solution {
+
+	private Map<Integer, Integer> map;
+	private int m;
+	private int n;
+	private int total;
+	private Random random;
+
+	public Solution(int m, int n) {
+		this.map = new HashMap<>();
+		this.random = new Random();
+		this.m = m;
+		this.n = n;
+		this.reset();
+	}
+
+	public int[] flip() {
+		int num = this.random.nextInt(this.total--);
+		int value = this.map.getOrDefault(num, num);
+		this.map.put(num, this.map.getOrDefault(this.total, this.total));
+		this.map.put(this.total, value);
+		return new int[] { value / this.n, value % this.n };
+	}
+
+	public void reset() {
+		this.total = this.m * this.n;
+	}
+
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(m, n);
+ * int[] param_1 = obj.flip();
+ * obj.reset();
+ */
---
+++ src/main/java/gracefulsoul/problems/RandomFlipMatrix.java
@@ -0,0 +1,18 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.object.solution.random.flip.Solution;
+import gracefulsoul.util.PrintUtil;
+
+public class RandomFlipMatrix {
+
+	// https://leetcode.com/submissions/detail/714523470/
+	public static void main(String[] args) {
+		Solution solution = new Solution(3, 1);
+		PrintUtil.print(solution.flip());	// return [1, 0], [0,0], [1,0], and [2,0] should be equally likely to be returned.
+		PrintUtil.print(solution.flip());	// return [2, 0], Since [1,0] was returned, [2,0] and [0,0]
+		PrintUtil.print(solution.flip());	// return [0, 0], Based on the previously returned indices, only [0,0] can be returned.
+		solution.reset(); 					// All the values are reset to 0 and can be returned.
+		PrintUtil.print(solution.flip());	// return [2, 0], [0,0], [1,0], and [2,0] should be equally likely to be returned.
+	}
+
+}
