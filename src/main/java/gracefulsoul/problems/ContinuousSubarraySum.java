89549b8301fa2953c51869942e9626650c4255ba
yongsik <mymyyoo@naver.com>
2022-06-09 19:25:29 +0900

    Add code about Leetcode - Continuous Subarray Sum

--- src/main/java/gracefulsoul/problems/ContinuousSubarraySum.java
+++ src/main/java/gracefulsoul/problems/ContinuousSubarraySum.java
@@ -2,26 +2,26 @@ package gracefulsoul.problems;
 
 public class ContinuousSubarraySum {
 
-	// https://leetcode.com/submissions/detail/718011272/
+	// https://leetcode.com/submissions/detail/718024868/
 	public static void main(String[] args) {
 		ContinuousSubarraySum test = new ContinuousSubarraySum();
 		System.out.println(test.checkSubarraySum(new int[] { 23, 2, 4, 6, 7 }, 6));
-		System.out.println(test.checkSubarraySum(new int[] { 23, 2, 4, 6, 7 }, 6));
+		System.out.println(test.checkSubarraySum(new int[] { 23, 2, 6, 4, 7 }, 6));
 		System.out.println(test.checkSubarraySum(new int[] { 23, 2, 6, 4, 7 }, 13));
 	}
 
 	public boolean checkSubarraySum(int[] nums, int k) {
 		int length = nums.length;
 		int sum = nums[length - 1];
-		for (int idx = length - 2; idx >= 0; idx--) {
-			if (nums[idx] == 0 && nums[idx + 1] == 0) {
+		for (int i = length - 2; i >= 0; i--) {
+			if (nums[i] == 0 && nums[i + 1] == 0) {
 				return true;
 			} else {
-				sum += nums[idx];
+				sum += nums[i];
 			}
-			if (sum >= k || sum == 0) {
-				int subSum = nums[idx];
-				for (int j = idx + 1; j < length; j++) {
+			if (sum == 0 || sum >= k) {
+				int subSum = nums[i];
+				for (int j = i + 1; j < length; j++) {
 					subSum += nums[j];
 					if (subSum % k == 0) {
 						return true;
