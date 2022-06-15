3b699880ebbcb006dbb7dddb871556cf1ecca9c1
yongsik <mymyyoo@naver.com>
2022-06-15 19:41:02 +0900

    Add code about Leetcode - Minimum Absolute Difference in BST

---
+++ src/main/java/gracefulsoul/problems/MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,33 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.object.node.TreeNode;
+
+public class MinimumAbsoluteDifferenceInBST {
+
+	// https://leetcode.com/submissions/detail/722793042/
+	public static void main(String[] args) {
+		System.out.println(new MinimumAbsoluteDifferenceInBST().getMinimumDifference(new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6))));
+		System.out.println(new MinimumAbsoluteDifferenceInBST().getMinimumDifference(new TreeNode(1, new TreeNode(0), new TreeNode(48, new TreeNode(12), new TreeNode(49)))));
+	}
+
+	private int min = Integer.MAX_VALUE;
+
+	public int getMinimumDifference(TreeNode root) {
+		this.dfs(root, null);
+		return this.min;
+	}
+
+	private TreeNode dfs(TreeNode root, TreeNode prev) {
+		if (root == null) {
+			return prev;
+		}
+		prev = this.dfs(root.left, prev);
+		if (prev != null) {
+			this.min = Math.min(this.min, Math.abs(root.val - prev.val));
+		}
+		prev = root;
+		prev = this.dfs(root.right, prev);
+		return prev;
+	}
+
+}
