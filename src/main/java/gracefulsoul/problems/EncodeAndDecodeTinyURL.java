3e8d068b352a9adff03519d8fbce6e94b498842e
yongsik <mymyyoo@naver.com>
2022-06-17 18:46:33 +0900

    Add blog about Leetcode - Encode and Decode TinyURL

--- src/main/java/gracefulsoul/problems/EncodeAndDecodeTinyURL.java
+++ src/main/java/gracefulsoul/problems/EncodeAndDecodeTinyURL.java
@@ -5,10 +5,11 @@ import java.util.Map;
 
 public class EncodeAndDecodeTinyURL {
 
-	// https://leetcode.com/submissions/detail/724321742/
+	// https://leetcode.com/submissions/detail/724327017/
 	public static void main(String[] args) {
 		Codec obj = new Codec();
-		String tiny = obj.encode("https://leetcode.com/problems/design-tinyurl"); // returns the encoded tiny url.
+		String key = "https://leetcode.com/problems/design-tinyurl";
+		String tiny = obj.encode(key); // returns the encoded tiny url.
 		System.out.println(tiny);
 		String ans = obj.decode(tiny); // returns the original url after deconding it.
 		System.out.println(ans);
@@ -19,18 +20,18 @@ public class EncodeAndDecodeTinyURL {
 class Codec {
 
 	private Map<Integer, String> map = new HashMap<>();
+	private static final String URL = "http://tinyurl.com/";
 
 	// Encodes a URL to a shortened URL.
 	public String encode(String longUrl) {
 		int key = longUrl.hashCode();
 		this.map.put(key, longUrl);
-		return String.valueOf(key);
-
+		return URL.concat(String.valueOf(key));
 	}
 
 	// Decodes a shortened URL to its original URL.
 	public String decode(String shortUrl) {
-		return this.map.get(Integer.parseInt(shortUrl));
+		return this.map.get(Integer.parseInt(shortUrl.replace(URL, "")));
 	}
 
 }
