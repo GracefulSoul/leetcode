32a3ef84b63db1721914cc51bda8e621e979f9ee
yongsik <mymyyoo@naver.com>
2022-06-20 19:46:18 +0900

    Add code about Leetcode - Convert BST to Greater Tree

--- src/main/java/gracefulsoul/problems/ConvertBSTToGreaterTree.java
+++ src/main/java/gracefulsoul/problems/ConvertBSTToGreaterTree.java
@@ -5,7 +5,7 @@ import gracefulsoul.util.PrintUtil;
 
 public class ConvertBSTToGreaterTree {
 
-	// https://leetcode.com/submissions/detail/726648870/
+	// https://leetcode.com/submissions/detail/726655860/
 	public static void main(String[] args) {
 		PrintUtil.print(new ConvertBSTToGreaterTree().convertBST(new TreeNode(4, new TreeNode(1, new TreeNode(0), new TreeNode(2, null, new TreeNode(3))), new TreeNode(6, new TreeNode(5), new TreeNode(7, null, new TreeNode(8))))), true, false);
 		PrintUtil.print(new ConvertBSTToGreaterTree().convertBST(new TreeNode(0, null, new TreeNode(1))), true, false);
@@ -14,15 +14,13 @@ public class ConvertBSTToGreaterTree {
 	private int sum = 0;
 
 	public TreeNode convertBST(TreeNode root) {
-		if (root == null) {
-			return null;
-		} else {
+		if (root != null) {
 			this.convertBST(root.right);
 			sum += root.val;
 			root.val = sum;
 			this.convertBST(root.left);
-			return root;
 		}
+		return root;
 	}
 
 }
