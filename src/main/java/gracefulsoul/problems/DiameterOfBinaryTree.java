bad359cd8e34e408c2d86add9ccecb59a8143cf9
yongsik <mymyyoo@naver.com>
2022-06-25 09:41:36 +0900

    Add code about Leetcode - Diameter of Binary Tree

---
+++ src/main/java/gracefulsoul/problems/DiameterOfBinaryTree.java
@@ -0,0 +1,29 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.object.node.TreeNode;
+
+public class DiameterOfBinaryTree {
+
+	public static void main(String[] args) {
+		DiameterOfBinaryTree test = new DiameterOfBinaryTree();
+		System.out.println(test.diameterOfBinaryTree(new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3))));
+		System.out.println(test.diameterOfBinaryTree(new TreeNode(1, new TreeNode(2), null)));
+	}
+
+	public int diameterOfBinaryTree(TreeNode root) {
+		int[] result = new int[1];
+		this.dfs(root, result);
+		return result[0];
+	}
+
+	private int dfs(TreeNode root, int[] result) {
+		if (root == null) {
+			return -1;
+		}
+		int left = this.dfs(root.left, result);
+		int right = this.dfs(root.right, result);
+		result[0] = Math.max(result[0], left + right + 2);
+		return Math.max(left + 1, right + 1);
+	}
+
+}
