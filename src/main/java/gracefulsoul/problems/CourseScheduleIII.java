be6b3a257bbc4d2c667071f3054b8a3edf947d3f
yongsik <mymyyoo@naver.com>
2022-08-20 09:09:18 +0900

    Add code about Leetcode - Course Schedule III

---
+++ src/main/java/gracefulsoul/problems/CourseScheduleIII.java
@@ -0,0 +1,40 @@
+package gracefulsoul.problems;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class CourseScheduleIII {
+
+	public static void main(String[] args) {
+		CourseScheduleIII test = new CourseScheduleIII();
+		System.out.println(test.scheduleCourse(new int[][] {
+			new int[] { 100, 200 },
+			new int[] { 200, 1300 },
+			new int[] { 1000, 1250 },
+			new int[] { 2000, 3200 }
+		}));
+		System.out.println(test.scheduleCourse(new int[][] {
+			new int[] { 1, 2 }
+		}));
+		System.out.println(test.scheduleCourse(new int[][] {
+			new int[] { 3, 2 },
+			new int[] { 4, 3 }
+		}));
+	}
+
+	public int scheduleCourse(int[][] courses) {
+		Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+		Queue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+		int time = 0;
+		for (int[] course : courses) {
+			time += course[0];
+			queue.add(course[0]);
+			if (time > course[1]) {
+				time -= queue.poll();
+			}
+		}
+		return queue.size();
+	}
+
+}
