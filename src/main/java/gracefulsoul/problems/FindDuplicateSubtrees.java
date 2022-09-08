512c891dc23add69ad8738827a7c25c3a2f4c8da
yongsik <mymyyoo@naver.com>
2022-09-08 19:21:23 +0900

    Add code about Leetcode - Find Duplicate Subtrees

--- src/main/java/gracefulsoul/problems/FindDuplicateSubtrees.java
+++ src/main/java/gracefulsoul/problems/FindDuplicateSubtrees.java
@@ -10,7 +10,7 @@ import gracefulsoul.util.PrintUtil;
 
 public class FindDuplicateSubtrees {
 
-	// https://leetcode.com/submissions/detail/794631426/
+	// https://leetcode.com/submissions/detail/794641421/
 	public static void main(String[] args) {
 		FindDuplicateSubtrees test = new FindDuplicateSubtrees();
 		PrintUtil.print(test.findDuplicateSubtrees(new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3, new TreeNode(2, new TreeNode(4), null), new TreeNode(4)))));
@@ -19,27 +19,27 @@ public class FindDuplicateSubtrees {
 	}
 
 	public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-	    List<TreeNode> result = new ArrayList<>();
-	    this.dfs(root, new HashMap<>(), result);
-	    return result;
+		List<TreeNode> result = new ArrayList<>();
+		this.dfs(root, new HashMap<>(), result);
+		return result;
 	}
 
 	private String dfs(TreeNode treeNode, Map<String, Integer> map, List<TreeNode> result) {
-	    if (treeNode == null) {
-	    	return "#";  
-	    }
-	    String subTree = new StringBuilder()
-	    		.append(treeNode.val)
-	    		.append(",")
-	    		.append(this.dfs(treeNode.left, map, result))
-	    		.append(",")
-	    		.append(this.dfs(treeNode.right, map, result))
-	    		.toString();
-	    map.put(subTree, map.getOrDefault(subTree, 0) + 1);
-	    if (map.get(subTree) == 2) {
-	    	result.add(treeNode);
-	    }
-	    return subTree;
+		if (treeNode == null) {
+			return ".";
+		}
+		String subTree = new StringBuilder()
+				.append(treeNode.val)
+				.append(",")
+				.append(this.dfs(treeNode.left, map, result))
+				.append(",")
+				.append(this.dfs(treeNode.right, map, result))
+				.toString();
+		map.put(subTree, map.getOrDefault(subTree, 0) + 1);
+		if (map.get(subTree) == 2) {
+			result.add(treeNode);
+		}
+		return subTree;
 	}
 
 }
