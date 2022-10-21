1280c6fcf7da656c08c130ea22c38c42e05f6b76
yongsik <mymyyoo@naver.com>
2022-10-22 08:42:35 +0900

    Add code about Leetcode - Partition to K Equal Sum Subsets

--- src/main/java/gracefulsoul/problems/PartitionToKEqualSumSubsets.java
+++ src/main/java/gracefulsoul/problems/PartitionToKEqualSumSubsets.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class PartitionToKEqualSumSubsets {
 
-	// https://leetcode.com/submissions/detail/827259091/
+	// https://leetcode.com/submissions/detail/827620530/
 	public static void main(String[] args) {
 		PartitionToKEqualSumSubsets test = new PartitionToKEqualSumSubsets();
 		System.out.println(test.canPartitionKSubsets(new int[] { 4, 3, 2, 3, 5, 2, 1 }, 4));
@@ -17,25 +17,23 @@ public class PartitionToKEqualSumSubsets {
 		if (sum % k != 0 || nums[nums.length - 1] > sum / k) {
 			return false;
 		}
-		int[] bucket = new int[k];
-		int target = sum / k;
-		return this.dfs(nums, target, bucket, nums.length - 1);
+		return this.dfs(nums, sum / k, new int[k], nums.length - 1);
 	}
 
 	private boolean dfs(int nums[], int target, int[] bucket, int index) {
 		if (index == -1) {
 			return true;
 		}
-		for (int i = 0; i < bucket.length; i++) {
-			if ((nums[index] + bucket[i] > target) || (i > 0 && bucket[i] == bucket[i - 1])) {
+		for (int idx = 0; idx < bucket.length; idx++) {
+			if ((nums[index] + bucket[idx] > target) || (idx > 0 && bucket[idx] == bucket[idx - 1])) {
 				continue;
 			}
-			bucket[i] += nums[index];
+			bucket[idx] += nums[index];
 			if (this.dfs(nums, target, bucket, index - 1)) {
 				return true;
 			}
-			bucket[i] -= nums[index];
-			if (bucket[i] == 0) {
+			bucket[idx] -= nums[index];
+			if (bucket[idx] == 0) {
 				break;
 			}
 		}
