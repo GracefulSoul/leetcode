14bd643c348bbc68e4638aace31f190ef125cfed
yongsik <mymyyoo@naver.com>
2022-11-06 08:59:11 +0900

    Add code about Leetcode - 1-bit and 2-bit Characters

--- src/main/java/gracefulsoul/problems/OneBitAndTwoBitCharacters.java
+++ src/main/java/gracefulsoul/problems/OneBitAndTwoBitCharacters.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class OneBitAndTwoBitCharacters {
 
-	// https://leetcode.com/submissions/detail/837659403/
+	// https://leetcode.com/submissions/detail/837662377/
 	public static void main(String[] args) {
 		OneBitAndTwoBitCharacters test = new OneBitAndTwoBitCharacters();
 		System.out.println(test.isOneBitCharacter(new int[] { 1, 0, 0 }));
@@ -11,7 +11,7 @@ public class OneBitAndTwoBitCharacters {
 
 	public boolean isOneBitCharacter(int[] bits) {
 		int bit = 0;
-		for (int i = bits.length - 2; i >= 0 && bits[i] != 0; i--) {
+		for (int idx = bits.length - 2; idx >= 0 && bits[idx] != 0; idx--) {
 			bit++;
 		}
 		return bit % 2 <= 0;
