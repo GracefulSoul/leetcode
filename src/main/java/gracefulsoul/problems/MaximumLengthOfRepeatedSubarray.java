dd8c9e84e0fa6c2685170648692e2305d5ba519d
yongsik <mymyyoo@naver.com>
2022-11-07 18:20:47 +0900

    Add code about Leetcode - Maximum Length of Repeated Subarray

--- src/main/java/gracefulsoul/problems/MaximumLengthOfRepeatedSubarray.java
+++ src/main/java/gracefulsoul/problems/MaximumLengthOfRepeatedSubarray.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class MaximumLengthOfRepeatedSubarray {
 
-	// https://leetcode.com/submissions/detail/838592862/
+	// https://leetcode.com/submissions/detail/838597570/
 	public static void main(String[] args) {
 		MaximumLengthOfRepeatedSubarray test = new MaximumLengthOfRepeatedSubarray();
 		System.out.println(test.findLength(new int[] { 1, 2, 3, 2, 1 }, new int[] { 3, 2, 1, 4, 7 }));
@@ -10,12 +10,12 @@ public class MaximumLengthOfRepeatedSubarray {
 	}
 
 	public int findLength(int[] nums1, int[] nums2) {
-		int num1Length = nums1.length;
-		int num2Length = nums2.length;
-		int[][] dp = new int[num1Length + 1][num2Length + 1];
+		int nums1Length = nums1.length;
+		int nums2Length = nums2.length;
+		int[][] dp = new int[nums1Length + 1][nums2Length + 1];
 		int max = 0;
-		for (int i = 0; i < num1Length; i++) {
-			for (int j = 0; j < num2Length; j++) {
+		for (int i = 0; i < nums1Length; i++) {
+			for (int j = 0; j < nums2Length; j++) {
 				if (nums1[i] == nums2[j]) {
 					dp[i + 1][j + 1] = dp[i][j] + 1;
 					max = Math.max(max, dp[i + 1][j + 1]);
