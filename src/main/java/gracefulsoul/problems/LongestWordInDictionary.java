12cfb3c8075e240b366e10e4e1486f12720f2bf9
yongsik <mymyyoo@naver.com>
2022-11-09 19:29:37 +0900

    Add code about Leetcode - Longest Word in Dictionary

--- src/main/java/gracefulsoul/problems/LongestWordInDictionary.java
+++ src/main/java/gracefulsoul/problems/LongestWordInDictionary.java
@@ -1,12 +1,11 @@
 package gracefulsoul.problems;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class LongestWordInDictionary {
 
-	// https://leetcode.com/submissions/detail/840020420/
+	// https://leetcode.com/submissions/detail/840021618/
 	public static void main(String[] args) {
 		LongestWordInDictionary test = new LongestWordInDictionary();
 		System.out.println(test.longestWord(new String[] { "w", "wo", "wor", "worl", "world" }));
@@ -14,16 +13,25 @@ public class LongestWordInDictionary {
 	}
 
 	public String longestWord(String[] words) {
-		Arrays.sort(words);
-		Set<String> set = new HashSet<String>();
 		String result = "";
+		Set<String> set = new HashSet<>();
 		for (String word : words) {
-			int length = word.length();
-			if (length == 1 || set.contains(word.substring(0, length - 1))) {
-				if (length > result.length()) {
+			set.add(word);
+		}
+		for (String word : words) {
+			int wordLength = word.length();
+			int longestLength = result.length();
+			if (wordLength > longestLength || (wordLength == longestLength && word.compareTo(result) < 0)) {
+				boolean isLongestWord = true;
+				for (int idx = 1; idx < wordLength; idx++) {
+					if (!set.contains(word.substring(0, idx))) {
+						isLongestWord = false;
+						break;
+					}
+				}
+				if (isLongestWord) {
 					result = word;
 				}
-				set.add(word);
 			}
 		}
 		return result;
