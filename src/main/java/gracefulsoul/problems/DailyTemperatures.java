6085070ce25cfda72288d6faadcf9087faadbe6e
yongsik <mymyyoo@naver.com>
2022-11-28 14:36:26 +0900

    Add code about Leetcode - Daily Temperatures

--- src/main/java/gracefulsoul/problems/DailyTemperatures.java
+++ src/main/java/gracefulsoul/problems/DailyTemperatures.java
@@ -4,7 +4,7 @@ import gracefulsoul.util.PrintUtil;
 
 public class DailyTemperatures {
 
-	// https://leetcode.com/submissions/detail/851036659/
+	// https://leetcode.com/submissions/detail/851040119/
 	public static void main(String[] args) {
 		DailyTemperatures test = new DailyTemperatures();
 		PrintUtil.print(test.dailyTemperatures(new int[] { 73, 74, 75, 71, 69, 72, 76, 73 }));
@@ -14,21 +14,21 @@ public class DailyTemperatures {
 
 	public int[] dailyTemperatures(int[] temperatures) {
 		int length = temperatures.length;
-		int hottest = 0;
-		int answer[] = new int[length];
+		int lastTemperature = 0;
+		int[] result = new int[length];
 		for (int idx = length - 1; idx >= 0; idx--) {
-			int temperature = temperatures[idx];
-			if (temperature >= hottest) {
-				hottest = temperature;
+			int currentTemperature = temperatures[idx];
+			if (currentTemperature >= lastTemperature) {
+				lastTemperature = currentTemperature;
 				continue;
 			}
 			int days = 1;
-			while (temperatures[idx + days] <= temperature) {
-				days += answer[idx + days];
+			while (temperatures[idx + days] <= currentTemperature) {
+				days += result[idx + days];
 			}
-			answer[idx] = days;
+			result[idx] = days;
 		}
-		return answer;
+		return result;
 	}
 
 }
