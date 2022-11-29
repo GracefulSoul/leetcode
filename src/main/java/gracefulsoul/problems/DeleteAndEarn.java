857341e362bd806e293bd8059fcf707c5e2e6f6a
yongsik <mymyyoo@naver.com>
2022-11-29 12:52:59 +0900

    Add code about Leetcode - Delete and Earn

---
+++ src/main/java/gracefulsoul/problems/DeleteAndEarn.java
@@ -0,0 +1,26 @@
+package gracefulsoul.problems;
+
+public class DeleteAndEarn {
+
+	// https://leetcode.com/submissions/detail/851527464/
+	public static void main(String[] args) {
+		DeleteAndEarn test = new DeleteAndEarn();
+		System.out.println(test.deleteAndEarn(new int[] { 3, 4, 2 }));
+		System.out.println(test.deleteAndEarn(new int[] { 2, 2, 3, 3, 3, 4 }));
+	}
+
+	public int deleteAndEarn(int[] nums) {
+		int n = 10001;
+		int[] dp = new int[n];
+		int max = 0;
+		for (int num : nums) {
+			dp[num] = dp[num] + num;
+			max = Math.max(num, max);
+		}
+		for (int idx = 2; idx <= max; idx++) {
+			dp[idx] = Math.max(dp[idx - 1], dp[idx] + dp[idx - 2]);
+		}
+		return dp[max];
+	}
+
+}
