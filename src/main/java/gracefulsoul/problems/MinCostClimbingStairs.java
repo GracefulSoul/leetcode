cf10030db5463394c51a2e61b3b8f61c190960f1
yongsik <mymyyoo@naver.com>
2022-12-03 10:54:07 +0900

    Add code about Leetcode - Min Cost Climbing Stairs

--- src/main/java/gracefulsoul/problems/MinCostClimbingStairs.java
+++ src/main/java/gracefulsoul/problems/MinCostClimbingStairs.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class MinCostClimbingStairs {
 
-	// https://leetcode.com/submissions/detail/853663482/
+	// https://leetcode.com/submissions/detail/853669106/
 	public static void main(String[] args) {
 		MinCostClimbingStairs test = new MinCostClimbingStairs();
 		System.out.println(test.minCostClimbingStairs(new int[] { 10, 15, 20 }));
@@ -15,9 +15,9 @@ public class MinCostClimbingStairs {
 		dp[0] = cost[0];
 		dp[1] = cost[1];
 		for (int idx = 2; idx < length; idx++) {
-			dp[idx] = Math.min(dp[idx - 1], dp[idx - 2]) + cost[idx];
+			dp[idx] = Math.min(dp[idx - 2], dp[idx - 1]) + cost[idx];
 		}
-		return Math.min(dp[length - 1], dp[length - 2]);
+		return Math.min(dp[length - 2], dp[length - 1]);
 	}
 
 }
