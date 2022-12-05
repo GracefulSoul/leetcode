733694e76f703ec3cd30a04e53604c62af491c3a
yongsik <mymyyoo@naver.com>
2022-12-05 10:51:20 +0900

    Add code about Leetcode - Shortest Completing Word

--- src/main/java/gracefulsoul/problems/ShortestCompletingWord.java
+++ src/main/java/gracefulsoul/problems/ShortestCompletingWord.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class ShortestCompletingWord {
 
-	// https://leetcode.com/submissions/detail/854757214/
+	// https://leetcode.com/submissions/detail/854758252/
 	public static void main(String[] args) {
 		ShortestCompletingWord test = new ShortestCompletingWord();
 		System.out.println(test.shortestCompletingWord("1s3 PSt", new String[] { "step", "steps", "stripe", "stepple" }));
@@ -11,16 +11,16 @@ public class ShortestCompletingWord {
 
 	public String shortestCompletingWord(String licensePlate, String[] words) {
 		int[] licenseChars = this.getCharCounts(licensePlate);
-		String shortest = "";
+		String result = "";
 		for (String word : words) {
-			if ((shortest.isEmpty() || shortest.length() > word.length()) && this.isComplete(licenseChars, this.getCharCounts(word))) {
-				shortest = word;
+			if ((result.isEmpty() || result.length() > word.length()) && this.isCompletingWord(licenseChars, this.getCharCounts(word))) {
+				result = word;
 			}
 		}
-		return shortest;
+		return result;
 	}
 
-	private boolean isComplete(int[] licenseChars, int[] wordChars) {
+	private boolean isCompletingWord(int[] licenseChars, int[] wordChars) {
 		for (int idx = 0; idx < 26; idx++) {
 			if (licenseChars[idx] != 0 && licenseChars[idx] > wordChars[idx]) {
 				return false;
@@ -30,13 +30,13 @@ public class ShortestCompletingWord {
 	}
 
 	private int[] getCharCounts(String s) {
-		int[] chars = new int[26];
+		int[] charCounts = new int[26];
 		for (char c : s.toCharArray()) {
 			if (Character.isLetter(c)) {
-				chars[Character.toLowerCase(c) - 'a']++;
+				charCounts[Character.toLowerCase(c) - 'a']++;
 			}
 		}
-		return chars;
+		return charCounts;
 	}
 
 }
