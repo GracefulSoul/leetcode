7af5fa9b5d03b443508592aef5a838c83ccc09f5
yongsik <mymyyoo@naver.com>
2022-12-06 13:37:34 +0900

    Add code about Leetcode - Open the Lock

---
+++ src/main/java/gracefulsoul/problems/OpenTheLock.java
@@ -0,0 +1,61 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class OpenTheLock {
+
+	// https://leetcode.com/problems/open-the-lock/submissions/855373585/
+	public static void main(String[] args) {
+		OpenTheLock test = new OpenTheLock();
+		System.out.println(test.openLock(new String[] { "0201", "0101", "0102", "1212", "2002" }, "0202"));
+		System.out.println(test.openLock(new String[] { "8888" }, "0009"));
+		System.out.println(test.openLock(new String[] { "8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888" }, "8888"));
+	}
+
+	public int openLock(String[] deadends, String target) {
+		Set<String> start = new HashSet<>();
+		start.add("0000");
+		Set<String> end = new HashSet<>();
+		end.add(target);
+		Set<String> checked = new HashSet<>(Arrays.asList(deadends));
+		int level = 0;
+		while (!start.isEmpty() && !end.isEmpty()) {
+			Set<String> temp = new HashSet<>();
+			for (String curr : start) {
+				if (end.contains(curr)) {
+					return level;
+				} else if (checked.contains(curr)) {
+					continue;
+				} else {
+					checked.add(curr);
+					for (String next : this.getNexts(curr)) {
+						temp.add(next);
+					}
+				}
+			}
+			level++;
+			start = end;
+			end = temp;
+		}
+		return -1;
+	}
+
+	private List<String> getNexts(String lock) {
+		List<String> locks = new ArrayList<>();
+		char[] charArray = lock.toCharArray();
+		for (int idx = 0; idx < charArray.length; idx++) {
+			char c = charArray[idx];
+			charArray[idx] = c == '9' ? '0' : (char) (c + 1);
+			locks.add(String.valueOf(charArray));
+			charArray[idx] = c == '0' ? '9' : (char) (c - 1);
+			locks.add(String.valueOf(charArray));
+			charArray[idx] = c;
+		}
+		return locks;
+	}
+
+}
