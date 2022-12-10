ee233756b45853994be3e3fc7ac348a7fa1311f3
yongsik <mymyyoo@naver.com>
2022-12-10 15:14:01 +0900

    Add code about Leetcode - Set Intersection Size At Least Two

---
+++ src/main/java/gracefulsoul/problems/SetIntersectionSizeAtLeastTwo.java
@@ -0,0 +1,49 @@
+package gracefulsoul.problems;
+
+import java.util.Arrays;
+
+public class SetIntersectionSizeAtLeastTwo {
+
+	// https://leetcode.com/problems/set-intersection-size-at-least-two/submissions/857433553/
+	public static void main(String[] args) {
+		SetIntersectionSizeAtLeastTwo test = new SetIntersectionSizeAtLeastTwo();
+		System.out.println(test.intersectionSizeTwo(new int[][] {
+			{ 1, 3 },
+			{ 3, 7 },
+			{ 8, 9 }
+		}));
+		System.out.println(test.intersectionSizeTwo(new int[][] {
+			{ 1, 3 },
+			{ 1, 4 },
+			{ 2, 5 },
+			{ 3, 5 }
+		}));
+		System.out.println(test.intersectionSizeTwo(new int[][] {
+			{ 1, 2 },
+			{ 2, 3 },
+			{ 2, 4 },
+			{ 4, 5 }
+		}));
+	}
+
+	public int intersectionSizeTwo(int[][] intervals) {
+		Arrays.sort(intervals, (a, b) -> a[1] != b[1] ? a[1] - b[1] : b[0] - a[0]);
+		int size = 0;
+		int start = intervals[0][1] - 1;
+		int end = intervals[0][1];
+		size += 2;
+		for (int idx = 1; idx < intervals.length; idx++) {
+			if (intervals[idx][0] > start && intervals[idx][0] <= end) {
+				size += 1;
+				start = end;
+				end = intervals[idx][1];
+			} else if (intervals[idx][0] > end) {
+				size += 2;
+				start = intervals[idx][1] - 1;
+				end = intervals[idx][1];
+			}
+		}
+		return size;
+	}
+
+}
