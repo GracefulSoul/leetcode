d4ffae955476d58c336e068edd486e06d41f7663
yongsik <mymyyoo@naver.com>
2022-12-14 05:12:43 +0900

    Add code about Leetcode - Partition Labels

---
+++ src/main/java/gracefulsoul/problems/PartitionLabels.java
@@ -0,0 +1,38 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartitionLabels {
+
+	// https://leetcode.com/problems/partition-labels/submissions/859343324/
+	public static void main(String[] args) {
+		PartitionLabels test = new PartitionLabels();
+		System.out.println(test.partitionLabels("ababcbacadefegdehijhklij"));
+		System.out.println(test.partitionLabels("eccbbbbdec"));
+	}
+
+	public List<Integer> partitionLabels(String s) {
+		int[] last = new int[26];
+		char[] charArray = s.toCharArray();
+		int length = charArray.length;
+		for (int idx = 0; idx < length; idx++) {
+			last[charArray[idx] - 'a'] = idx;
+		}
+		List<Integer> result = new ArrayList<>();
+		int start = 0;
+		int end = 0;
+		for (int idx = 0; idx < charArray.length; idx++) {
+			int index = last[charArray[idx] - 'a'];
+			if (index > end) {
+				end = index;
+			}
+			if (end == idx) {
+				result.add(end - start + 1);
+				start = end + 1;
+			}
+		}
+		return result;
+	}
+
+}
