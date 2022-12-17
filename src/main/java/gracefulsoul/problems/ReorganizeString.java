97e6e20d47eb631b79f8609245f99fc1554a182c
yongsik <mymyyoo@naver.com>
2022-12-17 11:13:42 +0900

    Add code about Leetcode - Reorganize String

---
+++ src/main/java/gracefulsoul/problems/ReorganizeString.java
@@ -0,0 +1,49 @@
+package gracefulsoul.problems;
+
+public class ReorganizeString {
+
+	// https://leetcode.com/problems/reorganize-string/submissions/860902867/
+	public static void main(String[] args) {
+		ReorganizeString test = new ReorganizeString();
+		System.out.println(test.reorganizeString("aab"));
+		System.out.println(test.reorganizeString("aaab"));
+	}
+
+	public String reorganizeString(String s) {
+		int length = s.length();
+		int[] hash = new int[26];
+		for (char c : s.toCharArray()) {
+			hash[c - 'a']++;
+		}
+		int max = 0;
+		int letter = 0;
+		for (int idx = 0; idx < hash.length; idx++) {
+			if (hash[idx] > max) {
+				max = hash[idx];
+				letter = idx;
+			}
+		}
+		if (max > (length + 1) / 2) {
+			return "";
+		}
+		char[] result = new char[length];
+		int index = 0;
+		while (hash[letter] > 0) {
+			result[index] = (char) (letter + 'a');
+			index += 2;
+			hash[letter]--;
+		}
+		for (int idx = 0; idx < hash.length; idx++) {
+			while (hash[idx] > 0) {
+				if (index >= result.length) {
+					index = 1;
+				}
+				result[index] = (char) (idx + 'a');
+				index += 2;
+				hash[idx]--;
+			}
+		}
+		return String.valueOf(result);
+	}
+
+}
