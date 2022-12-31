990a6a95ef5d315a9c24cc8d29feadcf5e58f784
yongsik <mymyyoo@naver.com>
2022-12-31 10:45:26 +0900

    Add code about Leetcode - Rotated Digits

--- src/main/java/gracefulsoul/problems/RotatedDigits.java
+++ src/main/java/gracefulsoul/problems/RotatedDigits.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class RotatedDigits {
 
-	// https://leetcode.com/problems/rotated-digits/submissions/868337543/
+	// https://leetcode.com/problems/rotated-digits/submissions/868338568/
 	public static void main(String[] args) {
 		RotatedDigits test = new RotatedDigits();
 		System.out.println(test.rotatedDigits(10));
@@ -11,21 +11,21 @@ public class RotatedDigits {
 	}
 
 	public int rotatedDigits(int n) {
-		String s = String.valueOf(n);
-		return this.getCount(s, 0, new int[] { 0, 1, 2, 5, 6, 8, 9 }) - this.getCount(s, 0, new int[] { 0, 1, 8 });
+		char[] charArray = String.valueOf(n).toCharArray();
+		return this.getCount(charArray, 0, new int[] { 0, 1, 2, 5, 6, 8, 9 }) - this.getCount(charArray, 0, new int[] { 0, 1, 8 });
 	}
 
-	private int getCount(String s, int index, int[] nums) {
-		if (index == s.length()) {
+	private int getCount(char[] charArray, int index, int[] nums) {
+		if (index == charArray.length) {
 			return 1;
 		}
-		int n = s.charAt(index) - '0';
+		int n = charArray[index] - '0';
 		int count = 0;
 		for (int num : nums) {
 			if (num < n) {
-				count += Math.pow(nums.length, s.length() - 1 - index);
+				count += Math.pow(nums.length, charArray.length - 1 - index);
 			} else if (num == n) {
-				count += this.getCount(s, index + 1, nums);
+				count += this.getCount(charArray, index + 1, nums);
 			}
 		}
 		return count;
