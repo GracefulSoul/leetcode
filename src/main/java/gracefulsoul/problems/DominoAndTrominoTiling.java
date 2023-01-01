0f7fb3040574cd4400d51f0b2678eea15a80fb22
yongsik <mymyyoo@naver.com>
2023-01-01 18:08:32 +0900

    Add code about Leetcode - Domino and Tromino Tiling

--- src/main/java/gracefulsoul/problems/DominoAndTrominoTiling.java
+++ src/main/java/gracefulsoul/problems/DominoAndTrominoTiling.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class DominoAndTrominoTiling {
 
-	// https://leetcode.com/problems/domino-and-tromino-tiling/submissions/868970313/
+	// https://leetcode.com/problems/domino-and-tromino-tiling/submissions/868975974/
 	public static void main(String[] args) {
 		DominoAndTrominoTiling test = new DominoAndTrominoTiling();
 		System.out.println(test.numTilings(3));
@@ -17,8 +17,8 @@ public class DominoAndTrominoTiling {
 		dp[0] = 1;
 		dp[1] = 1;
 		dp[2] = 2;
-		for (int i = 3; i < n + 1; i++) {
-			dp[i] = (2 * dp[i - 1] + dp[i - 3]) % 1000000007;
+		for (int idx = 3; idx < n + 1; idx++) {
+			dp[idx] = (2 * dp[idx - 1] + dp[idx - 3]) % 1000000007;
 		}
 		return (int) dp[n];
 	}
