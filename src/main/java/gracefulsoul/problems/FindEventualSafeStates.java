8755367714e1bd0aeb965d12bafc4bf5c7241726
yongsik <mymyyoo@naver.com>
2023-01-13 19:15:59 +0900

    Add code about Leetcode - Find Eventual Safe States

---
+++ src/main/java/gracefulsoul/problems/FindEventualSafeStates.java
@@ -0,0 +1,56 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindEventualSafeStates {
+
+	// https://leetcode.com/problems/find-eventual-safe-states/submissions/877361264/
+	public static void main(String[] args) {
+		FindEventualSafeStates test = new FindEventualSafeStates();
+		System.out.println(test.eventualSafeNodes(new int[][] {
+			{ 1, 2 },
+			{ 2, 3 },
+			{ 5 },
+			{ 0 },
+			{ 5 },
+			{},
+			{}
+		}));
+		System.out.println(test.eventualSafeNodes(new int[][] {
+			{ 1, 2, 3, 4 },
+			{ 1, 2 },
+			{ 3, 4 },
+			{ 0, 4 },
+			{}
+		}));
+	}
+
+	public List<Integer> eventualSafeNodes(int[][] graph) {
+		List<Integer> result = new ArrayList<>();
+		int length = graph.length;
+		int[] color = new int[length];
+		for (int idx = 0; idx < length; idx++) {
+			if (this.dfs(graph, idx, color)) {
+				result.add(idx);
+			}
+		}
+		return result;
+	}
+
+	private boolean dfs(int[][] graph, int start, int[] color) {
+		if (color[start] != 0) {
+			return color[start] == 1;
+		} else {
+			color[start] = 2;
+			for (int node : graph[start]) {
+				if (!this.dfs(graph, node, color)) {
+					return false;
+				}
+			}
+			color[start] = 1;
+			return true;
+		}
+	}
+
+}
