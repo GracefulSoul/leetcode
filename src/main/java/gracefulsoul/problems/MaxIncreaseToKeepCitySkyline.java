b7e2e87c49cdd9dd4a81963f48f5c0389ab4e4a0
yongsik <mymyyoo@naver.com>
2023-01-16 19:17:50 +0900

    Add code about Leetcode - Max Increase to Keep City Skyline

--- src/main/java/gracefulsoul/problems/MaxIncreaseToKeepCitySkyline.java
+++ src/main/java/gracefulsoul/problems/MaxIncreaseToKeepCitySkyline.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class MaxIncreaseToKeepCitySkyline {
 
-	// https://leetcode.com/problems/max-increase-to-keep-city-skyline/submissions/879133712/
+	// https://leetcode.com/problems/max-increase-to-keep-city-skyline/submissions/879135989/
 	public static void main(String[] args) {
 		MaxIncreaseToKeepCitySkyline test = new MaxIncreaseToKeepCitySkyline();
 		System.out.println(test.maxIncreaseKeepingSkyline(new int[][] {
@@ -20,8 +20,8 @@ public class MaxIncreaseToKeepCitySkyline {
 
 	public int maxIncreaseKeepingSkyline(int[][] grid) {
 		int length = grid.length;
-		int[] col = new int[length];
 		int[] row = new int[length];
+		int[] col = new int[length];
 		for (int i = 0; i < length; i++) {
 			for (int j = 0; j < length; j++) {
 				row[i] = Math.max(row[i], grid[i][j]);
