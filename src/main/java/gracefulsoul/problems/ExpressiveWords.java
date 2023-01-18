8414bdb9ada798082d9fdab3454fbfbe4ae67997
yongsik <mymyyoo@naver.com>
2023-01-18 19:38:27 +0900

    Add code about Leetcode - Expressive Words

---
+++ src/main/java/gracefulsoul/problems/ExpressiveWords.java
@@ -0,0 +1,37 @@
+package gracefulsoul.problems;
+
+public class ExpressiveWords {
+
+	// https://leetcode.com/problems/expressive-words/submissions/880487470/
+	public static void main(String[] args) {
+		ExpressiveWords test = new ExpressiveWords();
+		System.out.println(test.expressiveWords("heeellooo", new String[] { "hello", "hi", "helo" }));
+		System.out.println(test.expressiveWords("zzzzzyyyyy", new String[] { "zzyy", "zy", "zyy" }));
+	}
+
+	public int expressiveWords(String s, String[] words) {
+		int result = 0;
+		for (String word : words) {
+			if (this.isPossible(s.toCharArray(), word.toCharArray())) {
+				result++;
+			}
+		}
+		return result;
+	}
+
+	private boolean isPossible(char[] sCharArray, char[] wordCharArray) {
+		int sLength = sCharArray.length;
+		int wordLength = wordCharArray.length;
+		int j = 0;
+		for (int i = 0; i < sLength; i++) {
+			if (j < wordLength && sCharArray[i] == wordCharArray[j]) {
+				j++;
+			} else if (!(1 < i && sCharArray[i - 1] == sCharArray[i] && sCharArray[i - 2] == sCharArray[i - 1])
+					&& !(0 < i && i < sLength - 1 && sCharArray[i - 1] == sCharArray[i] && sCharArray[i] == sCharArray[i + 1])) {
+				return false;
+			}
+		}
+		return j == wordLength;
+	}
+
+}
