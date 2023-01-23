35b15f719867d007ddb370a4b89bea3913b2d00f
yongsik <mymyyoo@naver.com>
2023-01-23 18:33:32 +0900

    Add code about Leetcode - Largest Sum of Averages

--- src/main/java/gracefulsoul/problems/LargestSumOfAverages.java
+++ src/main/java/gracefulsoul/problems/LargestSumOfAverages.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class LargestSumOfAverages {
 
-	// https://leetcode.com/problems/largest-sum-of-averages/submissions/883591133/
+	// https://leetcode.com/problems/largest-sum-of-averages/submissions/883597625/
 	public static void main(String[] args) {
 		LargestSumOfAverages test = new LargestSumOfAverages();
 		System.out.println(test.largestSumOfAverages(new int[] { 9, 1, 2, 3, 9 }, 3));
@@ -17,10 +17,10 @@ public class LargestSumOfAverages {
 			sum += nums[idx];
 			dp[idx][1] = sum / (length - idx);
 		}
-		return this.getMaximumScore(nums, length, dp, 0, k);
+		return this.getMaximumScore(nums, k, length, dp, 0);
 	}
 
-	private double getMaximumScore(int[] nums, int length, double[][] dp, int start, int k) {
+	private double getMaximumScore(int[] nums, int k, int length, double[][] dp, int start) {
 		if (dp[start][k] == 0) {
 			double sum = 0;
 			for (int idx = start; idx < length; idx++) {
@@ -28,7 +28,7 @@ public class LargestSumOfAverages {
 					break;
 				}
 				sum += nums[idx];
-				dp[start][k] = Math.max((sum / (idx + 1 - start)) + this.getMaximumScore(nums, length, dp, idx + 1, k - 1), dp[start][k]);
+				dp[start][k] = Math.max((sum / (idx + 1 - start)) + this.getMaximumScore(nums, k - 1, length, dp, idx + 1), dp[start][k]);
 			}
 		}
 		return dp[start][k];
