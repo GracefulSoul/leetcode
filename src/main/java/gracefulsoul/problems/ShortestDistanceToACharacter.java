c6f9dc9f38b38a741fec18feff7f02ffe578043b
yongsik <mymyyoo@naver.com>
2023-01-29 09:04:32 +0900

    Add code about Leetcode - Shortest Distance to a Character

--- src/main/java/gracefulsoul/problems/ShortestDistanceToACharacter.java
+++ src/main/java/gracefulsoul/problems/ShortestDistanceToACharacter.java
@@ -1,26 +1,27 @@
 package gracefulsoul.problems;
 
+import gracefulsoul.util.PrintUtil;
+
 public class ShortestDistanceToACharacter {
 
-	// https://leetcode.com/problems/shortest-distance-to-a-character/submissions/887107369/
+	// https://leetcode.com/problems/shortest-distance-to-a-character/submissions/887111161/
 	public static void main(String[] args) {
 		ShortestDistanceToACharacter test = new ShortestDistanceToACharacter();
-		System.out.println(test.shortestToChar("loveleetcode", 'e'));
-		System.out.println(test.shortestToChar("aaab", 'b'));
+		PrintUtil.print(test.shortestToChar("loveleetcode", 'e'));
+		PrintUtil.print(test.shortestToChar("aaab", 'b'));
 	}
 
 	public int[] shortestToChar(String s, char c) {
 		int length = s.length();
 		int[] result = new int[length];
-		int prev = Integer.MIN_VALUE / 2;
+		int prev = -length;
 		for (int idx = 0; idx < length; idx++) {
 			if (s.charAt(idx) == c) {
 				prev = idx;
 			}
 			result[idx] = idx - prev;
 		}
-		prev = Integer.MAX_VALUE / 2;
-		for (int idx = length - 1; idx >= 0; idx--) {
+		for (int idx = prev - 1; idx >= 0; idx--) {
 			if (s.charAt(idx) == c) {
 				prev = idx;
 			}
