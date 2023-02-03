b0ed9a7e85e89bbb0ce521865658f649a56c1eed
yongsik <mymyyoo@naver.com>
2023-02-04 08:32:54 +0900

    Add code about Leetcode - Count Unique Characters of All Substrings of a Given String

--- src/main/java/gracefulsoul/problems/CountUniqueCharactersOfAllSubstringsOfAGivenString.java
+++ src/main/java/gracefulsoul/problems/CountUniqueCharactersOfAllSubstringsOfAGivenString.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CountUniqueCharactersOfAllSubstringsOfAGivenString {
 
-	// https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/submissions/890965571/
+	// https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/submissions/890967001/
 	public static void main(String[] args) {
 		CountUniqueCharactersOfAllSubstringsOfAGivenString test = new CountUniqueCharactersOfAllSubstringsOfAGivenString();
 		System.out.println(test.uniqueLetterString("ABC"));
@@ -13,18 +13,18 @@ public class CountUniqueCharactersOfAllSubstringsOfAGivenString {
 	}
 
 	public int uniqueLetterString(String s) {
+		int[] curr = new int[26];
+		Arrays.fill(curr, -1);
 		int[] last = new int[26];
 		Arrays.fill(last, -1);
-		int[] secondLast = new int[26];
-		Arrays.fill(secondLast, -1);
 		int j;
 		int count = 0;
 		int result = 0;
 		for (int i = 0; i < s.length(); i++) {
 			j = s.charAt(i) - 'A';
-			count += i - last[j] + secondLast[j] - last[j];
-			secondLast[j] = last[j];
-			last[j] = i;
+			count += i - curr[j] + last[j] - curr[j];
+			last[j] = curr[j];
+			curr[j] = i;
 			result += count;
 		}
 		return result;
