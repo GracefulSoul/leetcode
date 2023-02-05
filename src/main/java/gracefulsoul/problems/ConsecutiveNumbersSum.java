fe2ac3d6f0675d5d4908506dfeff6bbdb5237e46
yongsik <mymyyoo@naver.com>
2023-02-05 14:23:18 +0900

    Add code about Leetcode - Consecutive Numbers Sum

---
+++ src/main/java/gracefulsoul/problems/ConsecutiveNumbersSum.java
@@ -0,0 +1,28 @@
+package gracefulsoul.problems;
+
+public class ConsecutiveNumbersSum {
+
+	// https://leetcode.com/problems/consecutive-numbers-sum/submissions/891802801/
+	public static void main(String[] args) {
+		ConsecutiveNumbersSum test = new ConsecutiveNumbersSum();
+		System.out.println(test.consecutiveNumbersSum(5));
+		System.out.println(test.consecutiveNumbersSum(9));
+		System.out.println(test.consecutiveNumbersSum(15));
+	}
+
+	public int consecutiveNumbersSum(int n) {
+        int result = 1;
+        int count;
+        while (n % 2 == 0) n /= 2;
+        for (int idx = 3; idx * idx <= n; idx += 2) {
+            count = 0;
+            while (n % idx == 0) {
+                n /= idx;
+                count++;
+            }
+            result *= count + 1;
+        }
+        return n == 1 ? result : result * 2;
+	}
+
+}
