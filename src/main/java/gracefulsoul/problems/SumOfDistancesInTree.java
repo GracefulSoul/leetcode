82bf91c0118c9801faefede4d76ce49e7e6badc8
yongsik <mymyyoo@naver.com>
2023-02-09 21:29:06 +0900

    Add code about Leetcode - Sum of Distances in Tree

--- src/main/java/gracefulsoul/problems/SumOfDistancesInTree.java
+++ src/main/java/gracefulsoul/problems/SumOfDistancesInTree.java
@@ -2,10 +2,11 @@ package gracefulsoul.problems;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class SumOfDistancesInTree {
 
-	// https://leetcode.com/problems/sum-of-distances-in-tree/submissions/894666182/
+	// https://leetcode.com/problems/sum-of-distances-in-tree/submissions/894679668/
 	public static void main(String[] args) {
 		SumOfDistancesInTree test = new SumOfDistancesInTree();
 		System.out.println(test.sumOfDistancesInTree(6, new int[][] {
@@ -24,25 +25,23 @@ public class SumOfDistancesInTree {
 
 	@SuppressWarnings("unchecked")
 	public int[] sumOfDistancesInTree(int n, int[][] edges) {
-		ArrayList<Integer>[] graph = new ArrayList[n];
 		int[] count = new int[n];
 		Arrays.fill(count, 1);
 		int[] result = new int[n];
+		List<Integer>[] graph = new ArrayList[n];
 		for (int i = 0; i < n; i++) {
-			graph[i] = new ArrayList<Integer>();
+			graph[i] = new ArrayList<>();
 		}
 		for (int[] edge : edges) {
 			graph[edge[0]].add(edge[1]);
 			graph[edge[1]].add(edge[0]);
-
 		}
 		this.postOrderDFS(graph, count, result, 0, -1);
 		this.preOrderDFS(graph, n, count, result, 0, -1);
 		return result;
-
 	}
 
-	private void postOrderDFS(ArrayList<Integer>[] graph, int[] count, int[] result, int a, int parent) {
+	private void postOrderDFS(List<Integer>[] graph, int[] count, int[] result, int a, int parent) {
 		for (int b : graph[a]) {
 			if (b != parent) {
 				this.postOrderDFS(graph, count, result, b, a);
@@ -52,7 +51,7 @@ public class SumOfDistancesInTree {
 		}
 	}
 
-	private void preOrderDFS(ArrayList<Integer>[] graph, int n, int[] count, int[] result, int a, int parent) {
+	private void preOrderDFS(List<Integer>[] graph, int n, int[] count, int[] result, int a, int parent) {
 		for (int b : graph[a]) {
 			if (b != parent) {
 				result[b] = result[a] + (n - count[b]) - count[b];
