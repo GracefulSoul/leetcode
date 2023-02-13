40a2c6f20f130a92aee1108cffeec085b5801f60
yongsik <mymyyoo@naver.com>
2023-02-13 18:55:56 +0900

    Add code about Leetcode - Push Dominoes

---
+++ src/main/java/gracefulsoul/problems/PushDominoes.java
@@ -0,0 +1,51 @@
+package gracefulsoul.problems;
+
+public class PushDominoes {
+
+	// https://leetcode.com/problems/push-dominoes/submissions/897088790/
+	public static void main(String[] args) {
+		PushDominoes test = new PushDominoes();
+		System.out.println(test.pushDominoes("RR.L"));
+		System.out.println(test.pushDominoes(".L.R...LR..L.."));
+	}
+
+	public String pushDominoes(String dominoes) {
+		char[] charArray = dominoes.toCharArray();
+		int index = -1;
+		char c = '!';
+		for (int idx = 0; idx < charArray.length; idx++) {
+			if (charArray[idx] == 'L') {
+				if (index == -1 || c == 'L') {
+					while (index < idx - 1) {
+						index++;
+						charArray[index] = 'L';
+					}
+				} else if (c == 'R') {
+					int left = index + 1;
+					int right = idx - 1;
+					while (left < right) {
+						charArray[left++] = 'R';
+						charArray[right--] = 'L';
+					}
+				}
+				index = idx;
+				c = 'L';
+			} else if (charArray[idx] == 'R') {
+				if (c == 'R') {
+					while (index < idx - 1) {
+						charArray[++index] = 'R';
+					}
+				}
+				index = idx;
+				c = 'R';
+			}
+		}
+		if (c == 'R') {
+			while (index < charArray.length - 1) {
+				charArray[++index] = 'R';
+			}
+		}
+		return String.valueOf(charArray);
+	}
+
+}
