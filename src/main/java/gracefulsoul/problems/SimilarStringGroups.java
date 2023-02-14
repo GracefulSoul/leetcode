7184079591ec29369657b8e6ff45c76605b63b5a
yongsik <mymyyoo@naver.com>
2023-02-14 19:02:59 +0900

    Add code about Leetcode - Similar String Groups

--- src/main/java/gracefulsoul/problems/SimilarStringGroups.java
+++ src/main/java/gracefulsoul/problems/SimilarStringGroups.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class SimilarStringGroups {
 
-	// https://leetcode.com/problems/similar-string-groups/submissions/897764766/
+	// https://leetcode.com/problems/similar-string-groups/submissions/897770384/
 	public static void main(String[] args) {
 		SimilarStringGroups test = new SimilarStringGroups();
 		System.out.println(test.numSimilarGroups(new String[] { "tars", "rats", "arts", "star" }));
@@ -24,23 +24,23 @@ public class SimilarStringGroups {
 	private void dfs(String[] strs, int index, boolean[] visited) {
 		visited[index] = true;
 		for (int i = 0; i < strs.length; i++) {
-			if (!visited[i] && this.compare(strs[index], strs[i])) {
+			if (!visited[i] && this.isSimilar(strs[index], strs[i])) {
 				this.dfs(strs, i, visited);
 			}
 		}
 	}
 
-	private boolean compare(String s1, String s2) {
-		int diff = 0;
+	private boolean isSimilar(String s1, String s2) {
+		int count = 0;
 		for (int i = 0; i < s1.length(); i++) {
 			if (s1.charAt(i) != s2.charAt(i)) {
-				diff++;
+				count++;
 			}
-			if (diff > 2) {
+			if (count > 2) {
 				return false;
 			}
 		}
-		return diff == 0 || diff == 2;
+		return count == 0 || count == 2;
 	}
 
 }
