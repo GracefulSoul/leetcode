86fea4d7c8e24f9c890119d93d83a3aa107a3cf1
yongsik <mymyyoo@naver.com>
2023-02-16 19:13:21 +0900

    Add code about Leetcode - Keys and Rooms

---
+++ src/main/java/gracefulsoul/problems/KeysAndRooms.java
@@ -0,0 +1,38 @@
+package gracefulsoul.problems;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class KeysAndRooms {
+
+	// https://leetcode.com/problems/keys-and-rooms/submissions/899062413/
+	public static void main(String[] args) {
+		KeysAndRooms test = new KeysAndRooms();
+		System.out.println(test.canVisitAllRooms(Arrays.asList(Arrays.asList(1), Arrays.asList(2), Arrays.asList(3), Arrays.asList())));
+		System.out.println(test.canVisitAllRooms(Arrays.asList(Arrays.asList(1, 3), Arrays.asList(3, 0, 1), Arrays.asList(2), Arrays.asList())));
+	}
+
+	public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+		boolean[] visited = new boolean[rooms.size()];
+		List<Integer> firstRoom = rooms.get(0);
+		for (int i = 0; i < firstRoom.size(); i++) {
+			this.dfs(rooms, visited, firstRoom.get(i));
+		}
+		for (int i = 1; i < rooms.size(); i++) {
+			if (!visited[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private void dfs(List<List<Integer>> rooms, boolean[] visited, int index) {
+		visited[index] = true;
+		for (int i = 0; i < rooms.get(index).size(); i++) {
+			if (!visited[rooms.get(index).get(i)]) {
+				this.dfs(rooms, visited, rooms.get(index).get(i));
+			}
+		}
+	}
+
+}
