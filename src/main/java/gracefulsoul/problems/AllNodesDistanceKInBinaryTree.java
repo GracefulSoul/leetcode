ae9c0782e03a4bc4398e6d8d28ec7fdbe897712c
yongsik <mymyyoo@naver.com>
2023-03-06 19:00:43 +0900

    Add code about Leetcode - All Nodes Distance K in Binary Tree

--- src/main/java/gracefulsoul/problems/AllNodesDistanceKInBinaryTree.java
+++ src/main/java/gracefulsoul/problems/AllNodesDistanceKInBinaryTree.java
@@ -7,7 +7,7 @@ import gracefulsoul.object.node.TreeNode;
 
 public class AllNodesDistanceKInBinaryTree {
 
-	// https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/submissions/910074224/
+	// https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/submissions/910079501/
 	public static void main(String[] args) {
 		AllNodesDistanceKInBinaryTree test = new AllNodesDistanceKInBinaryTree();
 		System.out.println(test.distanceK(new TreeNode(3, new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4))), new TreeNode(1, new TreeNode(0), new TreeNode(8))), new TreeNode(5), 2));
@@ -19,12 +19,12 @@ public class AllNodesDistanceKInBinaryTree {
 		if (k == 0) {
 			result.add(target.val);
 		} else {
-			this.dfs(result, root, target.val, k, 0);
+			this.dfs(root, target.val, k, 0, result);
 		}
 		return result;
 	}
 
-	private int dfs(List<Integer> result, TreeNode node, int target, int k, int depth) {
+	private int dfs(TreeNode node, int target, int k, int depth, List<Integer> result) {
 		if (node == null) {
 			return 0;
 		} else if (depth == k) {
@@ -32,18 +32,18 @@ public class AllNodesDistanceKInBinaryTree {
 			return 0;
 		}
 		int correction = node.val == target || depth > 0 ? 1 : 0;
-		int left = this.dfs(result, node.left, target, k, depth + correction);
-		int right = this.dfs(result, node.right, target, k, depth + correction);
+		int left = this.dfs(node.left, target, k, depth + correction, result);
+		int right = this.dfs(node.right, target, k, depth + correction, result);
 		if (node.val == target) {
 			return 1;
 		} else if (left == k || right == k) {
 			result.add(node.val);
 			return 0;
 		} else if (left > 0) {
-			this.dfs(result, node.right, target, k, left + 1);
+			this.dfs(node.right, target, k, left + 1, result);
 			return left + 1;
 		} else if (right > 0) {
-			this.dfs(result, node.left, target, k, right + 1);
+			this.dfs(node.left, target, k, right + 1, result);
 			return right + 1;
 		} else {
 			return 0;
