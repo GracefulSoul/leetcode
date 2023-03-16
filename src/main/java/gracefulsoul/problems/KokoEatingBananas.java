eb65a2417fe909580a618f3397fd06a369781419
yongsik <mymyyoo@naver.com>
2023-03-16 23:59:12 +0900

    Add code about Leetcode - Koko Eating Bananas

--- src/main/java/gracefulsoul/problems/KokoEatingBananas.java
+++ src/main/java/gracefulsoul/problems/KokoEatingBananas.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class KokoEatingBananas {
 
-	// https://leetcode.com/problems/koko-eating-bananas/submissions/916324232/
+	// https://leetcode.com/problems/koko-eating-bananas/submissions/916326510/
 	public static void main(String[] args) {
 		KokoEatingBananas test = new KokoEatingBananas();
 		System.out.println(test.minEatingSpeed(new int[] { 3, 6, 7, 11 }, 8));
@@ -15,11 +15,11 @@ public class KokoEatingBananas {
 		int right = 1000000000;
 		while (left < right) {
 			int mid = (left + right) / 2;
-			int time = 0;
+			int times = 0;
 			for (int pile : piles) {
-				time += (pile + mid - 1) / mid;
+				times += (pile + mid - 1) / mid;
 			}
-			if (time > h) {
+			if (times > h) {
 				left = mid + 1;
 			} else {
 				right = mid;
