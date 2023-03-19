c26fc6b01525b6c6a1b91867b7899ea2527cb809
yongsik <mymyyoo@naver.com>
2023-03-19 12:55:32 +0900

    Add code about Leetcode - Stone Game

--- src/main/java/gracefulsoul/problems/StoneGame.java
+++ src/main/java/gracefulsoul/problems/StoneGame.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class StoneGame {
 
-	// https://leetcode.com/problems/stone-game/submissions/917784230/
+	// https://leetcode.com/problems/stone-game/submissions/917864497/
 	public static void main(String[] args) {
 		StoneGame test = new StoneGame();
 		System.err.println(test.stoneGame(new int[] { 5, 3, 4, 5 }));
@@ -10,21 +10,21 @@ public class StoneGame {
 	}
 
 	public boolean stoneGame(int[] piles) {
-		int sub = 0;
 		int sum = 0;
+		int total = 0;
 		for (int pile : piles) {
-			sum += pile;
+			total += pile;
 		}
 		int left = 0;
 		int right = piles.length - 1;
 		while (left <= right) {
 			if (piles[left] > piles[right]) {
-				sub += piles[left++];
+				sum += piles[left++];
 			} else {
-				sub += piles[right--];
+				sum += piles[right--];
 			}
 		}
-		return sub > sum / 2;
+		return sum > total / 2;
 	}
 
 }
