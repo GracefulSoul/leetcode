40756ffae713c18677a2ee341e0c440000e0daeb
yongsik <mymyyoo@naver.com>
2023-03-20 19:11:16 +0900

    Add code about Leetcode - Nth Magical Number

--- src/main/java/gracefulsoul/problems/NthMagicalNumber.java
+++ src/main/java/gracefulsoul/problems/NthMagicalNumber.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class NthMagicalNumber {
 
-	// https://leetcode.com/problems/nth-magical-number/submissions/918671112/
+	// https://leetcode.com/problems/nth-magical-number/submissions/918683201/
 	public static void main(String[] args) {
 		NthMagicalNumber test = new NthMagicalNumber();
 		System.out.println(test.nthMagicalNumber(1, 2, 3));
@@ -16,9 +16,9 @@ public class NthMagicalNumber {
 		long left = Math.min(a, b);
 		long right = (long) n * Math.min(a, b);
 		while (B > 0) {
-			long tmp = A;
+			long temp = A;
 			A = B;
-			B = tmp % B;
+			B = temp % B;
 		}
 		long lcm = (a * b) / A;
 		while (left < right) {
