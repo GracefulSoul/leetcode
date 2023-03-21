d0ed274e21fca72b5a214b58f15977a58d330976
yongsik <mymyyoo@naver.com>
2023-03-21 19:04:21 +0900

    Add code about Leetcode - Profitable Schemes

--- src/main/java/gracefulsoul/problems/ProfitableSchemes.java
+++ src/main/java/gracefulsoul/problems/ProfitableSchemes.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class ProfitableSchemes {
 
-	// https://leetcode.com/problems/profitable-schemes/submissions/919401660/
+	// https://leetcode.com/problems/profitable-schemes/submissions/919405144/
 	public static void main(String[] args) {
 		ProfitableSchemes test = new ProfitableSchemes();
 		System.out.println(test.profitableSchemes(5, 3, new int[] { 2, 2 }, new int[] { 2, 3 }));
@@ -10,21 +10,21 @@ public class ProfitableSchemes {
 	}
 
 	public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
-		int[][] dp = new int[n + 1][minProfit + 1];
+		int[][] dp = new int[minProfit + 1][n + 1];
 		dp[0][0] = 1;
 		int mod = 1000000007;
 		for (int k = 0; k < group.length; k++) {
 			int g = group[k];
 			int p = profit[k];
-			for (int i = n; g <= i; i--) {
-				for (int j = minProfit; 0 <= j; j--) {
-					dp[i][j] = (dp[i][j] + dp[i - g][Math.max(0, j - p)]) % mod;
+			for (int i = minProfit; 0 <= i; i--) {
+				for (int j = n; g <= j; j--) {
+					dp[i][j] = (dp[i][j] + dp[Math.max(0, i - p)][j - g]) % mod;
 				}
 			}
 		}
 		int result = 0;
-		for (int i = 0; i <= n; i++) {
-			result = (result + dp[i][minProfit]) % mod;
+		for (int num : dp[minProfit]) {
+			result = (result + num) % mod;
 		}
 		return result;
 	}
