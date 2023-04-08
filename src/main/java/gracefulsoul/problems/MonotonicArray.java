acedee5ab123f54586f79b7347da21ab0a41ea54
yongsik <mymyyoo@naver.com>
2023-04-08 11:21:53 +0900

    Add code about Leetcode - Monotonic Array

--- src/main/java/gracefulsoul/problems/MonotonicArray.java
+++ src/main/java/gracefulsoul/problems/MonotonicArray.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class MonotonicArray {
 
-	// https://leetcode.com/problems/monotonic-array/submissions/929908723/
+	// https://leetcode.com/problems/monotonic-array/submissions/929910430/
 	public static void main(String[] args) {
 		MonotonicArray test = new MonotonicArray();
 		System.out.println(test.isMonotonic(new int[] { 1, 2, 2, 3 }));
@@ -15,11 +15,10 @@ public class MonotonicArray {
 			boolean increase = true;
 			boolean decrease = true;
 			for (int i = 1; i < nums.length; i++) {
-				if (nums[i] > nums[i - 1]) {
-					decrease = false;
-				}
-				if (nums[i] < nums[i - 1]) {
+				if (nums[i - 1] > nums[i]) {
 					increase = false;
+				} else if (nums[i - 1] < nums[i]) {
+					decrease = false;
 				}
 				if (!decrease && !increase) {
 					return false;
