1e64be4cfc3565ecbd156af6f3d93c9502dce702
yongsik <mymyyoo@naver.com>
2023-04-24 19:46:43 +0900

    Add code about Leetcode - Sum of Subarray Minimums

--- src/main/java/gracefulsoul/problems/SumOfSubarrayMinimums.java
+++ src/main/java/gracefulsoul/problems/SumOfSubarrayMinimums.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class SumOfSubarrayMinimums {
 
-	// https://leetcode.com/problems/sum-of-subarray-minimums/submissions/938895139/
+	// https://leetcode.com/problems/sum-of-subarray-minimums/submissions/938895796/
 	public static void main(String[] args) {
 		SumOfSubarrayMinimums test = new SumOfSubarrayMinimums();
 		System.out.println(test.sumSubarrayMins(new int[] { 3, 1, 2, 4 }));
@@ -12,9 +12,9 @@ public class SumOfSubarrayMinimums {
 	public int sumSubarrayMins(int[] arr) {
 		double mod = 1000000007d;
 		int length = arr.length;
-		int[] stack = new int[length + 1];
 		int index = 0;
 		int[] dp = new int[length];
+		int[] stack = new int[length + 1];
 		long result = dp[0] = arr[0];
 		for (int i = 1; i < length; i++) {
 			while (index >= 0 && arr[stack[index]] >= arr[i]) {
