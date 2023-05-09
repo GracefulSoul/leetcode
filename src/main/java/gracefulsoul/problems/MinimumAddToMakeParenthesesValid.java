50ebbb786b55a8ff5ed892337e4fbbbe208d6526
yongsik <mymyyoo@naver.com>
2023-05-09 20:07:24 +0900

    Add code about Leetcode - Minimum Add to Make Parentheses Valid

--- src/main/java/gracefulsoul/problems/MinimumAddToMakeParenthesesValid.java
+++ src/main/java/gracefulsoul/problems/MinimumAddToMakeParenthesesValid.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class MinimumAddToMakeParenthesesValid {
 
-	// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/submissions/947171183/
+	// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/submissions/947189306/
 	public static void main(String[] args) {
 		MinimumAddToMakeParenthesesValid test = new MinimumAddToMakeParenthesesValid();
 		System.out.println(test.minAddToMakeValid("())"));
@@ -10,18 +10,18 @@ public class MinimumAddToMakeParenthesesValid {
 	}
 
 	public int minAddToMakeValid(String s) {
-		int open = 0;
-		int close = 0;
+		int left = 0;
+		int right = 0;
 		for (int i = 0; i < s.length(); i++) {
 			if (s.charAt(i) == '(') {
-				close++;
-			} else if (close > 0) {
-				close--;
+				right++;
+			} else if (right > 0) {
+				right--;
 			} else {
-				open++;
+				left++;
 			}
 		}
-		return open + close;
+		return left + right;
 	}
 
 }
