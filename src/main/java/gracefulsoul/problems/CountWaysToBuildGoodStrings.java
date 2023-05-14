66336a91db62d7b2876331c10f62773925b1350e
yongsik <mymyyoo@naver.com>
2023-05-14 12:10:51 +0900

    Modify code about Count Ways To Build Good Strings

--- src/main/java/gracefulsoul/problems/CountWaysToBuildGoodStrings.java
+++ src/main/java/gracefulsoul/problems/CountWaysToBuildGoodStrings.java
@@ -14,7 +14,7 @@ public class CountWaysToBuildGoodStrings {
 		int mod = 1000000007;
 		int dp[] = new int[high + 1];
 		dp[0] = 1;
-		for (int i = 1; i <= high; ++i) {
+		for (int i = 1; i <= high; i++) {
 			if (i >= zero) {
 				dp[i] = (dp[i] + dp[i - zero]) % mod;
 			}
