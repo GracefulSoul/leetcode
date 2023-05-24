01b86510f271cb99879ce261ae3a7a7db09aa55e
yongsik <mymyyoo@naver.com>
2023-05-24 20:16:01 +0900

    Add code about Leetcode - Three Equal Parts

--- src/main/java/gracefulsoul/problems/ThreeEqualParts.java
+++ src/main/java/gracefulsoul/problems/ThreeEqualParts.java
@@ -4,7 +4,7 @@ import gracefulsoul.util.PrintUtil;
 
 public class ThreeEqualParts {
 
-	// https://leetcode.com/problems/three-equal-parts/submissions/955695992/
+	// https://leetcode.com/problems/three-equal-parts/submissions/955703185/
 	public static void main(String[] args) {
 		ThreeEqualParts test = new ThreeEqualParts();
 		PrintUtil.print(test.threeEqualParts(new int[] { 1, 0, 1, 0, 1 }));
@@ -25,21 +25,18 @@ public class ThreeEqualParts {
 		if (count % 3 != 0) {
 			return new int[] { -1, -1 };
 		}
-		int last = 0;
-		int temp = 0;
-		for (int i = arr.length - 1; i >= 0; i--) {
-			if (arr[i] == 1) {
-				if (++temp == count / 3) {
-					last = i;
-					break;
-				}
+		int index = 0;
+		for (int i = arr.length - 1, oneThird = count / 3; i >= 0; i--) {
+			if (arr[i] == 1 && --oneThird == 0) {
+				index = i;
+				break;
 			}
 		}
-		int first = this.findIndex(arr, 0, last);
+		int first = this.findIndex(arr, 0, index);
 		if (first < 0) {
 			return new int[] { -1, -1 };
 		}
-		int second = this.findIndex(arr, first + 1, last);
+		int second = this.findIndex(arr, first + 1, index);
 		if (second < 0) {
 			return new int[] { -1, -1 };
 		}
