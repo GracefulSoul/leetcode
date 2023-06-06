d00c92e694e82f1cd130677b60b344206efb2d06
yongsik <mymyyoo@naver.com>
2023-06-06 10:02:39 +0900

    Add code about Leetcode - Can Make Arithmetic Progression From Sequence

--- src/main/java/gracefulsoul/problems/CanMakeArithmeticProgressionFromSequence.java
+++ src/main/java/gracefulsoul/problems/CanMakeArithmeticProgressionFromSequence.java
@@ -1,11 +1,8 @@
 package gracefulsoul.problems;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class CanMakeArithmeticProgressionFromSequence {
 
-	// https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/submissions/964753499/
+	// https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/submissions/964770392/
 	public static void main(String[] args) {
 		CanMakeArithmeticProgressionFromSequence test = new CanMakeArithmeticProgressionFromSequence();
 		System.out.println(test.canMakeArithmeticProgression(new int[] { 3, 5, 1 }));
@@ -13,25 +10,33 @@ public class CanMakeArithmeticProgressionFromSequence {
 	}
 
 	public boolean canMakeArithmeticProgression(int[] arr) {
-		Set<Integer> set = new HashSet<>();
+		int length = arr.length;
 		int min = Integer.MAX_VALUE;
 		int max = Integer.MIN_VALUE;
-		int length = arr.length;
 		for (int num : arr) {
 			min = Math.min(min, num);
 			max = Math.max(max, num);
-			set.add(num);
 		}
 		int diff = max - min;
 		if (diff % (length - 1) != 0) {
 			return false;
 		}
 		diff /= length - 1;
-		while (--length > 0) {
-			if (!set.contains(min)) {
+		for (int i = 0; i < length;) {
+			int num = arr[i] - min;
+			if (num == diff * i) {
+				i++;
+			} else if (num % diff != 0) {
 				return false;
+			} else {
+				int position = num / diff;
+				if (position < i || arr[position] == arr[i]) {
+					return false;
+				}
+				int temp = arr[position];
+				arr[position] = arr[i];
+				arr[i] = temp;
 			}
-			min += diff;
 		}
 		return true;
 	}
