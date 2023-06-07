bdf2fa017997677bb62e8e86622932c24e861512
yongsik <mymyyoo@naver.com>
2023-06-07 19:45:34 +0900

    Add code about Leetcode - Minimum Flips to Make a OR b Equal to c

---
+++ src/main/java/gracefulsoul/problems/MinimumFlipsToMakeAORBEqualToC.java
@@ -0,0 +1,28 @@
+package gracefulsoul.problems;
+
+public class MinimumFlipsToMakeAORBEqualToC {
+
+	// https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/submissions/965823655/
+	public static void main(String[] args) {
+		MinimumFlipsToMakeAORBEqualToC test = new MinimumFlipsToMakeAORBEqualToC();
+		System.out.println(test.minFlips(2, 6, 5));
+		System.out.println(test.minFlips(4, 2, 7));
+		System.out.println(test.minFlips(1, 2, 3));
+	}
+
+	public int minFlips(int a, int b, int c) {
+		int result = 0;
+		while (a > 0 || b > 0 || c > 0) {
+			if ((c & 1) == 0) {
+				result += (a & 1) + (b & 1);
+			} else if ((a & 1) == 0 && (b & 1) == 0) {
+				result += 1;
+			}
+			a >>= 1;
+			b >>= 1;
+			c >>= 1;
+		}
+		return result;
+	}
+
+}
