f25c3f830e4753313faed971e1cfefc1d814a8fd
yongsik <mymyyoo@naver.com>
2023-06-17 14:53:40 +0900

    Add code about Leetcode - Make Array Strictly Increasing

--- src/main/java/gracefulsoul/problems/MakeArrayStrictlyIncreasing.java
+++ src/main/java/gracefulsoul/problems/MakeArrayStrictlyIncreasing.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class MakeArrayStrictlyIncreasing {
 
-	// https://leetcode.com/problems/make-array-strictly-increasing/submissions/973114588/
+	// https://leetcode.com/problems/make-array-strictly-increasing/submissions/973120625/
 	public static void main(String[] args) {
 		MakeArrayStrictlyIncreasing test = new MakeArrayStrictlyIncreasing();
 		System.out.println(test.makeArrayIncreasing(new int[] { 1, 5, 3, 6, 7 }, new int[] { 1, 3, 2, 4 }));
@@ -22,16 +22,15 @@ public class MakeArrayStrictlyIncreasing {
 		for (int num : arr1) {
 			Map<Integer, Integer> temp = new HashMap<>();
 			for (Map.Entry<Integer, Integer> entry : dp.entrySet()) {
-				int value = entry.getValue();
 				if (num > entry.getKey()) {
-					temp.put(num, Math.min(temp.getOrDefault(num, Integer.MAX_VALUE), value));
+					temp.put(num, Math.min(temp.getOrDefault(num, Integer.MAX_VALUE), entry.getValue()));
 				}
 				int index = Arrays.binarySearch(arr2, entry.getKey() + 1);
 				if (index < 0) {
 					index = -index - 1;
 				}
 				if (index < arr2.length) {
-					temp.put(arr2[index], Math.min(value + 1, temp.getOrDefault(arr2[index], Integer.MAX_VALUE)));
+					temp.put(arr2[index], Math.min(entry.getValue() + 1, temp.getOrDefault(arr2[index], Integer.MAX_VALUE)));
 				}
 			}
 			dp = temp;
