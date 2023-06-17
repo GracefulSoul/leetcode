936bef298a7b46030da6b3b5fbd1530ebcd90520
yongsik <mymyyoo@naver.com>
2023-06-17 14:41:57 +0900

    Add code about Leetcode - Make Array Strictly Increasing

--- src/main/java/gracefulsoul/problems/MakeArrayStrictlyIncreasing.java
+++ src/main/java/gracefulsoul/problems/MakeArrayStrictlyIncreasing.java
@@ -7,12 +7,12 @@ import java.util.Map;
 
 public class MakeArrayStrictlyIncreasing {
 
-	// https://leetcode.com/problems/make-array-strictly-increasing/submissions/973016173/
+	// https://leetcode.com/problems/make-array-strictly-increasing/submissions/973114588/
 	public static void main(String[] args) {
 		MakeArrayStrictlyIncreasing test = new MakeArrayStrictlyIncreasing();
 		System.out.println(test.makeArrayIncreasing(new int[] { 1, 5, 3, 6, 7 }, new int[] { 1, 3, 2, 4 }));
-		System.out.println(test.makeArrayIncreasing(new int[] { 1, 5, 3, 6, 7 }, new int[] { 4, 3, 1 }));
-		System.out.println(test.makeArrayIncreasing(new int[] { 1, 5, 3, 6, 7 }, new int[] { 1, 6, 3, 3 }));
+//		System.out.println(test.makeArrayIncreasing(new int[] { 1, 5, 3, 6, 7 }, new int[] { 4, 3, 1 }));
+//		System.out.println(test.makeArrayIncreasing(new int[] { 1, 5, 3, 6, 7 }, new int[] { 1, 6, 3, 3 }));
 	}
 
 	public int makeArrayIncreasing(int[] arr1, int[] arr2) {
@@ -22,16 +22,16 @@ public class MakeArrayStrictlyIncreasing {
 		for (int num : arr1) {
 			Map<Integer, Integer> temp = new HashMap<>();
 			for (Map.Entry<Integer, Integer> entry : dp.entrySet()) {
-				int val = entry.getValue();
+				int value = entry.getValue();
 				if (num > entry.getKey()) {
-					temp.put(num, Math.min(temp.getOrDefault(num, Integer.MAX_VALUE), val));
+					temp.put(num, Math.min(temp.getOrDefault(num, Integer.MAX_VALUE), value));
 				}
 				int index = Arrays.binarySearch(arr2, entry.getKey() + 1);
 				if (index < 0) {
 					index = -index - 1;
 				}
 				if (index < arr2.length) {
-					temp.put(arr2[index], Math.min(val + 1, temp.getOrDefault(arr2[index], Integer.MAX_VALUE)));
+					temp.put(arr2[index], Math.min(value + 1, temp.getOrDefault(arr2[index], Integer.MAX_VALUE)));
 				}
 			}
 			dp = temp;
