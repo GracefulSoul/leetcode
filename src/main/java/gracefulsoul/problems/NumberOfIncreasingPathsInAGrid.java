24b06890d12316ebe22c1715d056ff564ec2880c
yongsik <mymyyoo@naver.com>
2023-06-18 09:28:45 +0900

    Add code about Leetcode - Number of Increasing Paths in a Grid

---
+++ src/main/java/gracefulsoul/problems/NumberOfIncreasingPathsInAGrid.java
@@ -0,0 +1,55 @@
+package gracefulsoul.problems;
+
+public class NumberOfIncreasingPathsInAGrid {
+
+	// https://leetcode.com/problems/number-of-increasing-paths-in-a-grid/submissions/973640816/
+	public static void main(String[] args) {
+		NumberOfIncreasingPathsInAGrid test = new NumberOfIncreasingPathsInAGrid();
+		System.out.println(test.countPaths(new int[][] {
+			{ 1, 1 },
+			{ 3, 4 }
+		}));
+		System.out.println(test.countPaths(new int[][] {
+			{ 1 },
+			{ 2 }
+		}));
+	}
+
+	private static final int MOD = 1000000007;
+
+	private int[][] DIRECTIONS = new int[][] {
+		{ 1, 0 },
+		{ 0, -1 },
+		{ -1, 0 },
+		{ 0, 1 }
+	};
+
+	public int countPaths(int[][] grid) {
+		int row = grid.length;
+		int col = grid[0].length;
+		int count = 0;
+		int[][] dp = new int[row][col];
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				count = (count + this.getCount(grid, dp, i, j, row, col, 0)) % MOD;
+			}
+		}
+		return (int) count;
+	}
+
+	private int getCount(int[][] grid, int[][] dp, int i, int j, int M, int N, int prev) {
+		if (i < 0 || j < 0 || i == M || j == N || grid[i][j] <= prev) {
+			return 0;
+		} else if (dp[i][j] != 0) {
+			return dp[i][j];
+		} else {
+			int count = 1;
+			for (int[] direction : DIRECTIONS) {
+				count = (count + this.getCount(grid, dp, i + direction[0], j + direction[1], M, N, grid[i][j])) % MOD;
+			}
+			dp[i][j] = count;
+			return count;
+		}
+	}
+
+}
