2962ad7d23e614aa00bb37284c6c789452cb416d
yongsik <mymyyoo@naver.com>
2023-06-23 19:01:43 +0900

    Add code about Leetcode - Longest Arithmetic Subsequence

---
+++ src/main/java/gracefulsoul/problems/LongestArithmeticSubsequence.java
@@ -0,0 +1,37 @@
+package gracefulsoul.problems;
+
+public class LongestArithmeticSubsequence {
+
+	// https://leetcode.com/problems/longest-arithmetic-subsequence/submissions/977751947/
+	public static void main(String[] args) {
+		LongestArithmeticSubsequence test = new LongestArithmeticSubsequence();
+		System.out.println(test.longestArithSeqLength(new int[] { 3, 6, 9, 12 }));
+		System.out.println(test.longestArithSeqLength(new int[] { 9, 4, 7, 2, 10 }));
+		System.out.println(test.longestArithSeqLength(new int[] { 20, 1, 15, 3, 10, 5, 8 }));
+	}
+
+	public int longestArithSeqLength(int[] nums) {
+		int result = 0;
+		int limit = 500;
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for (int num : nums) {
+			max = Math.max(max, num);
+			min = Math.min(min, num);
+		}
+		for (int i = 0; i <= max - min; i++) {
+			if (i * result > max - min) {
+				break;
+			}
+			int[] first = new int[limit + 1];
+			int[] second = new int[limit + 1];
+			for (int num : nums) {
+				first[num] = (num + i <= limit) ? (first[num + i] + 1) : 1;
+				second[num] = (num - i >= 0) ? (second[num - i] + 1) : 1;
+				result = Math.max(result, Math.max(first[num], second[num]));
+			}
+		}
+		return result;
+	}
+
+}
