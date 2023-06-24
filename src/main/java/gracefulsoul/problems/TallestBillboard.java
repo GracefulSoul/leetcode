ed84f50284bcb85983956a3191a36c79550286ab
yongsik <mymyyoo@naver.com>
2023-06-24 10:41:08 +0900

    Add code about Leetcode - Tallest Billboard

--- src/main/java/gracefulsoul/problems/TallestBillboard.java
+++ src/main/java/gracefulsoul/problems/TallestBillboard.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class TallestBillboard {
 
-	// https://leetcode.com/problems/tallest-billboard/submissions/978211296/
+	// https://leetcode.com/problems/tallest-billboard/submissions/978215265/
 	public static void main(String[] args) {
 		TallestBillboard test = new TallestBillboard();
 		System.out.println(test.tallestBillboard(new int[] { 1, 2, 3, 6 }));
@@ -17,16 +17,16 @@ public class TallestBillboard {
 			sum += rod;
 		}
 		int[][] dp = new int[length + 1][sum + 1];
-		for (int i = 1; i <= length; i++) {
+		for (int i = 0; i < length; i++) {
 			for (int j = 0; j <= sum; j++) {
-				if (dp[i - 1][j] < j) {
+				if (dp[i][j] < j) {
 					continue;
 				}
-				dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
-				int k = j + rods[i - 1];
-				dp[i][k] = Math.max(dp[i][k], dp[i - 1][j] + rods[i - 1]);
-				k = Math.abs(j - rods[i - 1]);
-				dp[i][k] = Math.max(dp[i][k], dp[i - 1][j] + rods[i - 1]);
+				dp[i + 1][j] = Math.max(dp[i + 1][j], dp[i][j]);
+				int k = j + rods[i];
+				dp[i + 1][k] = Math.max(dp[i + 1][k], dp[i][j] + rods[i]);
+				k = Math.abs(j - rods[i]);
+				dp[i + 1][k] = Math.max(dp[i + 1][k], dp[i][j] + rods[i]);
 			}
 		}
 		return dp[length][0] / 2;
