6c85b0b7d6c788ab080a97f72bae6650f4c318bc
yongsik <mymyyoo@naver.com>
2023-07-16 09:39:54 +0900

    Add code about Leetcode - Most Stones Removed with Same Row or Column

---
+++ src/main/java/gracefulsoul/problems/MostStonesRemovedWithSameRowOrColumn.java
@@ -0,0 +1,54 @@
+package gracefulsoul.problems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MostStonesRemovedWithSameRowOrColumn {
+
+	// https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/submissions/995435449/
+	public static void main(String[] args) {
+		MostStonesRemovedWithSameRowOrColumn test = new MostStonesRemovedWithSameRowOrColumn();
+		System.out.println(test.removeStones(new int[][] {
+			{ 0, 0 },
+			{ 0, 1 },
+			{ 1, 0 },
+			{ 1, 2 },
+			{ 2, 1 },
+			{ 2, 2 }
+		}));
+		System.out.println(test.removeStones(new int[][] {
+			{ 0, 0 },
+			{ 0, 2 },
+			{ 1, 1 },
+			{ 2, 0 },
+			{ 2, 2 }
+		}));
+		System.out.println(test.removeStones(new int[][] {
+			{ 0, 0 }
+		}));
+	}
+
+	public int removeStones(int[][] stones) {
+		Set<int[]> visited = new HashSet<>();
+		int numOfIslands = 0;
+		for (int[] stone : stones) {
+			if (!visited.contains(stone)) {
+				this.dfs(visited, stones, stone);
+				numOfIslands++;
+			}
+		}
+		return stones.length - numOfIslands;
+	}
+
+	private void dfs(Set<int[]> visited, int[][] stones, int[] prev) {
+		visited.add(prev);
+		for (int[] stone : stones) {
+			if (!visited.contains(stone)) {
+				if (prev[0] == stone[0] || prev[1] == stone[1]) {
+					this.dfs(visited, stones, stone);
+				}
+			}
+		}
+	}
+
+}
