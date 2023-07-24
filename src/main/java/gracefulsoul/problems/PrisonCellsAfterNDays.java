aeddf8f1dbec56649ed5bad60ed2b46bf34a97e3
yongsik <mymyyoo@naver.com>
2023-07-24 18:54:39 +0900

    Add code about Leetcode - Prison Cells After N Days

--- src/main/java/gracefulsoul/problems/PrisonCellsAfterNDays.java
+++ src/main/java/gracefulsoul/problems/PrisonCellsAfterNDays.java
@@ -4,7 +4,7 @@ import gracefulsoul.util.PrintUtil;
 
 public class PrisonCellsAfterNDays {
 
-	// https://leetcode.com/problems/prison-cells-after-n-days/submissions/1002510039/
+	// https://leetcode.com/problems/prison-cells-after-n-days/submissions/1002511386/
 	public static void main(String[] args) {
 		PrisonCellsAfterNDays test = new PrisonCellsAfterNDays();
 		PrintUtil.print(test.prisonAfterNDays(new int[] { 0, 1, 0, 1, 1, 0, 0, 1 }, 7));
@@ -12,7 +12,7 @@ public class PrisonCellsAfterNDays {
 	}
 
 	public int[] prisonAfterNDays(int[] cells, int n) {
-		for (n = (n - 1) % 14 + 1; n > 0; n--) {
+		for (n = ((n - 1) % 14) + 1; n > 0; n--) {
 			int[] temp = new int[8];
 			for (int i = 1; i < 7; i++) {
 				temp[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
