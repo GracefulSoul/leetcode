8b16f43db40f30e439262e6bdc45f217ddc778a5
yongsik <mymyyoo@naver.com>
2023-07-27 19:02:09 +0900

    Add code about Leetcode - Maximum Running Time of N Computers

---
+++ src/main/java/gracefulsoul/problems/MaximumRunningTimeOfNComputers.java
@@ -0,0 +1,34 @@
+package gracefulsoul.problems;
+
+public class MaximumRunningTimeOfNComputers {
+
+	// https://leetcode.com/problems/maximum-running-time-of-n-computers/submissions/1005221135/
+	public static void main(String[] args) {
+		MaximumRunningTimeOfNComputers test = new MaximumRunningTimeOfNComputers();
+		System.out.println(test.maxRunTime(2, new int[] { 3, 3, 3 }));
+		System.out.println(test.maxRunTime(2, new int[] { 1, 1, 1, 1 }));
+	}
+
+	public long maxRunTime(int n, int[] batteries) {
+		long sum = 0;
+		for (int battery : batteries) {
+			sum += battery;
+		}
+		long min = 1;
+		long max = (sum / n) + 1;
+		while (min < max) {
+			long mid = min + ((max - min) / 2);
+			long extra = 0;
+			for (int battery : batteries) {
+				extra += Math.min(battery, mid);
+			}
+			if (extra >= mid * n) {
+				min = mid + 1;
+			} else {
+				max = mid;
+			}
+		}
+		return min - 1;
+	}
+
+}
