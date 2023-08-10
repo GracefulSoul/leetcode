bdce1ad1e1795f869ac47d21b167895202443b10
yongsik <mymyyoo@naver.com>
2023-08-10 16:38:58 +0900

    Add code about Leetcode - Equal Rational Numbers

--- src/main/java/gracefulsoul/problems/EqualRationalNumbers.java
+++ src/main/java/gracefulsoul/problems/EqualRationalNumbers.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class EqualRationalNumbers {
 
-	// https://leetcode.com/problems/equal-rational-numbers/submissions/1017313092/
+	// https://leetcode.com/problems/equal-rational-numbers/submissions/1017318458/
 	public static void main(String[] args) {
 		EqualRationalNumbers test = new EqualRationalNumbers();
 		System.out.println(test.isRationalEqual("0.(52)", "0.5(25)"));
@@ -18,9 +18,9 @@ public class EqualRationalNumbers {
 		int index = str.indexOf('(');
 		if (index > 0) {
 			String base = str.substring(0, index);
-			String decimal = str.substring(index + 1, str.length() - 1);
-			for (int j = 1; j < 18; j++) {
-				base += decimal;
+			String num = str.substring(index + 1, str.length() - 1);
+			for (int i = 1; i < 18; i++) {
+				base += num;
 			}
 			return Double.valueOf(base);
 		} else {
