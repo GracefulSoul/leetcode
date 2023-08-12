a1f62f9049b7991191d02723d5f6177026b33cd1
yongsik <mymyyoo@naver.com>
2023-08-12 13:46:57 +0900

    Add code about Leetcode - K Closest Points to Origin

--- src/main/java/gracefulsoul/problems/KClosestPointsToOrigin.java
+++ src/main/java/gracefulsoul/problems/KClosestPointsToOrigin.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 
 public class KClosestPointsToOrigin {
 
-	// https://leetcode.com/problems/k-closest-points-to-origin/submissions/1018939783/
+	// https://leetcode.com/problems/k-closest-points-to-origin/submissions/1018943255/
 	public static void main(String[] args) {
 		KClosestPointsToOrigin test = new KClosestPointsToOrigin();
 		System.out.println(test.kClosest(new int[][] {
@@ -20,7 +20,7 @@ public class KClosestPointsToOrigin {
 	}
 
 	public int[][] kClosest(int[][] points, int k) {
-		Queue<int[]> queue = new PriorityQueue<>((a, b) -> (a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1]));
+		Queue<int[]> queue = new PriorityQueue<>((a, b) -> ((a[0] * a[0]) + (a[1] * a[1])) - ((b[0] * b[0]) + (b[1] * b[1])));
 		int[][] result = new int[k][2];
 		for (int[] point : points) {
 			queue.add(point);
