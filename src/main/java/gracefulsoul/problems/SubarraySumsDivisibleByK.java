4146a731aecf1a0aeae1d119c04c036644be5756
yongsik <mymyyoo@naver.com>
2023-08-14 20:48:15 +0900

    Add code about Leetcode - Subarray Sums Divisible by K

--- src/main/java/gracefulsoul/problems/SubarraySumsDivisibleByK.java
+++ src/main/java/gracefulsoul/problems/SubarraySumsDivisibleByK.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class SubarraySumsDivisibleByK {
 
-	// https://leetcode.com/problems/subarray-sums-divisible-by-k/submissions/1021038967/
+	// https://leetcode.com/problems/subarray-sums-divisible-by-k/submissions/1021055416/
 	public static void main(String[] args) {
 		SubarraySumsDivisibleByK test = new SubarraySumsDivisibleByK();
 		System.out.println(test.subarraysDivByK(new int[] { 4, 5, 0, -2, -3, 1 }, 5));
@@ -15,7 +15,7 @@ public class SubarraySumsDivisibleByK {
 		int[] count = new int[k];
 		count[0] = 1;
 		for (int num : nums) {
-			prefix = (prefix + num % k + k) % k;
+			prefix = (prefix + (num % k) + k) % k;
 			result += count[prefix];
 			count[prefix]++;
 		}
