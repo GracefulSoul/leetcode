a26bdca5a19f3ee38f70d0db5fcfe6c9df797452
yongsik <mymyyoo@naver.com>
2023-08-24 18:42:34 +0900

    Add code about Leetcode - Time Based Key-Value Store

---
+++ src/main/java/gracefulsoul/problems/TimeBasedKeyValueStore.java
@@ -0,0 +1,77 @@
+package gracefulsoul.problems;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TimeBasedKeyValueStore {
+
+	// https://leetcode.com/problems/time-based-key-value-store/submissions/1030360887/
+	public static void main(String[] args) {
+		TimeMap timeMap = new TimeMap();
+		timeMap.set("foo", "bar", 1);				// store the key "foo" and value "bar" along with timestamp = 1.
+		System.out.println(timeMap.get("foo", 1));	// return "bar"
+		System.out.println(timeMap.get("foo", 3));	// return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 is "bar".
+		timeMap.set("foo", "bar2", 4);				// store the key "foo" and value "bar2" along with timestamp = 4.
+		System.out.println(timeMap.get("foo", 4));	// return "bar2"
+		System.out.println(timeMap.get("foo", 5));	// return "bar2"
+	}
+
+}
+
+class TimeMap {
+
+	private Map<String, List<Pair>> map;
+
+	public TimeMap() {
+		this.map = new HashMap<>();
+	}
+
+	public void set(String key, String value, int timestamp) {
+		if (!this.map.containsKey(key)) {
+			this.map.put(key, new ArrayList<Pair>());
+		}
+		this.map.get(key).add(new Pair(value, timestamp));
+	}
+
+	public String get(String key, int timestamp) {
+		if (!this.map.containsKey(key)) {
+			return "";
+		} else {
+			return this.binarySearch(this.map.get(key), timestamp);
+		}
+	}
+
+	protected String binarySearch(List<Pair> list, int time) {
+		int left = 0;
+		int right = list.size() - 1;
+		while (left < right) {
+			int mid = (left + right + 1) >> 1;
+			if (list.get(mid).time <= time) {
+				left = mid;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return list.get(left).time <= time ? list.get(left).val : "";
+	}
+
+}
+
+class Pair {
+	String val;
+	int time;
+
+	public Pair(String val, int time) {
+		this.val = val;
+		this.time = time;
+	}
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
