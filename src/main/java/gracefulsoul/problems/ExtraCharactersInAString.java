b6aaff0b264ec72706337a3e04762052671c2130
yongsik <mymyyoo@naver.com>
2023-09-02 17:35:04 +0900

    Add code about Leetcode - Extra Characters in a String

---
+++ src/main/java/gracefulsoul/problems/ExtraCharactersInAString.java
@@ -0,0 +1,28 @@
+package gracefulsoul.problems;
+
+public class ExtraCharactersInAString {
+
+	// https://leetcode.com/problems/extra-characters-in-a-string/submissions/1038284412/
+	public static void main(String[] args) {
+		ExtraCharactersInAString test = new ExtraCharactersInAString();
+		System.out.println(test.minExtraChar("leetscode", new String[] { "leet", "code", "leetcode" }));
+		System.out.println(test.minExtraChar("sayhelloworld", new String[] { "hello", "world" }));
+	}
+
+	public int minExtraChar(String s, String[] dictionary) {
+		int length = s.length();
+		int[] dp = new int[length + 1];
+		dp[length] = 0;
+		for (int i = length - 1; i >= 0; i--) {
+			String str = s.substring(i);
+			dp[i] = dp[i + 1] + 1;
+			for (int j = 0; j < dictionary.length; j++) {
+				if (str.startsWith(dictionary[j])) {
+					dp[i] = Math.min(dp[i], dp[i + dictionary[j].length()]);
+				}
+			}
+		}
+		return dp[0];
+	}
+
+}
