99608fe90cbcb6a4fac6f51c9ab39de061078cc2
yongsik <mymyyoo@naver.com>
2023-09-17 01:37:18 +0900

    Add code about Leetcode - Rotting Oranges

---
+++ src/main/java/gracefulsoul/problems/RottingOranges.java
@@ -0,0 +1,56 @@
+package gracefulsoul.problems;
+
+public class RottingOranges {
+
+	// https://leetcode.com/problems/rotting-oranges/submissions/1051558034/
+	public static void main(String[] args) {
+		RottingOranges test = new RottingOranges();
+		System.out.println(test.orangesRotting(new int[][] {
+			{ 2, 1, 1 },
+			{ 1, 1, 0 },
+			{ 0, 1, 1 }
+		}));
+		System.out.println(test.orangesRotting(new int[][] {
+			{ 2, 1, 1 },
+			{ 0, 1, 1 },
+			{ 1, 0, 1 }
+		}));
+		System.out.println(test.orangesRotting(new int[][] {
+			{ 0, 2 }
+		}));
+	}
+
+	public int orangesRotting(int[][] grid) {
+		if (grid == null || grid.length == 0) {
+			return -1;
+		}
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == 2) {
+					this.dfs(grid, i, j, 2);
+				}
+			}
+		}
+		int minutes = 2;
+		for (int[] row : grid) {
+			for (int col : row) {
+				if (col == 1) {
+					return -1;
+				}
+				minutes = Math.max(minutes, col);
+			}
+		}
+		return minutes - 2;
+	}
+
+	private void dfs(int[][] grid, int i, int j, int minutes) {
+		if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] != 0 && (grid[i][j] <= 1 || grid[i][j] >= minutes)) {
+			grid[i][j] = minutes;
+			this.dfs(grid, i - 1, j, minutes + 1);
+			this.dfs(grid, i + 1, j, minutes + 1);
+			this.dfs(grid, i, j - 1, minutes + 1);
+			this.dfs(grid, i, j + 1, minutes + 1);
+		}
+	}
+
+}
