b40685be9a870b03aa14d77cc988dc44597722fd
yongsik <mymyyoo@naver.com>
2023-10-10 19:05:13 +0900

    Add code about Leetcode - Minimum Number of Operations to Make Array Continuous

--- src/main/java/gracefulsoul/problems/MinimumNumberOfOperationsToMakeArrayContinuous.java
+++ src/main/java/gracefulsoul/problems/MinimumNumberOfOperationsToMakeArrayContinuous.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MinimumNumberOfOperationsToMakeArrayContinuous {
 
-	// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-continuous/submissions/1071691807/
+	// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-continuous/submissions/1071694673/
 	public static void main(String[] args) {
 		MinimumNumberOfOperationsToMakeArrayContinuous test = new MinimumNumberOfOperationsToMakeArrayContinuous();
 		System.out.println(test.minOperations(new int[] { 4, 2, 5, 3 }));
@@ -15,10 +15,9 @@ public class MinimumNumberOfOperationsToMakeArrayContinuous {
 	public int minOperations(int[] nums) {
 		Arrays.sort(nums);
 		int result = Integer.MAX_VALUE;
-		int j = 1;
 		int count = 0;
 		int[] dp = new int[nums.length];
-		for (int i = 0; i < nums.length; i++) {
+		for (int i = 0, j = 1; i < nums.length; i++) {
 			while (j < nums.length && nums[j] <= nums[i] + nums.length - 1) {
 				if (nums[j] == nums[j - 1]) {
 					count++;
