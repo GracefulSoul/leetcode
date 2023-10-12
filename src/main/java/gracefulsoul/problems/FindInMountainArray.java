4753de6a56bfcc31abd5439e7985b5e0ddc9b374
yongsik <mymyyoo@naver.com>
2023-10-12 19:38:38 +0900

    Add code about Leetcode - Find in Mountain Array

---
+++ src/main/java/gracefulsoul/problems/FindInMountainArray.java
@@ -0,0 +1,64 @@
+package gracefulsoul.problems;
+
+public class FindInMountainArray {
+
+	// https://leetcode.com/problems/find-in-mountain-array/submissions/1073374785/
+	public static void main(String[] args) {
+		FindInMountainArray test = new FindInMountainArray();
+		System.out.println(test.findInMountainArray(3, new MountainArray(new int[] { 1, 2, 3, 4, 5, 3, 1 })));
+		System.out.println(test.findInMountainArray(3, new MountainArray(new int[] { 0, 1, 2, 4, 2, 1 })));
+	}
+
+	public int findInMountainArray(int target, MountainArray mountainArr) {
+		int length = mountainArr.length();
+		int peak = 0;
+		for (int left = 0, right = length - 1; left < right;) {
+			int mid = (left + right) / 2;
+			if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+				left = peak = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		for (int left = 0, right = peak; left <= right;) {
+			int mid = (left + right) / 2;
+			if (mountainArr.get(mid) < target) {
+				left = mid + 1;
+			} else if (mountainArr.get(mid) > target) {
+				right = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		for (int left = peak, right = length - 1; left <= right;) {
+			int mid = (left + right) / 2;
+			if (mountainArr.get(mid) > target) {
+				left = mid + 1;
+			} else if (mountainArr.get(mid) < target) {
+				right = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
+}
+
+class MountainArray {
+
+	private int[] array;
+
+	public MountainArray(int[] array) {
+		this.array = array;
+	}
+
+	public int get(int index) {
+		return this.array[index];
+	}
+
+	public int length() {
+		return this.array.length;
+	}
+
+}
