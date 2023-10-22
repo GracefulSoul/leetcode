6cb7b3d4be935c2a8e39dd37a35249de65b28dec
yongsik <mymyyoo@naver.com>
2023-10-22 13:48:12 +0900

    Add code about Leetcode - Maximum Score of a Good Subarray

--- src/main/java/gracefulsoul/problems/MaximumScoreOfAGoodSubarray.java
+++ src/main/java/gracefulsoul/problems/MaximumScoreOfAGoodSubarray.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class MaximumScoreOfAGoodSubarray {
 
-	// https://leetcode.com/problems/maximum-score-of-a-good-subarray/submissions/1081083175/
+	// https://leetcode.com/problems/maximum-score-of-a-good-subarray/submissions/1081095937/
 	public static void main(String[] args) {
 		MaximumScoreOfAGoodSubarray test = new MaximumScoreOfAGoodSubarray();
 		System.out.println(test.maximumScore(new int[] { 1, 4, 3, 7, 4, 5 }, 3));
@@ -20,23 +20,22 @@ public class MaximumScoreOfAGoodSubarray {
 		while (j < length && min <= nums[j]) {
 			j++;
 		}
-		int max = min * (j - i - 1);
+		int result = min * (j - i - 1);
 		while (0 <= i || j < length) {
 			if (i < 0 || (j < length && nums[i] <= nums[j])) {
 				min = nums[j];
 				while (j < length && min <= nums[j]) {
 					j++;
 				}
-				max = Math.max(max, min * (j - i - 1));
 			} else {
 				min = nums[i];
 				while (0 <= i && min <= nums[i]) {
 					i--;
 				}
-				max = Math.max(max, min * (j - i - 1));
 			}
+			result = Math.max(result, min * (j - i - 1));
 		}
-		return max;
+		return result;
 	}
 
 }
