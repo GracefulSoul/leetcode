f9196ba426342711100375ef8207e0e609457c18
yongsik <mymyyoo@naver.com>
2023-11-12 13:04:30 +0900

    Add code about Leetcode - Construct Binary Search Tree from Preorder Traversal

--- src/main/java/gracefulsoul/problems/ConstructBinarySearchTreeFromPreorderTraversal.java
+++ src/main/java/gracefulsoul/problems/ConstructBinarySearchTreeFromPreorderTraversal.java
@@ -5,7 +5,7 @@ import gracefulsoul.util.PrintUtil;
 
 public class ConstructBinarySearchTreeFromPreorderTraversal {
 
-	// https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/submissions/1096974151/
+	// https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/submissions/1097021993/
 	public static void main(String[] args) {
 		ConstructBinarySearchTreeFromPreorderTraversal test = new ConstructBinarySearchTreeFromPreorderTraversal();
 		PrintUtil.print(test.bstFromPreorder(new int[] { 8, 5, 1, 7, 10, 12 }), true, false);
@@ -13,16 +13,16 @@ public class ConstructBinarySearchTreeFromPreorderTraversal {
 	}
 
 	public TreeNode bstFromPreorder(int[] preorder) {
-		return this.bstFromPreorder(preorder, Integer.MAX_VALUE, new int[] { 0 });
+		return this.bst(preorder, Integer.MAX_VALUE, new int[] { 0 });
 	}
 
-	private TreeNode bstFromPreorder(int[] preorder, int bound, int[] i) {
-		if (i[0] == preorder.length || preorder[i[0]] > bound) {
+	private TreeNode bst(int[] preorder, int bound, int[] i) {
+		if (i[0] == preorder.length || bound < preorder[i[0]]) {
 			return null;
 		}
 		TreeNode root = new TreeNode(preorder[i[0]++]);
-		root.left = this.bstFromPreorder(preorder, root.val, i);
-		root.right = this.bstFromPreorder(preorder, bound, i);
+		root.left = this.bst(preorder, root.val, i);
+		root.right = this.bst(preorder, bound, i);
 		return root;
 	}
 
