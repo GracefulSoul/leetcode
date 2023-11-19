547ec18ce97de427e616ce2b1624e1d0b1eeac75
yongsik <mymyyoo@naver.com>
2023-11-19 13:06:03 +0900

    Add code about Leetcode - Reduction Operations to Make the Array Elements Equal

---
+++ src/main/java/gracefulsoul/problems/ReductionOperationsToMakeTheArrayElementsEqual.java
@@ -0,0 +1,27 @@
+package gracefulsoul.problems;
+
+import java.util.Arrays;
+
+public class ReductionOperationsToMakeTheArrayElementsEqual {
+
+	// https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/submissions/1101858339/
+	public static void main(String[] args) {
+		ReductionOperationsToMakeTheArrayElementsEqual test = new ReductionOperationsToMakeTheArrayElementsEqual();
+		System.out.println(test.reductionOperations(new int[] { 5, 1, 3 }));
+		System.out.println(test.reductionOperations(new int[] { 1, 1, 1 }));
+		System.out.println(test.reductionOperations(new int[] { 1, 1, 2, 2, 3 }));
+	}
+
+	public int reductionOperations(int[] nums) {
+		int result = 0;
+		int length = nums.length;
+		Arrays.sort(nums);
+		for (int i = length - 1; i > 0; --i) {
+			if (nums[i - 1] != nums[i]) {
+				result += length - i;
+			}
+		}
+		return result;
+	}
+
+}
