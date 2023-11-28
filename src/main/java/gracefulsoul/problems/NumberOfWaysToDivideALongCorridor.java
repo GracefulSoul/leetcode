36d7201bf906ecbc107d181234212ed1066b6be1
yongsik <mymyyoo@naver.com>
2023-11-28 20:31:26 +0900

    Add code about Leetcode - Number of Ways to Divide a Long Corridor

--- src/main/java/gracefulsoul/problems/NumberOfWaysToDivideALongCorridor.java
+++ src/main/java/gracefulsoul/problems/NumberOfWaysToDivideALongCorridor.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class NumberOfWaysToDivideALongCorridor {
 
-	// https://leetcode.com/problems/number-of-ways-to-divide-a-long-corridor/submissions/1108090595/
+	// https://leetcode.com/problems/number-of-ways-to-divide-a-long-corridor/submissions/1108107032/
 	public static void main(String[] args) {
 		NumberOfWaysToDivideALongCorridor test = new NumberOfWaysToDivideALongCorridor();
 		System.out.println(test.numberOfWays("SSPPSPS"));
@@ -11,18 +11,29 @@ public class NumberOfWaysToDivideALongCorridor {
 	}
 
 	public int numberOfWays(String corridor) {
-		int seat = 1;
-		int plant = 0;
-		int divider = 0;
-		for (char c : corridor.toCharArray()) {
-			seat = (seat + divider) % 1000000007;
-			if (c == 'S') {
-				divider = plant;
-				plant = seat;
-				seat = 0;
+		char[] charArray = corridor.toCharArray();
+		int length = charArray.length;
+		int seat = 0;
+		long result = 1;
+		for (int i = 0; i < length; i++) {
+			if (charArray[i] == 'S') {
+				seat++;
+				while (++i < length && charArray[i] != 'S') {
+				}
+				if (i < length && charArray[i] == 'S') {
+					seat++;
+				}
+				int divider = 1;
+				while (++i < length && charArray[i] != 'S') {
+					divider++;
+				}
+				if (divider > 1 && i < length) {
+					result = (result * divider) % 1000000007;
+				}
+				i--;
 			}
 		}
-		return divider;
+		return seat != 0 && seat % 2 == 0 ? (int) result : 0;
 	}
 
 }
