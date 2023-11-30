b412c1c38e2069fa721d792540d2c4e50791afd9
yongsik <mymyyoo@naver.com>
2023-11-30 19:51:32 +0900

    Add code about Leetcode - Minimum One Bit Operations to Make Integers Zero

---
+++ src/main/java/gracefulsoul/problems/MinimumOneBitOperationsToMakeIntegersZero.java
@@ -0,0 +1,23 @@
+package gracefulsoul.problems;
+
+public class MinimumOneBitOperationsToMakeIntegersZero {
+
+	// https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/submissions/1109488008/
+	public static void main(String[] args) {
+		MinimumOneBitOperationsToMakeIntegersZero test = new MinimumOneBitOperationsToMakeIntegersZero();
+		System.out.println(test.minimumOneBitOperations(3));
+		System.out.println(test.minimumOneBitOperations(6));
+	}
+
+	public int minimumOneBitOperations(int n) {
+		int result = 0;
+		int sign = 1;
+		while (n > 0) {
+			result += n ^ (n - 1) * sign;
+			sign = -sign;
+			n &= n - 1;
+		}
+		return Math.abs(result);
+	}
+
+}
