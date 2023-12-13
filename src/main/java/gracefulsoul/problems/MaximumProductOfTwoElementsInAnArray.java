e9797e93c7ec3104e25fef426ed433a96106b58c
GracefulSoul <Soul@121.133.117.156>
2023-12-13 22:22:19 +0900

    Add code about Leetcode - Maximum Product of Two Elements in an Array

--- src/main/java/gracefulsoul/problems/MaximumProductOfTwoElementsInAnArray.java
+++ src/main/java/gracefulsoul/problems/MaximumProductOfTwoElementsInAnArray.java
@@ -12,12 +12,12 @@ public class MaximumProductOfTwoElementsInAnArray {
 
 	public int maxProduct(int[] nums) {
 		int[] max = new int[] { Integer.MIN_VALUE, Integer.MIN_VALUE };
-		for (int n : nums) {
-			if (n > max[0]) {
+		for (int num : nums) {
+			if (num > max[0]) {
 				max[1] = max[0];
-				max[0] = n;
-			} else if (n > max[1]) {
-				max[1] = n;
+				max[0] = num;
+			} else if (num > max[1]) {
+				max[1] = num;
 			}
 		}
 		return (max[0] - 1) * (max[1] - 1);
