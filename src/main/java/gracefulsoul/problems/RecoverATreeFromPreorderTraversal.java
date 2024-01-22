b7af0a9958c42944001b1da480a35d9958baee34
GracefulSoul <Soul@121.133.117.156>
2024-01-22 18:41:31 +0900

    Add code about Leetcode - Recover a Tree From Preorder Traversal

--- src/main/java/gracefulsoul/problems/RecoverATreeFromPreorderTraversal.java
+++ src/main/java/gracefulsoul/problems/RecoverATreeFromPreorderTraversal.java
@@ -4,7 +4,7 @@ import gracefulsoul.object.node.TreeNode;
 
 public class RecoverATreeFromPreorderTraversal {
 
-	// https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/submissions/1153442420/
+	// https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/submissions/1153445603/
 	public static void main(String[] args) {
 		RecoverATreeFromPreorderTraversal test = new RecoverATreeFromPreorderTraversal();
 		System.out.println(test.recoverFromPreorder("1-2--3--4-5--6--7"));
@@ -19,18 +19,18 @@ public class RecoverATreeFromPreorderTraversal {
 	}
 
 	private TreeNode dfs(String traversal, int depth) {
-		int numDash = 0;
-		while (this.index + numDash < traversal.length() && traversal.charAt(this.index + numDash) == '-') {
-			numDash++;
+		int num = 0;
+		while (this.index + num < traversal.length() && traversal.charAt(this.index + num) == '-') {
+			num++;
 		}
-		if (numDash != depth) {
+		if (num != depth) {
 			return null;
 		}
-		int next = this.index + numDash;
+		int next = this.index + num;
 		while (next < traversal.length() && traversal.charAt(next) != '-') {
 			next++;
 		}
-		int val = Integer.parseInt(traversal.substring(this.index + numDash, next));
+		int val = Integer.parseInt(traversal.substring(this.index + num, next));
 		this.index = next;
 		TreeNode root = new TreeNode(val);
 		root.left = this.dfs(traversal, depth + 1);
