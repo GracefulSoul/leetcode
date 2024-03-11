750ca8b3d6a55cac0069cfdc9914e61508024ddc
GracefulSoul <GracefulSoul@github.com>
2024-03-11 18:20:50 +0900

    Add code about Leetcode - Robot Bounded In Circle

--- src/main/java/gracefulsoul/problems/RobotBoundedInCircle.java
+++ src/main/java/gracefulsoul/problems/RobotBoundedInCircle.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class RobotBoundedInCircle {
 
-	// https://leetcode.com/problems/robot-bounded-in-circle/submissions/1200378411/
+	// https://leetcode.com/problems/robot-bounded-in-circle/submissions/1200380179/
 	public static void main(String[] args) {
 		RobotBoundedInCircle test = new RobotBoundedInCircle();
 		System.out.println(test.isRobotBounded("GGLLGG"));
@@ -16,18 +16,17 @@ public class RobotBoundedInCircle {
 		{ 0, -1 },
 		{ 1, 0 }
 	};
-	
+
 	public boolean isRobotBounded(String instructions) {
 		int i = 0;
 		int[] point = new int[2];
 		for (char c : instructions.toCharArray()) {
-			if (c == 'L') {
-				i = (i + 1) % 4;
-			} else if (c == 'R') {
-				i = (i + 3) % 4;
-			} else {
-				point[0] = point[0] + DIRECTIONS[i][0];
-				point[1] = point[1] + DIRECTIONS[i][1];
+			switch (c) {
+				case 'L': i = (i + 1) % 4; break;
+				case 'R': i = (i + 3) % 4; break;
+				default:
+					point[0] += DIRECTIONS[i][0];
+					point[1] += DIRECTIONS[i][1];
 			}
 		}
 		return point[0] == 0 && point[1] == 0 || i != 0;
