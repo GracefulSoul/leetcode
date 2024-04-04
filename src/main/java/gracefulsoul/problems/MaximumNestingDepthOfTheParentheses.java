08d531a360160eeed8cb45a6c6edf62cff39d956
GracefulSoul <GracefulSoul@github.com>
2024-04-04 18:33:20 +0900

    Add code about Leetcode - Maximum Nesting Depth of the Parentheses

--- src/main/java/gracefulsoul/problems/MaximumNestingDepthOfTheParentheses.java
+++ src/main/java/gracefulsoul/problems/MaximumNestingDepthOfTheParentheses.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class MaximumNestingDepthOfTheParentheses {
 
-	// https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/submissions/1222925951/
+	// https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/submissions/1222934960/
 	public static void main(String[] args) {
 		MaximumNestingDepthOfTheParentheses test = new MaximumNestingDepthOfTheParentheses();
 		System.out.println(test.maxDepth("(1+(2*3)+((8)/4))+1"));
@@ -12,8 +12,8 @@ public class MaximumNestingDepthOfTheParentheses {
 	public int maxDepth(String s) {
 		int count = 0;
 		int result = 0;
-		for (int i = 0; i < s.length(); i++) {
-			switch (s.charAt(i)) {
+		for (char c : s.toCharArray()) {
+			switch (c) {
 				case '(': result = Math.max(result, ++count); break;
 				case ')': count--; break;
 				default:
