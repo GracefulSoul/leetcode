dca7fe8fe258b89f469db3ac1b1bcc50f468dc07
GracefulSoul <GracefulSoul@github.com>
2024-04-06 11:50:41 +0900

    Add code about Leetcode - Minimum Remove to Make Valid Parentheses

---
+++ src/main/java/gracefulsoul/problems/MinimumRemoveToMakeValidParentheses.java
@@ -0,0 +1,38 @@
+package gracefulsoul.problems;
+
+import java.util.Stack;
+
+public class MinimumRemoveToMakeValidParentheses {
+
+	// https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/submissions/1224453732/
+	public static void main(String[] args) {
+		MinimumRemoveToMakeValidParentheses test = new MinimumRemoveToMakeValidParentheses();
+		System.out.println(test.minRemoveToMakeValid("lee(t(c)o)de)"));
+		System.out.println(test.minRemoveToMakeValid("a)b(c)d"));
+		System.out.println(test.minRemoveToMakeValid("))(("));
+	}
+
+	public String minRemoveToMakeValid(String s) {
+		StringBuilder sb = new StringBuilder(s);
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < sb.length(); i++) {
+			switch (sb.charAt(i)) {
+			case '(':
+				stack.add(i);
+				break;
+			case ')':
+				if (!stack.empty()) {
+					stack.pop();
+				} else {
+					sb.setCharAt(i, '*');
+				}
+				break;
+			}
+		}
+		while (!stack.empty()) {
+			sb.setCharAt(stack.pop(), '*');
+		}
+		return sb.toString().replaceAll("\\*", "");
+	}
+
+}
