6dcfa9eb593c9efbc5665ebfb4f7c8f56bd63e0a
GracefulSoul <GracefulSoul@github.com>
2024-04-08 19:14:07 +0900

    Add code about Leetcode - Number of Students Unable to Eat Lunch

---
+++ src/main/java/gracefulsoul/problems/NumberOfStudentsUnableToEatLunch.java
@@ -0,0 +1,24 @@
+package gracefulsoul.problems;
+
+public class NumberOfStudentsUnableToEatLunch {
+
+	// https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/submissions/1226589671/
+	public static void main(String[] args) {
+		NumberOfStudentsUnableToEatLunch test = new NumberOfStudentsUnableToEatLunch();
+		System.out.println(test.countStudents(new int[] { 1, 1, 0, 0 }, new int[] { 0, 1, 0, 1 }));
+		System.out.println(test.countStudents(new int[] { 1, 1, 1, 0, 0, 1 }, new int[] { 1, 0, 0, 0, 1, 1 }));
+	}
+
+	public int countStudents(int[] students, int[] sandwiches) {
+		int count[] = { 0, 0 };
+		int length = students.length;
+		for (int student : students) {
+			count[student]++;
+		}
+		for (int k = 0; 0 < length && 0 < count[sandwiches[k]]; k++, length--) {
+			count[sandwiches[k]]--;
+		}
+		return length;
+	}
+
+}
