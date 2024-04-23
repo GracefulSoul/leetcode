98ef5359efc8e9f35e905df36288fb72dbd8bfd4
GracefulSoul <GracefulSoul@github.com>
2024-04-23 18:45:56 +0900

    Add code about Leetcode - Shortest Common Supersequence

--- src/main/java/gracefulsoul/problems/ShortestCommonSupersequence.java
+++ src/main/java/gracefulsoul/problems/ShortestCommonSupersequence.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class ShortestCommonSupersequence {
 
-	// https://leetcode.com/problems/shortest-common-supersequence/submissions/1239755784/
+	// https://leetcode.com/problems/shortest-common-supersequence/submissions/1239768789/
 	public static void main(String[] args) {
 		ShortestCommonSupersequence test = new ShortestCommonSupersequence();
 		System.out.println(test.shortestCommonSupersequence("abac", "cab"));
@@ -10,13 +10,13 @@ public class ShortestCommonSupersequence {
 	}
 
 	public String shortestCommonSupersequence(String str1, String str2) {
-		int row = str1.length();
-		int col = str2.length();
+		int str1Length = str1.length();
+		int str2Length = str2.length();
 		char[] str1CharArray = str1.toCharArray();
 		char[] str2CharArray = str2.toCharArray();
-		int[][] dp = new int[row + 1][col + 1];
-		for (int i = row - 1; i >= 0; i--) {
-			for (int j = col - 1; j >= 0; j--) {
+		int[][] dp = new int[str1Length + 1][str2Length + 1];
+		for (int i = str1Length - 1; i >= 0; i--) {
+			for (int j = str2Length - 1; j >= 0; j--) {
 				if (str1CharArray[i] == str2CharArray[j]) {
 					dp[i][j] = 1 + dp[i + 1][j + 1];
 				} else {
@@ -25,9 +25,9 @@ public class ShortestCommonSupersequence {
 			}
 		}
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0, j = 0; i < row || j < col;) {
-			if (i < row ^ j < col) {
-				sb.append(i < row ? str1CharArray[i++] : str2CharArray[j++]);
+		for (int i = 0, j = 0; i < str1Length || j < str2Length;) {
+			if (i < str1Length ^ j < str2Length) {
+				sb.append(i < str1Length ? str1CharArray[i++] : str2CharArray[j++]);
 			} else if (str1CharArray[i] == str2CharArray[j]) {
 				sb.append(str1CharArray[i++]);
 				++j;
