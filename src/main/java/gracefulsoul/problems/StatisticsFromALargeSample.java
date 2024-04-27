f5c43dab5b3e56fe9e4850a862eeb8753c936e57
GracefulSoul <GracefulSoul@github.com>
2024-04-27 11:50:47 +0900

    Add code about Leetcode - Statistics from a Large Sample

---
+++ src/main/java/gracefulsoul/problems/StatisticsFromALargeSample.java
@@ -0,0 +1,76 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.util.PrintUtil;
+
+public class StatisticsFromALargeSample {
+
+	// https://leetcode.com/problems/statistics-from-a-large-sample/submissions/1242912818/
+	public static void main(String[] args) {
+		StatisticsFromALargeSample test = new StatisticsFromALargeSample();
+		PrintUtil.print(test.sampleStats(new int[] { 0, 1, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }));
+		PrintUtil.print(test.sampleStats(new int[] { 0, 4, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }));
+	}
+
+	public double[] sampleStats(int[] count) {
+		double[] result = new double[] { 256, -1, 0, 0, 0 };
+		int left = 0;
+		int right = 255;
+		int count1 = 0;
+		int count2 = 0;
+		int mid1 = 0;
+		int mid2 = 0;
+		while (left <= right) {
+			while (count[left] == 0) {
+				left++;
+			}
+			while (count[right] == 0) {
+				right--;
+			}
+			if (count1 < count2) {
+				result[2] += (double) count[left] * left;
+				count1 += count[left];
+				if (count[left] > count[(int) result[4]]) {
+					result[4] = left;
+				}
+				result[1] = Math.max(result[1], left);
+				result[0] = Math.min(result[0], left);
+				mid1 = left;
+				left++;
+			} else {
+				result[2] += (double) count[right] * right;
+				count2 += count[right];
+				if (count[right] > count[(int) result[4]]) {
+					result[4] = right;
+				}
+				result[1] = Math.max(result[1], right);
+				result[0] = Math.min(result[0], right);
+				mid2 = right;
+				right--;
+			}
+		}
+		result[2] /= (count1 + count2);
+		if (count1 < count2) {
+			result[3] = mid2;
+		} else if (count1 > count2) {
+			result[3] = mid1;
+		} else {
+			result[3] = (mid1 + mid2) / 2.0;
+		}
+		return result;
+	}
+
+}
