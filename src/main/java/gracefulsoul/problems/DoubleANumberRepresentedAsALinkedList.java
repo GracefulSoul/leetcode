089b569648f4a88f39bd0dd9882cc3d320dcb209
GracefulSoul <GracefulSoul@github.com>
2024-05-07 18:11:35 +0900

    Add code about Leetcode - Double a Number Represented as a Linked List

---
+++ src/main/java/gracefulsoul/problems/DoubleANumberRepresentedAsALinkedList.java
@@ -0,0 +1,30 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.object.node.ListNode;
+import gracefulsoul.util.PrintUtil;
+
+public class DoubleANumberRepresentedAsALinkedList {
+
+	// https://leetcode.com/problems/double-a-number-represented-as-a-linked-list/submissions/1251622114/
+	public static void main(String[] args) {
+		DoubleANumberRepresentedAsALinkedList test = new DoubleANumberRepresentedAsALinkedList();
+		PrintUtil.print(test.doubleIt(new ListNode(1, new ListNode(8, new ListNode(9)))));
+		PrintUtil.print(test.doubleIt(new ListNode(9, new ListNode(9, new ListNode(9)))));
+	}
+
+	public ListNode doubleIt(ListNode head) {
+		if (head.val > 4) {
+			head = new ListNode(0, head);
+		}
+		ListNode temp = head;
+		while (temp != null) {
+			temp.val = (temp.val * 2) % 10;
+			if (temp.next != null && temp.next.val > 4) {
+				temp.val++;
+			}
+			temp = temp.next;
+		}
+		return head;
+	}
+
+}
