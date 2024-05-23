33b479cab3ac82b07c947a5b913f4b9595488b8b
GracefulSoul <GracefulSoul@github.com>
2024-05-23 20:25:10 +0900

    Add code about Leetcode - The Number of Beautiful Subsets

--- src/main/java/gracefulsoul/problems/TheNumberOfBeautifulSubsets.java
+++ src/main/java/gracefulsoul/problems/TheNumberOfBeautifulSubsets.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class TheNumberOfBeautifulSubsets {
 
-	// https://leetcode.com/problems/the-number-of-beautiful-subsets/submissions/1265713869/
+	// https://leetcode.com/problems/the-number-of-beautiful-subsets/submissions/1265718820/
 	public static void main(String[] args) {
 		TheNumberOfBeautifulSubsets test = new TheNumberOfBeautifulSubsets();
 		System.out.println(test.beautifulSubsets(new int[] { 2, 4, 6 }, 2));
@@ -16,14 +16,13 @@ public class TheNumberOfBeautifulSubsets {
 		return this.dfs(nums, k, new int[1001], 0, 0);
 	}
 
-	private int dfs(int[] nums, int k, int[] freqMap, int i, int count) {
+	private int dfs(int[] nums, int k, int[] counts, int i, int count) {
 		for (int j = i; j < nums.length; j++) {
-			if (nums[j] > k && freqMap[nums[j] - k] > 0) {
-				continue;
+			if (nums[j] <= k || counts[nums[j] - k] == 0) {
+				counts[nums[j]]++;
+				count = this.dfs(nums, k, counts, j + 1, count + 1);
+				counts[nums[j]]--;
 			}
-			freqMap[nums[j]]++;
-			count = this.dfs(nums, k, freqMap, j + 1, count + 1);
-			freqMap[nums[j]]--;
 		}
 		return count;
 	}
