0c0fb7553f5217f44c13a692eca0f7220d0b78be
GracefulSoul <GracefulSoul@github.com>
2024-05-31 18:27:30 +0900

    Add code about Leetcode - Count Triplets That Can Form Two Arrays of Equal XOR

--- src/main/java/gracefulsoul/problems/CountTripletsThatCanFormTwoArraysOfEqualXOR.java
+++ src/main/java/gracefulsoul/problems/CountTripletsThatCanFormTwoArraysOfEqualXOR.java
@@ -1,11 +1,8 @@
 package gracefulsoul.problems;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class CountTripletsThatCanFormTwoArraysOfEqualXOR {
 
-	// https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/submissions/1272785003/
+	// https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/submissions/1273151764/
 	public static void main(String[] args) {
 		CountTripletsThatCanFormTwoArraysOfEqualXOR test = new CountTripletsThatCanFormTwoArraysOfEqualXOR();
 		System.out.println(test.countTriplets(new int[] { 2, 3, 1, 6, 7 }));
@@ -14,19 +11,15 @@ public class CountTripletsThatCanFormTwoArraysOfEqualXOR {
 
 	public int countTriplets(int[] arr) {
 		int result = 0;
-		int prefix = 0;
-		int count;
-		int total;
-		Map<Integer, Integer> countMap = new HashMap<>();
-		Map<Integer, Integer> totalMap = new HashMap<>();
-		countMap.put(0, 1);
-		for (int i = 0; i < arr.length; i++) {
-			prefix ^= arr[i];
-			count = countMap.getOrDefault(prefix, 0);
-			total = totalMap.getOrDefault(prefix, 0);
-			result += (count * i) - total;
-			countMap.put(prefix, count + 1);
-			totalMap.put(prefix, total + i + 1);
+		int length = arr.length;
+		for (int i = 0; i < length; i++) {
+			int temp = arr[i];
+			for (int j = i + 1; j < length; j++) {
+				temp ^= arr[j];
+				if (temp == 0) {
+					result += (j - i);
+				}
+			}
 		}
 		return result;
 	}
