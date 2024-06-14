b02de6fb5877c1fb98945f46e9d0b4e315ed4084
GracefulSoul <GracefulSoul@github.com>
2024-06-14 18:10:07 +0900

    Modify code about Leetcode - Lowest Common Ancestor of Deepest Leaves

--- src/main/java/gracefulsoul/problems/LowestCommonAncestorOfDeepestLeaves.java
+++ src/main/java/gracefulsoul/problems/LowestCommonAncestorOfDeepestLeaves.java
@@ -5,14 +5,14 @@ import gracefulsoul.util.PrintUtil;
 
 public class LowestCommonAncestorOfDeepestLeaves {
 
-	// https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/submissions/1287915135/
+	// https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/submissions/1287927384/
 	public static void main(String[] args) {
 		LowestCommonAncestorOfDeepestLeaves test = new LowestCommonAncestorOfDeepestLeaves();
 		PrintUtil.print(test.lcaDeepestLeaves(new TreeNode(3, new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4))), new TreeNode(1, new TreeNode(0), new TreeNode(8)))), true, false);
 		PrintUtil.print(test.lcaDeepestLeaves(new TreeNode(1)), true, false);
 	}
 
-	private int level = 0;
+	private int depth = 0;
 	private TreeNode root;
 
 	public TreeNode lcaDeepestLeaves(TreeNode root) {
@@ -20,14 +20,14 @@ public class LowestCommonAncestorOfDeepestLeaves {
 		return this.root;
 	}
 
-	private int dfs(TreeNode node, int level) {
-		this.level = Math.max(this.level, level);
+	private int dfs(TreeNode node, int depth) {
+		this.depth = Math.max(this.depth, depth);
 		if (node == null) {
-			return level;
+			return depth;
 		} else {
-			int left = this.dfs(node.left, level + 1);
-			int right = this.dfs(node.right, level + 1);
-			if (left == this.level && right == this.level) {
+			int left = this.dfs(node.left, depth + 1);
+			int right = this.dfs(node.right, depth + 1);
+			if (left == this.depth && right == this.depth) {
 				this.root = node;
 			}
 			return Math.max(left, right);
