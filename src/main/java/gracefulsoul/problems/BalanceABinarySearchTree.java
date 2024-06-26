9c9ebb98faeaf2cb2a3de0fc3b49fe3c88583496
GracefulSoul <GracefulSoul@github.com>
2024-06-26 18:40:14 +0900

    Add code about Leetcode - Balance a Binary Search Tree

--- src/main/java/gracefulsoul/problems/BalanceABinarySearchTree.java
+++ src/main/java/gracefulsoul/problems/BalanceABinarySearchTree.java
@@ -8,7 +8,7 @@ import gracefulsoul.util.PrintUtil;
 
 public class BalanceABinarySearchTree {
 
-	// https://leetcode.com/problems/balance-a-binary-search-tree/submissions/1300714941/
+	// https://leetcode.com/problems/balance-a-binary-search-tree/submissions/1300727148/
 	public static void main(String[] args) {
 		BalanceABinarySearchTree test = new BalanceABinarySearchTree();
 		PrintUtil.print(test.balanceBST(new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3, null, new TreeNode(4))))), true, false);
@@ -17,26 +17,26 @@ public class BalanceABinarySearchTree {
 
 	public TreeNode balanceBST(TreeNode root) {
 		List<TreeNode> list = new ArrayList<>();
-		this.sort(root, list);
-		return this.getBST(list, 0, list.size() - 1);
+		this.addListInorder(root, list);
+		return this.createBalanceBST(list, 0, list.size() - 1);
 	}
 
-	private void sort(TreeNode root, List<TreeNode> list) {
+	private void addListInorder(TreeNode root, List<TreeNode> list) {
 		if (root != null) {
-			this.sort(root.left, list);
+			this.addListInorder(root.left, list);
 			list.add(root);
-			this.sort(root.right, list);
+			this.addListInorder(root.right, list);
 		}
 	}
 
-	private TreeNode getBST(List<TreeNode> list, int start, int end) {
+	private TreeNode createBalanceBST(List<TreeNode> list, int start, int end) {
 		if (start > end) {
 			return null;
 		} else {
 			int mid = (start + end) / 2;
 			TreeNode root = list.get(mid);
-			root.left = this.getBST(list, start, mid - 1);
-			root.right = this.getBST(list, mid + 1, end);
+			root.left = this.createBalanceBST(list, start, mid - 1);
+			root.right = this.createBalanceBST(list, mid + 1, end);
 			return root;
 		}
 	}
