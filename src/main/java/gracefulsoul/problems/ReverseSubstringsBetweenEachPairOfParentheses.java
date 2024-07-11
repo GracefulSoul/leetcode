4871c6f47e77cc3932ada0b3a6625faf030b9da0
GracefulSoul <GracefulSoul@github.com>
2024-07-11 18:36:09 +0900

    Add code about Leetcode - Reverse Substrings Between Each Pair of Parentheses

--- src/main/java/gracefulsoul/problems/ReverseSubstringsBetweenEachPairOfParentheses.java
+++ src/main/java/gracefulsoul/problems/ReverseSubstringsBetweenEachPairOfParentheses.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class ReverseSubstringsBetweenEachPairOfParentheses {
 
-	// https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/submissions/1317390532/
+	// https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/submissions/1317399345/
 	public static void main(String[] args) {
 		ReverseSubstringsBetweenEachPairOfParentheses test = new ReverseSubstringsBetweenEachPairOfParentheses();
 		System.out.println(test.reverseParentheses("(abcd)"));
@@ -19,11 +19,11 @@ public class ReverseSubstringsBetweenEachPairOfParentheses {
 
 	private String reverseParentheses(char[] charArray) {
 		StringBuilder sb = new StringBuilder();
-		while (index < charArray.length) {
-			switch (charArray[index]) {
-				case ')': index++; return sb.reverse().toString();
-				case '(': index++; sb.append(this.reverseParentheses(charArray)); break;
-				default: sb.append(charArray[index]); index++; break;
+		while (this.index < charArray.length) {
+			switch (charArray[this.index]) {
+				case ')': this.index++; return sb.reverse().toString();
+				case '(': this.index++; sb.append(this.reverseParentheses(charArray)); break;
+				default: sb.append(charArray[this.index]); this.index++; break;
 			}
 		}
 		return sb.toString();
