19261d970861b1a9bd8abf423a04f6242629e658
GracefulSoul <GracefulSoul@github.com>
2024-07-16 18:32:51 +0900

    Add code about Leetcode - Step-By-Step Directions From a Binary Tree Node to Another

--- src/main/java/gracefulsoul/problems/StepByStepDirectionsFromABinaryTreeNodeToAnother.java
+++ src/main/java/gracefulsoul/problems/StepByStepDirectionsFromABinaryTreeNodeToAnother.java
@@ -4,7 +4,7 @@ import gracefulsoul.object.node.TreeNode;
 
 public class StepByStepDirectionsFromABinaryTreeNodeToAnother {
 
-	// https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/submissions/1322752304/
+	// https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/submissions/1322757909/
 	public static void main(String[] args) {
 		StepByStepDirectionsFromABinaryTreeNodeToAnother test = new StepByStepDirectionsFromABinaryTreeNodeToAnother();
 		System.out.println(test.getDirections(new TreeNode(5, new TreeNode(1, new TreeNode(3), null), new TreeNode(2, new TreeNode(6), new TreeNode(4))), 3, 6));
@@ -14,8 +14,8 @@ public class StepByStepDirectionsFromABinaryTreeNodeToAnother {
 	public String getDirections(TreeNode root, int startValue, int destValue) {
 		StringBuilder start = new StringBuilder();
 		StringBuilder dest = new StringBuilder();
-		this.find(root, startValue, start);
-		this.find(root, destValue, dest);
+		this.findPaths(root, startValue, start);
+		this.findPaths(root, destValue, dest);
 		int count = 0;
 		for (int i = start.length(), j = dest.length(); i > 0 && j > 0 && start.charAt(i - 1) == dest.charAt(j - 1); i--, j--) {
 			count++;
@@ -23,12 +23,12 @@ public class StepByStepDirectionsFromABinaryTreeNodeToAnother {
 		return "U".repeat(start.length() - count) + dest.reverse().substring(count).toString();
 	}
 
-	private boolean find(TreeNode n, int val, StringBuilder sb) {
-		if (n.val == val) {
+	private boolean findPaths(TreeNode node, int value, StringBuilder sb) {
+		if (node.val == value) {
 			return true;
-		} else if (n.left != null && this.find(n.left, val, sb)) {
+		} else if (node.left != null && this.findPaths(node.left, value, sb)) {
 			sb.append("L");
-		} else if (n.right != null && this.find(n.right, val, sb)) {
+		} else if (node.right != null && this.findPaths(node.right, value, sb)) {
 			sb.append("R");
 		}
 		return sb.length() > 0;
