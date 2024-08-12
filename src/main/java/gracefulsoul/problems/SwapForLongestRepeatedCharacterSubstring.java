0e8ff9ae28ab0e3f640c137551401ffe4e8ede3d
GracefulSoul <GracefulSoul@github.com>
2024-08-12 17:47:33 +0900

    Add code about Leetcode - Swap For Longest Repeated Character Substring

---
+++ src/main/java/gracefulsoul/problems/SwapForLongestRepeatedCharacterSubstring.java
@@ -0,0 +1,41 @@
+package gracefulsoul.problems;
+
+public class SwapForLongestRepeatedCharacterSubstring {
+
+	// https://leetcode.com/problems/swap-for-longest-repeated-character-substring/submissions/1352925549/
+	public static void main(String[] args) {
+		SwapForLongestRepeatedCharacterSubstring test = new SwapForLongestRepeatedCharacterSubstring();
+		System.out.println(test.maxRepOpt1("ababa"));
+		System.out.println(test.maxRepOpt1("aaabaaa"));
+		System.out.println(test.maxRepOpt1("aaaaa"));
+	}
+
+	public int maxRepOpt1(String s) {
+		int[] counts = new int[26];
+		char[] charArray = s.toCharArray();
+		int length = charArray.length;
+		for (int i = 0; i < length; i++) {
+			counts[charArray[i] - 'a']++;
+		}
+		int max = 0;
+		for (int i = 0; i < length; i++) {
+			char curr = charArray[i];
+			int j = i;
+			int count = 0;
+			int diff = 0;
+			while (j < length && (curr == charArray[j] || diff == 0) && count < counts[curr - 'a']) {
+				if (curr != charArray[j]) {
+					diff++;
+				}
+				count++;
+				j++;
+			}
+			if (count < counts[charArray[i] - 'a'] && diff == 0) {
+				count++;
+			}
+			max = Math.max(max, count);
+		}
+		return max;
+	}
+
+}
