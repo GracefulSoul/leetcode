65f3b22e4d65e97a4a1aeb56a58ce43375d7d959
GracefulSoul <GracefulSoul@github.com>
2024-09-05 19:35:35 +0900

    Add code about Leetcode - Find Missing Observations

---
+++ src/main/java/gracefulsoul/problems/FindMissingObservations.java
@@ -0,0 +1,40 @@
+package gracefulsoul.problems;
+
+import java.util.Arrays;
+
+import gracefulsoul.util.PrintUtil;
+
+public class FindMissingObservations {
+
+	// https://leetcode.com/problems/find-missing-observations/submissions/1379816873/
+	public static void main(String[] args) {
+		FindMissingObservations test = new FindMissingObservations();
+		PrintUtil.print(test.missingRolls(new int[] { 3, 2, 4, 3 }, 4, 2));
+		PrintUtil.print(test.missingRolls(new int[] { 1, 5, 6 }, 3, 4));
+		PrintUtil.print(test.missingRolls(new int[] { 1, 2, 3, 4 }, 6, 4));
+	}
+
+	public int[] missingRolls(int[] rolls, int mean, int n) {
+		int sum = 0;
+		for (int roll : rolls) {
+			sum += roll;
+		}
+		int less = (mean * (n + rolls.length)) - sum;
+		if (less < n || (6 * n) < less) {
+			return new int[0];
+		} else {
+			int[] result = new int[n];
+			int quotient = less / n;
+			int remainder = less % n;
+			Arrays.fill(result, less / n);
+			for (int i = 0; i < remainder; i++) {
+				result[i] = quotient + 1;
+			}
+			for (int i = remainder; i < n; i++) {
+				result[i] = quotient;
+			}
+			return result;
+		}
+	}
+
+}
