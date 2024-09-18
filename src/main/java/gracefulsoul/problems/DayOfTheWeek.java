faea3c1c79a4aa2ed1ab16d2834902fcf509c055
GracefulSoul <GracefulSoul@github.com>
2024-09-18 10:27:29 +0900

    Add code about Leetcode - Day of the Week

---
+++ src/main/java/gracefulsoul/problems/DayOfTheWeek.java
@@ -0,0 +1,26 @@
+package gracefulsoul.problems;
+
+public class DayOfTheWeek {
+
+	// https://leetcode.com/problems/day-of-the-week/submissions/1393825364/
+	public static void main(String[] args) {
+		DayOfTheWeek test = new DayOfTheWeek();
+		System.out.println(test.dayOfTheWeek(31, 8, 2019));
+		System.out.println(test.dayOfTheWeek(18, 7, 1999));
+		System.out.println(test.dayOfTheWeek(15, 8, 1993));
+	}
+
+	private String[] days = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+	public String dayOfTheWeek(int day, int month, int year) {
+		if (month < 3) {
+			month += 12;
+			year -= 1;
+		}
+		int century = year / 100;
+		year %= 100;
+		int w = ((century / 4) - (2 * century) + year + (year / 4) + ((13 * (month + 1)) / 5) + day - 1) % 7;
+		return days[(w + 7) % 7];
+	}
+
+}
