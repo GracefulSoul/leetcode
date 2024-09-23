1fe861797d5854f492c01d5ae4720ee982c45975
GracefulSoul <GracefulSoul@github.com>
2024-09-23 20:39:01 +0900

    Add code about Leetcode - Critical Connections in a Network

--- src/main/java/gracefulsoul/problems/CriticalConnectionsInANetwork.java
+++ src/main/java/gracefulsoul/problems/CriticalConnectionsInANetwork.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class CriticalConnectionsInANetwork {
 
-	// https://leetcode.com/problems/critical-connections-in-a-network/submissions/1399475876/
+	// https://leetcode.com/problems/critical-connections-in-a-network/submissions/1399508008/
 	public static void main(String[] args) {
 		CriticalConnectionsInANetwork test = new CriticalConnectionsInANetwork();
 		System.out.println(test.criticalConnections(4,Arrays.asList(
@@ -26,11 +26,9 @@ public class CriticalConnectionsInANetwork {
 		for (int i = 0; i < n; i++) {
 			graph[i] = new ArrayList<>();
 		}
-		for (List<Integer> oneConnection : connections) {
-			int node1 = oneConnection.get(0);
-			int node2 = oneConnection.get(1);
-			graph[node1].add(node2);
-			graph[node2].add(node1);
+		for (List<Integer> connection : connections) {
+			graph[connection.get(0)].add(connection.get(1));
+			graph[connection.get(1)].add(connection.get(0));
 		}
 		List<List<Integer>> results = new ArrayList<>();
 		this.criticalConnections(results, graph, -1, 0, new boolean[n], new int[n], 0);
