06a41dc5240cc3bd980d334268d655e6a3950598
GracefulSoul <GracefulSoul@github.com>
2024-10-10 18:22:59 +0900

    Add code about Leetcode - Remove All Adjacent Duplicates in String II

---
+++ src/main/java/gracefulsoul/problems/RemoveAllAdjacentDuplicatesInStringII.java
@@ -0,0 +1,27 @@
+package gracefulsoul.problems;
+
+public class RemoveAllAdjacentDuplicatesInStringII {
+
+	public static void main(String[] args) {
+		RemoveAllAdjacentDuplicatesInStringII test = new RemoveAllAdjacentDuplicatesInStringII();
+		System.out.println(test.removeDuplicates("abcd", 2));
+		System.out.println(test.removeDuplicates("deeedbbcccbdaa", 3));
+		System.out.println(test.removeDuplicates("pbbcggttciiippooaais", 2));
+	}
+
+	public String removeDuplicates(String s, int k) {
+		int[] count = new int[s.length()];
+		StringBuilder sb = new StringBuilder();
+		for (char c : s.toCharArray()) {
+			sb.append(c);
+			int length = sb.length();
+			int i = length - 1;
+			count[i] = 1 + (i > 0 && sb.charAt(i) == sb.charAt(i - 1) ? count[i - 1] : 0);
+			if (count[i] >= k) {
+				sb.delete(length - k, length);
+			}
+		}
+		return sb.toString();
+	}
+
+}
