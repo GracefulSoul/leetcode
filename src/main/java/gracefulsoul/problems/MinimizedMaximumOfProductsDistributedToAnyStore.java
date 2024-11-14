1ed6a35e91b0f87d56e459fbfac25befd2b000c0
GracefulSoul <GracefulSoul@github.com>
2024-11-14 18:23:49 +0900

    Add code about Leetcode - Minimized Maximum of Products Distributed to Any Store

---
+++ src/main/java/gracefulsoul/problems/MinimizedMaximumOfProductsDistributedToAnyStore.java
@@ -0,0 +1,30 @@
+package gracefulsoul.problems;
+
+public class MinimizedMaximumOfProductsDistributedToAnyStore {
+
+	public static void main(String[] args) {
+		MinimizedMaximumOfProductsDistributedToAnyStore test = new MinimizedMaximumOfProductsDistributedToAnyStore();
+		System.out.println(test.minimizedMaximum(6, new int[] { 11, 6 }));
+		System.out.println(test.minimizedMaximum(7, new int[] { 15, 10, 10 }));
+		System.out.println(test.minimizedMaximum(1, new int[] { 100000 }));
+	}
+
+	public int minimizedMaximum(int n, int[] quantities) {
+		int left = 1;
+		int right = 100000;
+		while (left < right) {
+			int mid = (left + right) / 2;
+			int sum = 0;
+			for (int quantity : quantities) {
+				sum += (quantity + (mid - 1)) / mid;
+			}
+			if (sum > n) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return left;
+	}
+
+}
