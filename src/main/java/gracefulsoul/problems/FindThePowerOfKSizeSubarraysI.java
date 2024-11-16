07ab2909a3f9af4cfadbe8418f50bf91cdb27876
GracefulSoul <GracefulSoul@github.com>
2024-11-16 10:42:32 +0900

    Add code about Leetcode - Find the Power of K-Size Subarrays I

---
+++ src/main/java/gracefulsoul/problems/FindThePowerOfKSizeSubarraysI.java
@@ -0,0 +1,36 @@
+package gracefulsoul.problems;
+
+import gracefulsoul.util.PrintUtil;
+
+public class FindThePowerOfKSizeSubarraysI {
+
+	// https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i/submissions/1453936055/
+	public static void main(String[] args) {
+		FindThePowerOfKSizeSubarraysI test = new FindThePowerOfKSizeSubarraysI();
+		PrintUtil.print(test.resultsArray(new int[] { 1, 2, 3, 4, 3, 2, 5 }, 3));
+		PrintUtil.print(test.resultsArray(new int[] { 2, 2, 2, 2, 2 }, 4));
+		PrintUtil.print(test.resultsArray(new int[] { 3, 2, 3, 2, 3, 2 }, 2));
+	}
+
+	public int[] resultsArray(int[] nums, int k) {
+		int length = nums.length;
+		int m = length - k + 1;
+		int[] result = new int[m];
+		int sum = 0;
+		for (int i = 0; i < k - 1; i++) {
+			sum += nums[i];
+		}
+		int index = 0;
+		for (int i = 0, j = k - 1; j < length; i++, j++, index++) {
+			sum += nums[j];
+			if ((k * (nums[i] + nums[i] + k - 1) / 2) == sum && nums[i] <= nums[j]) {
+				result[index] = nums[j];
+			} else {
+				result[index] = -1;
+			}
+			sum -= nums[i];
+		}
+		return result;
+	}
+
+}
