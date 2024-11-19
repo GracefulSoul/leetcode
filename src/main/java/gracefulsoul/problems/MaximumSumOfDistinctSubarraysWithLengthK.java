05558d46c1d90800f53031d1a5e2c2b1e883c61f
GracefulSoul <GracefulSoul@github.com>
2024-11-19 18:28:06 +0900

    Add code about Leetcode - Maximum Sum of Distinct Subarrays With Length K

--- src/main/java/gracefulsoul/problems/MaximumSumOfDistinctSubarraysWithLengthK.java
+++ src/main/java/gracefulsoul/problems/MaximumSumOfDistinctSubarraysWithLengthK.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class MaximumSumOfDistinctSubarraysWithLengthK {
 
-	// https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/submissions/1457094539/
+	// https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/submissions/1457096839/
 	public static void main(String[] args) {
 		MaximumSumOfDistinctSubarraysWithLengthK test = new MaximumSumOfDistinctSubarraysWithLengthK();
 		System.out.println(test.maximumSubarraySum(new int[] { 1, 5, 4, 2, 9, 9, 9 }, 3));
@@ -15,7 +15,7 @@ public class MaximumSumOfDistinctSubarraysWithLengthK {
 		long result = 0L;
 		for (int i = 0, j = 0; j < nums.length; j++) {
 			int num = nums[j];
-			while (visited[num] || (j - i + 1 > k) && i < j) {
+			while (visited[num] || (j - i + 1 > k && i < j)) {
 				sum -= nums[i];
 				visited[nums[i++]] = false;
 			}
