bc5bc5d9dc3e714d30f31b8bc30b3ce8ba2e2ac6
GracefulSoul <GracefulSoul@github.com>
2024-12-09 18:17:18 +0900

    Add print about boolean array

--- src/main/java/gracefulsoul/util/PrintUtil.java
+++ src/main/java/gracefulsoul/util/PrintUtil.java
@@ -150,6 +150,10 @@ public class PrintUtil {
 		System.out.println(Arrays.toString(array));
 	}
 
+	public static void print(boolean[] array) {
+		System.out.println(Arrays.toString(array));
+	}
+
 	public static void print(String[] array) {
 		System.out.println(Arrays.toString(array));
 	}
