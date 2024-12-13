3ed44c3e7d1281ed8b1e650a72e22486908c5a3d
GracefulSoul <GracefulSoul@github.com>
2024-12-13 18:56:30 +0900

    Add code about Leetcode - Find Score of an Array After Marking All Elements

--- src/main/java/gracefulsoul/problems/FindScoreOfAnArrayAfterMarkingAllElements.java
+++ src/main/java/gracefulsoul/problems/FindScoreOfAnArrayAfterMarkingAllElements.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class FindScoreOfAnArrayAfterMarkingAllElements {
 
-	// https://leetcode.com/problems/find-score-of-an-array-after-marking-all-elements/submissions/1477722553/
+	// https://leetcode.com/problems/find-score-of-an-array-after-marking-all-elements/submissions/1477724240/
 	public static void main(String[] args) {
 		FindScoreOfAnArrayAfterMarkingAllElements test = new FindScoreOfAnArrayAfterMarkingAllElements();
 		System.out.println(test.findScore(new int[] { 2, 1, 3, 4, 5, 2 }));
@@ -12,14 +12,14 @@ public class FindScoreOfAnArrayAfterMarkingAllElements {
 	}
 
 	public long findScore(int[] nums) {
-		int n = nums.length;
-		Integer[] dp = new Integer[n];
-		for (int i = 0; i < n; i++) {
+		int length = nums.length;
+		Integer[] dp = new Integer[length];
+		for (int i = 0; i < length; i++) {
 			dp[i] = i;
 		}
 		Arrays.sort(dp, (a, b) -> nums[a] - nums[b]);
 		long result = 0;
-		boolean[] marked = new boolean[n + 2];
+		boolean[] marked = new boolean[length + 2];
 		for (int i : dp) {
 			if (!marked[i + 1]) {
 				marked[i] = marked[i + 2] = true;
