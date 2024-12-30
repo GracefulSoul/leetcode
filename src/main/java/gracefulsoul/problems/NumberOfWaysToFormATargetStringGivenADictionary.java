bae1af7e969a64684f838c9b66f08ecf83c293e0
GracefulSoul <GracefulSoul@github.com>
2024-12-30 18:24:35 +0900

    Modify code about Number of Ways to Form a Target String Given a Dictionary

--- src/main/java/gracefulsoul/problems/NumberOfWaysToFormATargetStringGivenADictionary.java
+++ src/main/java/gracefulsoul/problems/NumberOfWaysToFormATargetStringGivenADictionary.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class NumberOfWaysToFormATargetStringGivenADictionary {
 
-	// https://leetcode.com/problems/number-of-ways-to-form-a-target-string-given-a-dictionary/submissions/1491256490/
+	// https://leetcode.com/problems/number-of-ways-to-form-a-target-string-given-a-dictionary/submissions/1491301276/
 	public static void main(String[] args) {
 		NumberOfWaysToFormATargetStringGivenADictionary test = new NumberOfWaysToFormATargetStringGivenADictionary();
 		System.out.println(test.numWays(new String[] { "acca", "bbbb", "caca" }, "aba"));
@@ -21,7 +21,7 @@ public class NumberOfWaysToFormATargetStringGivenADictionary {
 				count[word.charAt(i) - 'a']++;
 			}
 			for (int j = length - 1; j >= 0; j--) {
-				dp[j + 1] += dp[j] * count[target.charAt(j) - 'a'] % MOD;
+				dp[j + 1] += (dp[j] * count[target.charAt(j) - 'a']) % MOD;
 			}
 		}
 		return (int) (dp[length] % MOD);
