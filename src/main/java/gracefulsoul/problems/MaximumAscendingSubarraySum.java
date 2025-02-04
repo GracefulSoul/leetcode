8ef249f188454e821639328ba8398e07851ae807
GracefulSoul <GracefulSoul@github.com>
2025-02-04 19:25:54 +0900

    Add code about Leetcode - Maximum Ascending Subarray Sum

--- src/main/java/gracefulsoul/problems/MaximumAscendingSubarraySum.java
+++ src/main/java/gracefulsoul/problems/MaximumAscendingSubarraySum.java
@@ -14,7 +14,7 @@ public class MaximumAscendingSubarraySum {
 		int result = nums[0];
 		int curr = nums[0];
 		for (int i = 1; i < nums.length; i++) {
-			if (nums[i] > nums[i - 1]) {
+			if (nums[i - 1] < nums[i]) {
 				curr += nums[i];
 			} else {
 				curr = nums[i];
