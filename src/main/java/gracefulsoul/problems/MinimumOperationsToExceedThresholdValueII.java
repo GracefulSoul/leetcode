70dfbb897c68f6e7a1c5beda0d4a1fff0a3fcb1d
GracefulSoul <GracefulSoul@github.com>
2025-02-13 18:09:26 +0900

    Add code about Leetcode - Minimum Operations to Exceed Threshold Value II

---
+++ src/main/java/gracefulsoul/problems/MinimumOperationsToExceedThresholdValueII.java
@@ -0,0 +1,38 @@
+package gracefulsoul.problems;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MinimumOperationsToExceedThresholdValueII {
+
+	public static void main(String[] args) {
+		MinimumOperationsToExceedThresholdValueII test = new MinimumOperationsToExceedThresholdValueII();
+		System.out.println(test.minOperations(new int[] { 2, 11, 10, 1, 3 }, 10));
+		System.out.println(test.minOperations(new int[] { 1, 1, 2, 4, 9 }, 20));
+	}
+
+	public int minOperations(int[] nums, int k) {
+		Queue<Integer> queue = new PriorityQueue<>();
+		for (int num : nums) {
+			if (num < k) {
+				queue.add(num);
+			}
+		}
+		int result = 0;
+		while (!queue.isEmpty()) {
+			int x = queue.poll();
+			result++;
+			if (queue.isEmpty()) {
+				break;
+			} else {
+				int y = queue.poll();
+				long value = 2L * x + y;
+				if (value < k) {
+					queue.add((int) value);
+				}
+			}
+		}
+		return result;
+	}
+
+}
