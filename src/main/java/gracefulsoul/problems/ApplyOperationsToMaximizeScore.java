e14b6fa67ff9c3d9e575e0753017c7a1deda243c
GracefulSoul <GracefulSoul@github.com>
2025-03-29 17:53:37 +0900

    Add code about Leetcode - Apply Operations to Maximize Score

--- src/main/java/gracefulsoul/problems/ApplyOperationsToMaximizeScore.java
+++ src/main/java/gracefulsoul/problems/ApplyOperationsToMaximizeScore.java
@@ -1,90 +1,119 @@
 package gracefulsoul.problems;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Stack;
 
 public class ApplyOperationsToMaximizeScore {
 
+	// https://leetcode.com/problems/apply-operations-to-maximize-score/submissions/1589866267/
 	public static void main(String[] args) {
 		ApplyOperationsToMaximizeScore test = new ApplyOperationsToMaximizeScore();
 		System.out.println(test.maximumScore(Arrays.asList(8, 3, 9, 3, 8), 2));
 		System.out.println(test.maximumScore(Arrays.asList(19, 12, 14, 6, 10, 18), 3));
 	}
 
-	private static final int MOD = 1000000007;
+	public static final int MOD = (int) 1e9 + 7;
 
 	public int maximumScore(List<Integer> nums, int k) {
-		int size = nums.size();
-		int upper = Collections.max(nums) + 1;
-		boolean[] prime = new boolean[upper];
-		int[] primeScore = new int[upper];
-		Arrays.fill(prime, true);
-		prime[0] = prime[1] = false;
-		for (int i = 2; i < upper; i++) {
-			if (prime[i]) {
-				for (int j = i; j < upper; j += i) {
-					primeScore[j]++;
-					prime[j] = false;
+		int n = nums.size(), max = 0;
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = nums.get(i);
+		}
+		for (int num : arr) {
+			max = Math.max(max, num);
+		}
+		int[] primeScores = getPrimeScores(arr, max);
+		int[] multiplierCnt = new int[max + 1];
+		int[] greaterThanLeft = new int[n];
+		int[] greaterThanRight = new int[n];
+		greaterThanLeft[0] = -1;
+		greaterThanRight[n - 1] = n;
+		for (int i = 1; i < n; i++) {
+			int left = i - 1;
+			while (left >= 0 && (primeScores[arr[i]] > primeScores[arr[left]])) {
+				left = greaterThanLeft[left];
+			}
+			greaterThanLeft[i] = left;
+			int right = n - i;
+			while (right < n && (primeScores[arr[n - i - 1]] >= primeScores[arr[right]])) {
+				right = greaterThanRight[right];
+			}
+			greaterThanRight[n - i - 1] = right;
+		}
+		for (int i = 0; i < n; i++) {
+			int size = greaterThanRight[i] - greaterThanLeft[i] - 1, leftSz = i - greaterThanLeft[i] - 1,
+					rightSz = greaterThanRight[i] - i - 1;
+			long subarrays = ((size * (size + 1L)) / 2 - (leftSz * (leftSz + 1L)) / 2 - (rightSz * (rightSz + 1L)) / 2);
+			multiplierCnt[arr[i]] = (int) Math.min(k, multiplierCnt[arr[i]] + subarrays);
+		}
+
+		long result = 1;
+		for (int mult = max; mult > 0; mult--) {
+			if (multiplierCnt[mult] == 0)
+				continue;
+			if (multiplierCnt[mult] >= k) {
+				result = (result * modExp(mult, k)) % MOD;
+				break;
+			} else {
+				result = (result * modExp(mult, multiplierCnt[mult])) % MOD;
+				k -= multiplierCnt[mult];
+			}
+		}
+		return (int) result;
+	}
+
+	private static int[] getPrimeScores(int[] nums, int max) {
+		int[] spf = sieveSmallestPFactors(max);
+		int[] primeScores = new int[max + 1];
+		for (int num : nums) {
+			if (primeScores[num] != 0) {
+				continue;
+			}
+			int x = num;
+			while (x > 1) {
+				primeScores[num]++;
+				int p = spf[x];
+				while (spf[x] == p) {
+					x /= p;
 				}
 			}
 		}
-		int[] nextGreaterElement = new int[size];
-		Arrays.fill(nextGreaterElement, size);
-		Stack<Integer> s = new Stack<>();
-		for (int i = size - 1; i >= 0; i--) {
-			while (!s.empty() && primeScore[nums.get(i)] >= primeScore[nums.get(s.peek())]) {
-				s.pop();
-			}
-			nextGreaterElement[i] = s.empty() ? size : s.peek();
-			s.push(i);
-		}
-		int[] prevGreaterOrEqualElement = new int[size];
-		Arrays.fill(prevGreaterOrEqualElement, -1);
-		s = new Stack<>();
-		for (int i = 0; i < size; i++) {
-			while (!s.empty() && primeScore[nums.get(i)] > primeScore[nums.get(s.peek())]) {
-				s.pop();
-			}
-			prevGreaterOrEqualElement[i] = s.empty() ? -1 : s.peek();
-			s.push(i);
-		}
-		int res = 1;
-		int[][] tuples = new int[size][2];
-		for (int i = 0; i < size; i++) {
-			tuples[i][0] = nums.get(i);
-			tuples[i][1] = i;
-		}
-		Arrays.sort(tuples, new Comparator<int[]>() {
-			public int compare(int[] a, int[] b) {
-				return b[0] - a[0];
-			}
-		});
-		for (int i = 0; i < size; i++) {
-			int num = tuples[i][0];
-			int idx = tuples[i][1];
-			int operations = Math.min((idx - prevGreaterOrEqualElement[idx]) * (nextGreaterElement[idx] - idx), k);
-			res = (int) ((1L * res * pow(num, operations)) % MOD);
-			k -= operations;
-			if (k == 0) {
-				return res;
-			}
-		}
-		return res;
+		return primeScores;
 	}
 
-	public int pow(int x, int n) {
-		int result = 1;
-		while (n > 0) {
-			if (n % 2 == 1) {
-				result = (int) ((1L * result * x) % MOD);
+	private static int[] sieveSmallestPFactors(int lim) {
+		int[] spf = new int[lim + 1];
+		boolean stopPostItr = false;
+		for (int i = 3; i <= lim; i += 2) {
+			spf[i - 1] = 2;
+			if (spf[i] != 0) {
+				continue;
 			}
-			x = (int) ((1L * x * x) % MOD);
-			n /= 2;
+			spf[i] = i;
+			if (stopPostItr) {
+				continue;
+			}
+			stopPostItr = i * i > lim;
+			for (int j = i * i; j <= lim; j += 2 * i) {
+				if (spf[j] == 0)
+					spf[j] = i;
+			}
 		}
-		return result;
+		if (lim % 2 == 0)
+			spf[lim] = 2;
+		return spf;
+	}
+
+	private static long modExp(int base, int exp) {
+		long multiplier = base, res = 1;
+		while (exp > 0) {
+			if ((exp & 1) == 1)
+				res = (res * multiplier) % MOD;
+			multiplier = (multiplier * multiplier) % MOD;
+			exp >>= 1;
+		}
+		return res;
 	}
 
 }
