9f7be4b8febaff0d59ae391d6a36444b10512360
GracefulSoul <GracefulSoul@github.com>
2025-04-09 18:07:30 +0900

    Add code about Leetcode - Minimum Operations to Make Array Values Equal to K

---
+++ src/main/java/gracefulsoul/problems/MinimumOperationsToMakeArrayValuesEqualToK.java
@@ -0,0 +1,31 @@
+package gracefulsoul.problems;
+
+public class MinimumOperationsToMakeArrayValuesEqualToK {
+
+	// https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k/submissions/1601510499/
+	public static void main(String[] args) {
+		MinimumOperationsToMakeArrayValuesEqualToK test = new MinimumOperationsToMakeArrayValuesEqualToK();
+		System.out.println(test.minOperations(new int[] { 5, 2, 5, 4, 5 }, 2));
+		System.out.println(test.minOperations(new int[] { 2, 1, 2 }, 2));
+		System.out.println(test.minOperations(new int[] { 9, 7, 5, 3 }, 1));
+	}
+
+	public int minOperations(int[] nums, int k) {
+		boolean[] seen = new boolean[101];
+		for (int num : nums) {
+			seen[num] = true;
+		}
+		int count = 0;
+		for (int i = 1; i < 101; i++) {
+			if (seen[i]) {
+				if (seen[i] && i < k) {
+					return -1;
+				} else if (seen[i] && i > k) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+}
