f1e5a7cd20549ad9eb65c0505092ca93568804c0
GracefulSoul <GracefulSoul@github.com>
2025-04-11 21:43:46 +0900

    Add code about Leetcode - Count Symmetric Integers

---
+++ src/main/java/gracefulsoul/problems/CountSymmetricIntegers.java
@@ -0,0 +1,28 @@
+package gracefulsoul.problems;
+
+public class CountSymmetricIntegers {
+
+	// https://leetcode.com/problems/count-symmetric-integers/submissions/1603633988/
+	public static void main(String[] args) {
+		CountSymmetricIntegers test = new CountSymmetricIntegers();
+		System.out.println(test.countSymmetricIntegers(1, 100));
+		System.out.println(test.countSymmetricIntegers(1200, 1230));
+	}
+
+	public int countSymmetricIntegers(int low, int high) {
+		int result = 0;
+		for (int i = low; i <= high; i++) {
+			String s = Integer.toString(i);
+			int length = s.length();
+			int diff = 0;
+			for (int j = 0; j < length / 2; j++) {
+				diff += s.charAt(j) - s.charAt(length - j - 1);
+			}
+			if (length % 2 == 0 && diff == 0) {
+				result++;
+			}
+		}
+		return result;
+	}
+
+}
