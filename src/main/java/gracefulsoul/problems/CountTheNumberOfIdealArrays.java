ded8c43bce2e3c70327438afd632c17feaea2f31
GracefulSoul <GracefulSoul@github.com>
2025-04-22 19:42:26 +0900

    Add code about Leetcode - Count the Number of Ideal Arrays

---
+++ src/main/java/gracefulsoul/problems/CountTheNumberOfIdealArrays.java
@@ -0,0 +1,55 @@
+package gracefulsoul.problems;
+
+import java.math.BigInteger;
+
+public class CountTheNumberOfIdealArrays {
+
+	// https://leetcode.com/problems/count-the-number-of-ideal-arrays/submissions/1614510916/
+	public static void main(String[] args) {
+		CountTheNumberOfIdealArrays test = new CountTheNumberOfIdealArrays();
+		System.out.println(test.idealArrays(2, 5));
+		System.out.println(test.idealArrays(5, 3));
+	}
+
+	private static final int MOD = 1000000007;
+
+	public int idealArrays(int n, int maxValue) {
+		int[] min = new int[maxValue + 1];
+		for (int i = 2; i <= maxValue; i++) {
+			if (min[i] != 0) {
+				continue;
+			}
+			for (int j = i; j <= maxValue; j += i) {
+				if (min[j] == 0) {
+					min[j] = i;
+				}
+			}
+		}
+		int maxPow = (int) (Math.log(maxValue) / Math.log(2));
+		int[] binCoeff = new int[maxPow + 1];
+		BigInteger bi = BigInteger.ONE;
+		BigInteger biMod = BigInteger.valueOf(MOD);
+		for (int i = 1; i <= maxPow; i++) {
+			bi = bi.multiply(BigInteger.valueOf(n + i - 1));
+			bi = bi.divide(BigInteger.valueOf(i));
+			binCoeff[i] = bi.mod(biMod).intValue();
+		}
+		int result = 0;
+		for (int i = 1; i <= maxValue; i++) {
+			int index = i;
+			long temp = 1;
+			while (index > 1) {
+				int curr = min[index];
+				int count = 0;
+				while (index % curr == 0) {
+					count++;
+					index /= curr;
+				}
+				temp = (temp * binCoeff[count]) % MOD;
+			}
+			result = (result + (int) temp) % MOD;
+		}
+		return result;
+	}
+
+}
