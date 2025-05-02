ffe274208b30c45ee3a43d4e4ea958147078b01f
GracefulSoul <GracefulSoul@github.com>
2025-05-02 18:14:11 +0900

    Add code about Leetcode - Valid Number

---
+++ src/main/java/gracefulsoul/problems/ValidNumber.java
@@ -0,0 +1,54 @@
+package gracefulsoul.problems;
+
+public class ValidNumber {
+
+	// https://leetcode.com/problems/valid-number/submissions/1623482976/
+	public static void main(String[] args) {
+		ValidNumber test = new ValidNumber();
+		System.out.println(test.isNumber("0"));
+		System.out.println(test.isNumber("e"));
+		System.out.println(test.isNumber("."));
+	}
+
+	public boolean isNumber(String s) {
+		char[] charArray = s.toCharArray();
+		boolean seenNumber = false;
+		boolean seenExponent = false;
+		boolean seenDot = false;
+		for (int i = 0; i < s.length(); i++) {
+			switch (charArray[i]) {
+				case '.':
+					if (seenDot || seenExponent) {
+						return false;
+					} else {
+						seenDot = true;
+						break;
+					}
+				case 'e': case 'E':
+					if (seenExponent || !seenNumber) {
+						return false;
+					} else {
+						seenExponent = true;
+						seenNumber = false;
+						break;
+					}
+				case '+': case '-':
+					if (i != 0 && (charArray[i - 1] != 'e' && charArray[i - 1] != 'E')) {
+						return false;
+					} else {
+						seenNumber = false;
+						break;
+					}
+				default:
+					if (charArray[i] - '0' < 0 || charArray[i] - '0' > 9) {
+						return false;
+					} else {
+						seenNumber = true;
+					}
+			}
+
+		}
+		return seenNumber;
+	}
+
+}
