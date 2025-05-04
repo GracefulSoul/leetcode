d2d1cac441f2baad3ddcf8cc19474b82351affe9
GracefulSoul <GracefulSoul@github.com>
2025-05-04 20:14:09 +0900

    Add code about Leetcode - Scramble String

--- src/main/java/gracefulsoul/problems/ScrambleString.java
+++ src/main/java/gracefulsoul/problems/ScrambleString.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class ScrambleString {
 
-	// https://leetcode.com/problems/scramble-string/submissions/1625138031/
+	// https://leetcode.com/problems/scramble-string/submissions/1625203618/
 	public static void main(String[] args) {
 		ScrambleString test = new ScrambleString();
 		System.out.println(test.isScramble("great", "rgeat"));
@@ -19,7 +19,7 @@ public class ScrambleString {
 					if (k == 1) {
 						dp[i][j][k] = s1.charAt(i) == s2.charAt(j);
 					} else {
-						for (int l = 1; l < k && !dp[i][j][k]; ++l) {
+						for (int l = 1; l < k && !dp[i][j][k]; l++) {
 							dp[i][j][k] = (dp[i][j][l] && dp[i + l][j + l][k - l]) || (dp[i][j + k - l][l] && dp[i + l][j][k - l]);
 						}
 					}
