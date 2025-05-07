3a9d5c2a936ad0c6843562c6e34123c6409b7c97
GracefulSoul <GracefulSoul@github.com>
2025-05-07 19:42:10 +0900

    Add code about Leetcode - Find Minimum Time to Reach Last Room I

---
+++ src/main/java/gracefulsoul/problems/FindMinimumTimeToReachLastRoomI.java
@@ -0,0 +1,67 @@
+package gracefulsoul.problems;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class FindMinimumTimeToReachLastRoomI {
+
+	// https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/submissions/1627728250/
+	public static void main(String[] args) {
+		FindMinimumTimeToReachLastRoomI test = new FindMinimumTimeToReachLastRoomI();
+		System.out.println(test.minTimeToReach(new int[][] {
+			{ 0, 4 },
+			{ 4, 4 }
+		}));
+		System.out.println(test.minTimeToReach(new int[][] {
+			{ 0, 0, 0 },
+			{ 0, 0, 0 }
+		}));
+		System.out.println(test.minTimeToReach(new int[][] {
+			{ 0, 1 },
+			{ 1, 2 }
+		}));
+	}
+
+	private static final int[][] DIRECTIONS = {
+		{ 1, 0 },
+		{ -1, 0 },
+		{ 0, 1 },
+		{ 0, -1 }
+	};
+
+	public int minTimeToReach(int[][] moveTime) {
+		int rows = moveTime.length;
+		int cols = moveTime[0].length;
+		Queue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+		int[][] dp = new int[rows][cols];
+		for (int[] row : dp) {
+			Arrays.fill(row, Integer.MAX_VALUE);
+		}
+		minHeap.offer(new int[] { 0, 0, 0 });
+		dp[0][0] = 0;
+		while (!minHeap.isEmpty()) {
+			int[] curr = minHeap.poll();
+			int time = curr[0];
+			int x = curr[1];
+			int y = curr[2];
+			if (x == rows - 1 && y == cols - 1) {
+				return time;
+			}
+			for (int[] direction : DIRECTIONS) {
+				int dx = x + direction[0];
+				int dy = y + direction[1];
+				if (dx >= 0 && dx < rows && dy >= 0 && dy < cols) {
+					int next = Math.max(moveTime[dx][dy] - time, 0) + time + 1;
+					if (next < dp[dx][dy]) {
+						dp[dx][dy] = next;
+						minHeap.offer(new int[] { next, dx, dy });
+					}
+				}
+			}
+		}
+		return -1;
+	}
+
+}
