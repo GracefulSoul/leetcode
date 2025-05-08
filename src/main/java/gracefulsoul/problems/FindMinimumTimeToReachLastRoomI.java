e09e8c5b39576b0fae8289cd39a0e01f09ee19c2
GracefulSoul <GracefulSoul@github.com>
2025-05-08 22:08:54 +0900

    Modify code about Find Minimum Time to Reach Last Room I

--- src/main/java/gracefulsoul/problems/FindMinimumTimeToReachLastRoomI.java
+++ src/main/java/gracefulsoul/problems/FindMinimumTimeToReachLastRoomI.java
@@ -7,7 +7,7 @@ import java.util.Queue;
 
 public class FindMinimumTimeToReachLastRoomI {
 
-	// https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/submissions/1627728250/
+	// https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/submissions/1628591467/
 	public static void main(String[] args) {
 		FindMinimumTimeToReachLastRoomI test = new FindMinimumTimeToReachLastRoomI();
 		System.out.println(test.minTimeToReach(new int[][] {
@@ -34,29 +34,30 @@ public class FindMinimumTimeToReachLastRoomI {
 	public int minTimeToReach(int[][] moveTime) {
 		int rows = moveTime.length;
 		int cols = moveTime[0].length;
-		Queue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+		Queue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
 		int[][] dp = new int[rows][cols];
 		for (int[] row : dp) {
 			Arrays.fill(row, Integer.MAX_VALUE);
 		}
-		minHeap.offer(new int[] { 0, 0, 0 });
+		queue.offer(new int[] { 0, 0, 0 });
 		dp[0][0] = 0;
-		while (!minHeap.isEmpty()) {
-			int[] curr = minHeap.poll();
+		while (!queue.isEmpty()) {
+			int[] curr = queue.poll();
 			int time = curr[0];
 			int x = curr[1];
 			int y = curr[2];
 			if (x == rows - 1 && y == cols - 1) {
 				return time;
-			}
-			for (int[] direction : DIRECTIONS) {
-				int dx = x + direction[0];
-				int dy = y + direction[1];
-				if (dx >= 0 && dx < rows && dy >= 0 && dy < cols) {
-					int next = Math.max(moveTime[dx][dy] - time, 0) + time + 1;
-					if (next < dp[dx][dy]) {
-						dp[dx][dy] = next;
-						minHeap.offer(new int[] { next, dx, dy });
+			} else {
+				for (int[] direction : DIRECTIONS) {
+					int dx = x + direction[0];
+					int dy = y + direction[1];
+					if (dx >= 0 && dx < rows && dy >= 0 && dy < cols) {
+						int next = Math.max(moveTime[dx][dy] - time, 0) + time + 1;
+						if (next < dp[dx][dy]) {
+							dp[dx][dy] = next;
+							queue.offer(new int[] { next, dx, dy });
+						}
 					}
 				}
 			}
