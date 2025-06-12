46874ffb2dcdf721b0fc79cd28414b5b6db240ec
GracefulSoul <GracefulSoul@github.com>
2025-06-12 19:07:05 +0900

    Modify code about Leetcode - Maximum Difference Between Even and Odd Frequency II

--- src/main/java/gracefulsoul/problems/MaximumDifferenceBetweenEvenAndOddFrequencyII.java
+++ src/main/java/gracefulsoul/problems/MaximumDifferenceBetweenEvenAndOddFrequencyII.java
@@ -13,38 +13,41 @@ public class MaximumDifferenceBetweenEvenAndOddFrequencyII {
 	}
 
 	public int maxDifference(String s, int k) {
-        int n = s.length();
-        //[0, i]中某个数字的出现次数
-        int[][] preSum = new int[n+1][5];
-        for (int i = 0; i < s.length(); ++i) {
-            for (int j = 0; j <= 4; ++j) {
-                preSum[i+1][j] = preSum[i][j];
-            }
-            preSum[i+1][s.charAt(i)-'0'] = preSum[i][s.charAt(i)-'0'] + 1;
-        }
-        int max = Integer.MIN_VALUE;
-        for (int a = 0; a <= 4; ++a) {
-            for (int b = 0; b <= 4; ++b) {
-                if (a == b) {
-                    continue;
-                }
-                int left = 0, right = 0;
-                int[][] min = new int[][]{{Integer.MAX_VALUE/2, Integer.MAX_VALUE/2}, {Integer.MAX_VALUE/2, Integer.MAX_VALUE/2}};
-                while(right < n) {
-                    while(right - left + 1 >= k && preSum[right+1][a] > preSum[left][a] && preSum[right+1][b] > preSum[left][b]) {
-                            int p = preSum[left][a] & 1;
-                            int q = preSum[left][b] & 1;
-                            min[p][q] = Math.min(min[p][q], preSum[left][a] - preSum[left][b]);
-                            ++left;
-                    }
-                    int x = preSum[right+1][a] & 1 ^ 1;
-                    int y = preSum[right+1][b] & 1;
-                    max = Math.max(max, preSum[right+1][a] - preSum[right+1][b] - min[x][y]);
-                    ++right;
-                }
-            }
-        }
-        return max;
+		char[] charArray = s.toCharArray();
+		int length = charArray.length;
+		int result = Integer.MIN_VALUE;
+		for (int a = 0; a <= 4; a++) {
+			for (int b = 0; b <= 4; b++) {
+				if (a == b) {
+					continue;
+				}
+				int[] sumA = new int[length + 1];
+				int[] sumB = new int[length + 1];
+				for (int i = 1; i <= length; ++i) {
+					sumA[i] = sumA[i - 1] + (charArray[i - 1] - '0' == a ? 1 : 0);
+					sumB[i] = sumB[i - 1] + (charArray[i - 1] - '0' == b ? 1 : 0);
+				}
+				int[][] best = new int[2][2];
+				for (int i = 0; i < 2; i++) {
+					Arrays.fill(best[i], Integer.MIN_VALUE);
+				}
+				int j = 0;
+				for (int i = k; i <= length; i++) {
+					while (i - j >= k && sumA[i] > sumA[j] && sumB[i] > sumB[j]) {
+						int prevA = sumA[j] % 2;
+						int prevB = sumB[j] % 2;
+						best[prevA][prevB] = Math.max(best[prevA][prevB], sumB[j] - sumA[j]);
+						j++;
+					}
+					int prevA = sumA[i] % 2;
+					int prevB = sumB[i] % 2;
+					if (best[1 - prevA][prevB] != Integer.MIN_VALUE) {
+						result = Math.max(result, (sumA[i] - sumB[i]) + best[1 - prevA][prevB]);
+					}
+				}
+			}
+		}
+		return result == Integer.MIN_VALUE ? -1 : result;
 	}
 
 }
