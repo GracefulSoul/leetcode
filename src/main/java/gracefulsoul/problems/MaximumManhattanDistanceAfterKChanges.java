7f026c83c8963b296a071cfc6861c992751a148f
GracefulSoul <GracefulSoul@github.com>
2025-06-20 20:32:31 +0900

    Add code about Leetcode - Maximum Manhattan Distance After K Changes

---
+++ src/main/java/gracefulsoul/problems/MaximumManhattanDistanceAfterKChanges.java
@@ -0,0 +1,30 @@
+package gracefulsoul.problems;
+
+public class MaximumManhattanDistanceAfterKChanges {
+
+	// https://leetcode.com/problems/maximum-manhattan-distance-after-k-changes/submissions/1670462503/
+	public static void main(String[] args) {
+		MaximumManhattanDistanceAfterKChanges test = new MaximumManhattanDistanceAfterKChanges();
+		System.out.println(test.maxDistance("NWSE", 1));
+		System.out.println(test.maxDistance("NSWWEW", 3));
+	}
+
+	public int maxDistance(String s, int k) {
+		char[] charArray = s.toCharArray();
+		int[] position = new int[2];
+		int result = 0;
+		for (int i = 0; i < charArray.length; i++) {
+			char c = charArray[i];
+			switch (c) {
+				case 'N': position[1]++; break;
+				case 'S': position[1]--; break;
+				case 'E': position[0]++; break;
+				default: position[0]--; break;
+			}
+			int distance = Math.abs(position[0]) + Math.abs(position[1]);
+			result = Math.max(result, distance + Math.min(2 * k, i + 1 - distance));
+		}
+		return result;
+	}
+
+}
