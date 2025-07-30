a42e2151c3186d224d713f2775f859c852d8a5d9
GracefulSoul <GracefulSoul@github.com>
2025-07-30 19:56:17 +0900

    Add code about Leetcode - Longest Subarray With Maximum Bitwise AND

---
+++ src/main/java/gracefulsoul/problems/LongestSubarrayWithMaximumBitwiseAND.java
@@ -0,0 +1,31 @@
+package gracefulsoul.problems;
+
+public class LongestSubarrayWithMaximumBitwiseAND {
+
+	// https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/submissions/1716979312/
+	public static void main(String[] args) {
+		LongestSubarrayWithMaximumBitwiseAND test = new LongestSubarrayWithMaximumBitwiseAND();
+		System.out.println(test.longestSubarray(new int[] { 1, 2, 3, 3, 2, 2 }));
+		System.out.println(test.longestSubarray(new int[] { 1, 2, 3, 4 }));
+	}
+
+	public int longestSubarray(int[] nums) {
+		int result = 1;
+		int max = Integer.MIN_VALUE;
+		for (int num : nums) {
+			max = Math.max(max, num);
+		}
+		int temp = 0;
+		for (int num : nums) {
+			if (num == max) {
+				temp++;
+			} else {
+				result = Math.max(result, temp);
+				temp = 0;
+			}
+		}
+		result = Math.max(result, temp);
+		return result;
+	}
+
+}
