3d777553be6a992c7be4f15c8690d902eee34cc4
GracefulSoul <GracefulSoul@github.com>
2025-08-20 18:33:03 +0900

    Add code about Leetcode - Element Appearing More Than 25% In Sorted Array

--- src/main/java/gracefulsoul/problems/ElementAppearingMoreThan25PercentInSortedArray.java
+++ src/main/java/gracefulsoul/problems/ElementAppearingMoreThan25PercentInSortedArray.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class ElementAppearingMoreThan25PercentInSortedArray {
 
-	// https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/submissions/1741844368/
+	// https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/submissions/1741872152/
 	public static void main(String[] args) {
 		ElementAppearingMoreThan25PercentInSortedArray test = new ElementAppearingMoreThan25PercentInSortedArray();
 		System.out.println(test.findSpecialInteger(new int[] { 1, 2, 2, 6, 6, 6, 6, 7, 10 }));
@@ -11,9 +11,9 @@ public class ElementAppearingMoreThan25PercentInSortedArray {
 
 	public int findSpecialInteger(int[] arr) {
 		int length = arr.length;
-		int half = length / 4;
-		for (int i = 0; i < length - half; i++) {
-			if (arr[i] == arr[i + half]) {
+		int quarter = length / 4;
+		for (int i = 0; i < length - quarter; i++) {
+			if (arr[i] == arr[i + quarter]) {
 				return arr[i];
 			}
 		}
