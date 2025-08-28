5b15b57306cefcb9e10caf023cab740e5a849154
GracefulSoul <GracefulSoul@github.com>
2025-08-28 20:34:49 +0900

    Add code about Leetcode - Sort Matrix by Diagonals

--- src/main/java/gracefulsoul/problems/SortMatrixByDiagonals.java
+++ src/main/java/gracefulsoul/problems/SortMatrixByDiagonals.java
@@ -7,7 +7,7 @@ import gracefulsoul.util.PrintUtil;
 
 public class SortMatrixByDiagonals {
 
-	// https://leetcode.com/problems/sort-matrix-by-diagonals/submissions/1751217298/
+	// https://leetcode.com/problems/sort-matrix-by-diagonals/submissions/1751230240/
 	public static void main(String[] args) {
 		SortMatrixByDiagonals test = new SortMatrixByDiagonals();
 		PrintUtil.print(test.sortMatrix(new int[][] {
@@ -35,16 +35,16 @@ public class SortMatrixByDiagonals {
 	private void sortDiagonal(int[][] grid, int row, int col, boolean isAscending) {
 		int length = grid.length - (isAscending ? col : row);
 		Integer[] diagonal = new Integer[length];
-		for (int i = row, j = col, k = 0; k < length; k++) {
-			diagonal[k] = grid[i++][j++];
+		for (int i = row, j = col, k = 0; k < length; i++, j++, k++) {
+			diagonal[k] = grid[i][j];
 		}
 		if (isAscending) {
 			Arrays.sort(diagonal);
 		} else {
 			Arrays.sort(diagonal, Collections.reverseOrder());
 		}
-		for (int i = row, j = col, k = 0; k < length; k++) {
-			grid[i++][j++] = diagonal[k];
+		for (int num : diagonal) {
+			grid[row++][col++] = num;
 		}
 	}
 
