0b2ffd298dbbe240f1e14f5ca636a69bb6016a65
GracefulSoul <GracefulSoul@github.com>
2025-09-23 20:45:37 +0900

    Modify code about Leetcode - Compare Version Numbers

--- src/main/java/gracefulsoul/problems/CompareVersionNumbers.java
+++ src/main/java/gracefulsoul/problems/CompareVersionNumbers.java
@@ -2,7 +2,7 @@ package gracefulsoul.problems;
 
 public class CompareVersionNumbers {
 
-	// https://leetcode.com/submissions/detail/555661062/
+	// https://leetcode.com/problems/compare-version-numbers/submissions/1780116664/
 	public static void main(String[] args) {
 		CompareVersionNumbers test = new CompareVersionNumbers();
 		System.out.println(test.compareVersion("1.01", "1.001"));
@@ -12,15 +12,34 @@ public class CompareVersionNumbers {
 	}
 
 	public int compareVersion(String version1, String version2) {
-		String[] splitVersion1 = version1.split("\\.");
-		String[] splitVersion2 = version2.split("\\.");
-		for (int idx = 0; idx < splitVersion1.length || idx < splitVersion2.length; idx++) {
-			int int1 = idx < splitVersion1.length ? Integer.parseInt(splitVersion1[idx]) : 0;
-			int int2 = idx < splitVersion2.length ? Integer.parseInt(splitVersion2[idx]) : 0;
-			if (int1 > int2) {
+		int i = 0;
+		int j = 0;
+		char[] version1CharArray = version1.toCharArray();
+		char[] version2CharArray = version2.toCharArray();
+		int version1Length = version1CharArray.length;
+		int version2Length = version2.length();
+		while (i < version1Length || j < version2Length) {
+			int num1 = 0;
+			int num2 = 0;
+			while (i < version1Length && version1CharArray[i] != '.') {
+				num1 = num1 * 10 + (version1CharArray[i] - '0');
+				i++;
+			}
+			while (j < version2Length && version2CharArray[j] != '.') {
+				num2 = num2 * 10 + (version2CharArray[j] - '0');
+				j++;
+			}
+			if (num1 > num2) {
 				return 1;
-			} else if (int1 < int2) {
+			} else if (num1 < num2) {
 				return -1;
+			} else {
+				if (i < version1Length && version1CharArray[i] == '.') {
+					i++;
+				}
+				if (j < version2Length && version2CharArray[j] == '.') {
+					j++;
+				}
 			}
 		}
 		return 0;
