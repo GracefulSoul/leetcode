775fef540a121b2fb07b55bfb0cba90d7c732226
GracefulSoul <GracefulSoul@github.com>
2025-10-31 17:45:48 +0900

    Add code about Leetcode - The Two Sneaky Numbers of Digitville

--- src/main/java/gracefulsoul/problems/TheTwoSneakyNumbersOfDigitville.java
+++ src/main/java/gracefulsoul/problems/TheTwoSneakyNumbersOfDigitville.java
@@ -4,7 +4,7 @@ import gracefulsoul.util.PrintUtil;
 
 public class TheTwoSneakyNumbersOfDigitville {
 
-	// https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/submissions/1816689815/
+	// https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/submissions/1816690927/
 	public static void main(String[] args) {
 		TheTwoSneakyNumbersOfDigitville test = new TheTwoSneakyNumbersOfDigitville();
 		PrintUtil.print(test.getSneakyNumbers(new int[] { 0, 1, 1, 0 }));
@@ -15,18 +15,14 @@ public class TheTwoSneakyNumbersOfDigitville {
 	public int[] getSneakyNumbers(int[] nums) {
 		int n = nums.length - 2;
 		int max = 0;
-		int count = 0;
 		int[] counts = new int[n];
 		for (int num : nums) {
 			if (max < ++counts[num]) {
-				count = 1;
 				max = counts[num];
-			} else if (max == counts[num]) {
-				count++;
 			}
 		}
 		int index = 0;
-		int[] result = new int[count];
+		int[] result = new int[2];
 		for (int i = 0; i < counts.length; i++) {
 			if (counts[i] == max) {
 				result[index++] = i;
