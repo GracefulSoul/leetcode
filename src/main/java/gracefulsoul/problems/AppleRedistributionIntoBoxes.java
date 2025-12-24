50283a3e36d8a732dee44921c41010b1c37cdd6e
GracefulSoul <GracefulSoul@github.com>
2025-12-24 11:33:19 +0900

    Add code about Leetcode - Apple Redistribution into Boxes

---
+++ src/main/java/gracefulsoul/problems/AppleRedistributionIntoBoxes.java
@@ -0,0 +1,28 @@
+package gracefulsoul.problems;
+
+import java.util.Arrays;
+
+public class AppleRedistributionIntoBoxes {
+
+	// https://leetcode.com/problems/apple-redistribution-into-boxes/submissions/1863772918/
+	public static void main(String[] args) {
+		AppleRedistributionIntoBoxes test = new AppleRedistributionIntoBoxes();
+		System.out.println(test.minimumBoxes(new int[] { 1, 3, 2 }, new int[] { 4, 3, 1, 5, 2 }));
+		System.out.println(test.minimumBoxes(new int[] { 5, 5, 5 }, new int[] { 2, 4, 2, 7 }));
+	}
+
+	public int minimumBoxes(int[] apple, int[] capacity) {
+		Arrays.sort(capacity);
+		int sum = 0;
+		for (int count : apple) {
+			sum += count;
+		}
+		int result = 0;
+		for (int i = capacity.length - 1; 0 <= i && 0 < sum; i--) {
+			result++;
+			sum -= capacity[i];
+		}
+		return result;
+	}
+
+}
