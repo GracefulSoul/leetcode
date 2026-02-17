c0b0b12080d4f28d5fab42cc466d419cd51fc7d8
GracefulSoul <GracefulSoul@github.com>
2026-02-17 13:08:11 +0900

    Modify code about Leetcode - Binary Watch

--- src/main/java/gracefulsoul/problems/BinaryWatch.java
+++ src/main/java/gracefulsoul/problems/BinaryWatch.java
@@ -5,28 +5,23 @@ import java.util.List;
 
 public class BinaryWatch {
 
-	// https://leetcode.com/submissions/detail/651045172/
+	// https://leetcode.com/problems/binary-watch/submissions/1921693601/
 	public static void main(String[] args) {
 		BinaryWatch test = new BinaryWatch();
 		System.out.println(test.readBinaryWatch(1));
-//		System.out.println(test.readBinaryWatch(9));
+		System.out.println(test.readBinaryWatch(9));
 	}
 
 	public List<String> readBinaryWatch(int turnedOn) {
 		List<String> times = new ArrayList<>();
 		for (int hour = 0; hour < 12; hour++) {
 			for (int minute = 0; minute < 60; minute++) {
-				int bits = Integer.bitCount(hour) + Integer.bitCount(minute);
-				if (bits == turnedOn) {
-					times.add(this.getTime(hour, minute));
+				if (Integer.bitCount(hour) + Integer.bitCount(minute) == turnedOn) {
+					times.add(hour + ":" + (minute / 10) + (minute % 10));
 				}
 			}
 		}
 		return times;
-	}
-
-	private String getTime(int hour, int minute) {
-		return new StringBuilder().append(hour).append(":").append(minute / 10).append(minute % 10).toString();
 	}
 
 }
