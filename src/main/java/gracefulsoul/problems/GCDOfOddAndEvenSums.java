0551e8817c782bf50fa010ef5101f457ce038aab
GracefulSoul <GracefulSoul@github.com>
2026-07-15 18:59:28 +0900

    Add code about Leetcode - GCD of Odd and Even Sums

---
+++ src/main/java/gracefulsoul/problems/GCDOfOddAndEvenSums.java
@@ -0,0 +1,17 @@
+package gracefulsoul.problems;
+
+public class GCDOfOddAndEvenSums {
+
+	// https://leetcode.com/problems/gcd-of-odd-and-even-sums/submissions/2068446825/
+	public static void main(String[] args) {
+		GCDOfOddAndEvenSums test = new GCDOfOddAndEvenSums();
+		System.out.println(test.gcdOfOddEvenSums(7));
+		System.out.println(test.gcdOfOddEvenSums(4));
+		System.out.println(test.gcdOfOddEvenSums(5));
+	}
+
+	public int gcdOfOddEvenSums(int n) {
+		return n;
+	}
+
+}
