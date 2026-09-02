33164b3b19d1db4f2099e1d704d15535beb2543e
GracefulSoul <GracefulSoul@github.com>
2026-09-02 19:27:48 +0900

    Add code about Leetcode - Construct Uniform Parity Array I

---
+++ src/main/java/gracefulsoul/problems/ConstructUniformParityArrayI.java
@@ -0,0 +1,16 @@
+package gracefulsoul.problems;
+
+public class ConstructUniformParityArrayI {
+
+	// https://leetcode.com/problems/construct-uniform-parity-array-i/submissions/2128313494/
+	public static void main(String[] args) {
+		ConstructUniformParityArrayI test = new ConstructUniformParityArrayI();
+		System.out.println(test.uniformArray(new int[] { 2, 3 }));
+		System.out.println(test.uniformArray(new int[] { 4, 6 }));
+	}
+
+	public boolean uniformArray(int[] nums1) {
+		return true;
+	}
+
+}
